2210c761e9263a4b5eb0f692b2ec709b7976189e
Stefan Ritt <stefan.ritt@psi.ch>
2019-05-16 13:45:11 +0200

    Cleaned up source code

--- frontends/farm/farm_fe.cu
+++ frontends/farm/farm_fe.cu
@@ -14,10 +14,8 @@
 #include <sstream>
 #include <fstream>
 
-
 #include "mudaq_device.h"
 #include "mfe.h"
-
 
 using namespace std;
 
@@ -57,7 +55,6 @@ ofstream myfile;
 mudaq::DmaMudaqDevice * mup;
 mudaq::DmaMudaqDevice::DataBlock block;
 
-
 /* DB and related */
 HNDLE hdatagenerator;
 
@@ -80,7 +77,7 @@ INT interrupt_configure(INT cmd, INT source, POINTER_T adr);
 
 EQUIPMENT equipment[] = {
 
-   {"rec_board_frontend",               /* equipment name */
+   {"stream",                /* equipment name */
     {1, 0,                   /* event ID, trigger mask */
      "SYSTEM",               /* event buffer */
      EQ_POLLED,              /* equipment type */
@@ -100,90 +97,63 @@ EQUIPMENT equipment[] = {
    {""}
 };
 
-/********************************************************************\
-              Callback routines for system transitions
-
-  These routines are called whenever a system transition like start/
-  stop of a run occurs. The routines are called on the following
-  occations:
-
-  frontend_init:  When the frontend program is started. This routine
-                  should initialize the hardware.
-
-  frontend_exit:  When the frontend program is shut down. Can be used
-                  to releas any locked resources like memory, commu-
-                  nications ports etc.
-
-  begin_of_run:   When a new run is started. Clear scalers, open
-                  rungates, etc.
-
-  end_of_run:     Called on a request to stop a run. Can send
-                  end-of-run event and close run gates.
-
-  pause_run:      When a run is paused. Should disable trigger events.
-
-  resume_run:     When a run is resumed. Should enable trigger events.
-\********************************************************************/
-
 /*-- Frontend Init -------------------------------------------------*/
 
 INT frontend_init()
 {
-    set_equipment_status(equipment[0].name, "Initializing...", "yellow");
-
-    /* Get database */
-    cm_get_experiment_database(&hDB, NULL);
-
-
-    /* Book Setting space */
-      STREAM_DATAGENERATOR_STR(stream_datagenerator_str);
-
-
-      /* Map /equipment/Stream/Datagenerator (structure defined in experim.h) */
-      char set_str[255];
-      sprintf(set_str, "/Equipment/Stream/Datagenerator");
-      int status = db_create_record(hDB, 0, set_str, strcomb(stream_datagenerator_str));
-      status = db_find_key (hDB, 0, set_str, &hdatagenerator);
-      if (status != DB_SUCCESS){
-        cm_msg(MINFO,"frontend_init","Key %s not found", set_str);
-        return status;
-    }
-
-
-    // Allocate memory for the DMA buffer - this can fail!
-    if(cudaMallocHost( (void**)&dma_buf, dma_buf_size ) != cudaSuccess){
-        cout << "Allocation failed, aborting!" << endl;
-        cm_msg(MERROR, "frontend_init" , "Allocation failed, aborting!");
-        return FE_ERR_DRIVER;
-    }
-
-    // initialize to zero
-    for (int i = 0; i <  dma_buf_nwords ; i++) {
-      (dma_buf)[ i ] = 0;
-    }
-
-    mup = new mudaq::DmaMudaqDevice("/dev/mudaq0");
-    if ( !mup->open() ) {
+   HNDLE hDB;
+   
+   set_equipment_status(equipment[0].name, "Initializing...", "yellow");
+   
+   /* Get database */
+   cm_get_experiment_database(&hDB, NULL);
+   
+   /* Book Setting space */
+   STREAM_DATAGENERATOR_STR(stream_datagenerator_str);
+   
+   /* Map /equipment/Stream/Datagenerator (structure defined in experim.h) */
+   char set_str[255];
+   sprintf(set_str, "/Equipment/Stream/Datagenerator");
+   int status = db_create_record(hDB, 0, set_str, strcomb(stream_datagenerator_str));
+   status = db_find_key (hDB, 0, set_str, &hdatagenerator);
+   if (status != DB_SUCCESS){
+      cm_msg(MINFO,"frontend_init","Key %s not found", set_str);
+      return status;
+   }
+   
+   // Allocate memory for the DMA buffer - this can fail!
+   if(cudaMallocHost( (void**)&dma_buf, dma_buf_size ) != cudaSuccess){
+      cout << "Allocation failed, aborting!" << endl;
+      cm_msg(MERROR, "frontend_init" , "Allocation failed, aborting!");
+      return FE_ERR_DRIVER;
+   }
+   
+   // initialize to zero
+   for (int i = 0; i <  dma_buf_nwords ; i++) {
+      (dma_buf)[i] = 0;
+   }
+   
+   mup = new mudaq::DmaMudaqDevice("/dev/mudaq0");
+   if ( !mup->open() ) {
       cout << "Could not open device " << endl;
       cm_msg(MERROR, "frontend_init" , "Could not open device");
       return FE_ERR_DRIVER;
-    }
-
-    if ( !mup->is_ok() )
-        return FE_ERR_DRIVER;
-
-
-    cm_msg(MINFO, "frontend_init" , "Mudaq device is ok");
-    cout << "Mudaq device is ok " << endl;
-
-    struct mesg user_message;
-    user_message.address = dma_buf;
-    user_message.size = dma_buf_size;
-
-    /* map memory to bus addresses for FPGA */
-    int ret_val = mup->map_pinned_dma_mem( user_message );
-
-    if ( ret_val < 0 ) {
+   }
+   
+   if ( !mup->is_ok() )
+      return FE_ERR_DRIVER;
+   
+   cm_msg(MINFO, "frontend_init" , "Mudaq device is ok");
+   cout << "Mudaq device is ok " << endl;
+   
+   struct mesg user_message;
+   user_message.address = dma_buf;
+   user_message.size = dma_buf_size;
+   
+   /* map memory to bus addresses for FPGA */
+   int ret_val = mup->map_pinned_dma_mem( user_message );
+   
+   if (ret_val < 0) {
       cout << "Mapping failed " << endl;
       cm_msg(MERROR, "frontend_init" , "Mapping failed");
       mup->disable();
@@ -191,18 +161,19 @@ INT frontend_init()
       free( (void *)dma_buf );
       delete mup;
       return FE_ERR_DRIVER;
-    }
-    // switch off and reste DMA for now
-    mup->disable();
-    // switch off the data generator (just in case..)
-    mup->write_register(DATAGENERATOR_REGISTER_W, 0x0);
-    usleep(2000);
-    mup->write_register(LED_REGISTER_W,0x0);
-    usleep(5000);
-
-    set_equipment_status(equipment[0].name, "Ready for running", "green");
-
-
+   }
+   
+   // switch off and reset DMA for now
+   mup->disable();
+   
+   // switch off the data generator (just in case..)
+   mup->write_register(DATAGENERATOR_REGISTER_W, 0x0);
+   usleep(2000);
+   mup->write_register(LED_REGISTER_W,0x0);
+   usleep(5000);
+   
+   set_equipment_status(equipment[0].name, "Ready for running", "green");
+   
    return SUCCESS;
 }
 
@@ -210,16 +181,16 @@ INT frontend_init()
 
 INT frontend_exit()
 {
-    if(mup){
-        mup->disable();
-        mup->close();
-        delete mup;
+   if (mup) {
+      mup->disable();
+      mup->close();
+      delete mup;
    }
-
-    cout<<"frontend exit called"<<endl;
-    free( (void *)dma_buf );
-    cout<<"frontend exit done"<<endl;
-
+   
+   cout<<"frontend exit called"<<endl;
+   free( (void *)dma_buf );
+   cout<<"frontend exit done"<<endl;
+   
    return SUCCESS;
 }
 
@@ -227,77 +198,63 @@ INT frontend_exit()
 
 INT begin_of_run(INT run_number, char *error)
 { 
-    set_equipment_status(equipment[0].name, "Starting run", "yellow");
-
-    mudaq::DmaMudaqDevice & mu = *mup;
-
-    /*Reset last written address used for polling */
-    laddr = mu.last_written_addr();
-    newdata = 0;
-    readindex = 0;
-    moreevents = false;
-    firstevent = true;
-
-    /* Reset dma part and data generator */
-    uint32_t reset_reg =0;
-    reset_reg = SET_RESET_BIT_DATAGEN(reset_reg);
-    mu.write_register_wait(RESET_REGISTER_W, reset_reg,100);
-    mu.write_register_wait(RESET_REGISTER_W, 0x0,100);
-
-    /* Enable register on FPGA for continous readout */
-    mu.enable_continous_readout(0);
-
-
-    /* Get DB info for datagenerator */
-    INT status,size;
-    STREAM_DATAGENERATOR sdg;  // defined in experim.h
-
-
-    /* Get current  settings */
-    size = sizeof(sdg);
-    status = db_get_record(hDB, hdatagenerator, &sdg, &size, 0); // HNDLE hdatagenerator obtained in frontend_init()
-    if (status != DB_SUCCESS)
-    {
-        cm_msg(MERROR, "begin_of_run", "cannot retrieve datagenerotor settings record (size of ps=%d)", size);
-        return status;
-    }
-
-    /* Set up data generator */
-    mu.write_register(DATAGENERATOR_DIVIDER_REGISTER_W, sdg.divider);
-
-    uint32_t datagen_setup =0;
-    if(sdg.enable_pixel)
-        datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE_PIXEL(datagen_setup);
-    if(sdg.enable_fibre)
-        datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE_FIBRE(datagen_setup);
-    if(sdg.enable_tile)
-        datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE_TILE(datagen_setup);
-    datagen_setup  = SET_DATAGENERATOR_NPIXEL_RANGE(datagen_setup, sdg.npixel);
-    datagen_setup  = SET_DATAGENERATOR_NFIBRE_RANGE(datagen_setup, sdg.nfibre);
-    datagen_setup  = SET_DATAGENERATOR_NTILE_RANGE(datagen_setup, sdg.ntile);
-    if(sdg.enable)
-        datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
-
-    //usleep(500000);
-
-   // cout << "Starting!" << endl;
-    cm_msg(MINFO, "begin_of_run" , "addr 0x%x" , mu.last_written_addr());
+   set_equipment_status(equipment[0].name, "Starting run", "yellow");
+   
+   mudaq::DmaMudaqDevice & mu = *mup;
+   
+   // Reset last written address used for polling
+   laddr = mu.last_written_addr();
+   newdata = 0;
+   readindex = 0;
+   moreevents = false;
+   firstevent = true;
+   
+   // Reset dma part and data generator
+   uint32_t reset_reg =0;
+   reset_reg = SET_RESET_BIT_DATAGEN(reset_reg);
+   mu.write_register_wait(RESET_REGISTER_W, reset_reg,100);
+   mu.write_register_wait(RESET_REGISTER_W, 0x0,100);
+   
+   // Enable register on FPGA for continous readout
+   mu.enable_continous_readout(0);
+   
+   // Get DB info for datagenerator
+   INT status,size;
+   STREAM_DATAGENERATOR sdg;  // defined in experim.h
+   
+   /* Get current  settings */
+   size = sizeof(sdg);
+   
+   // HNDLE hdatagenerator obtained in frontend_init()
+   status = db_get_record(hDB, hdatagenerator, &sdg, &size, 0);
+   if (status != DB_SUCCESS) {
+      cm_msg(MERROR, "begin_of_run", "cannot retrieve datagenerotor settings record (size of ps=%d)", size);
+      return status;
+   }
+   
+   /* Set up data generator */
+   mu.write_register(DATAGENERATOR_DIVIDER_REGISTER_W, sdg.divider);
+   
+   uint32_t datagen_setup =0;
+   if(sdg.enable_pixel)
+      datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE_PIXEL(datagen_setup);
+   if(sdg.enable_fibre)
+      datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE_FIBRE(datagen_setup);
+   if(sdg.enable_tile)
+      datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE_TILE(datagen_setup);
+   datagen_setup  = SET_DATAGENERATOR_NPIXEL_RANGE(datagen_setup, sdg.npixel);
+   datagen_setup  = SET_DATAGENERATOR_NFIBRE_RANGE(datagen_setup, sdg.nfibre);
+   datagen_setup  = SET_DATAGENERATOR_NTILE_RANGE(datagen_setup, sdg.ntile);
+   if(sdg.enable)
+      datagen_setup  = SET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
+   
+   cm_msg(MINFO, "begin_of_run" , "addr 0x%x" , mu.last_written_addr());
    // mu.write_register(DATAGENERATOR_REGISTER_W, datagen_setup);
-    mu.write_register(DATAGENERATOR_REGISTER_W, 0xffffffff);// start data generator
-    mu.write_register(LED_REGISTER_W,0xffffffff);
-
-
-    set_equipment_status(equipment[0].name, "Running", "#00FF00");
-
-  //  myfile.open("/home/martin/Desktop/memory_content.txt");
-  //  if ( !myfile ) {
-  //    cout << "Could not open file " << endl;
-  //    return -1;
-  //  }
-
-  //  myfile << "begin of run " << run_number << endl;
-
-
+   mu.write_register(DATAGENERATOR_REGISTER_W, 0xffffffff);// start data generator
+   mu.write_register(LED_REGISTER_W,0xffffffff);
+   
+   set_equipment_status(equipment[0].name, "Running", "#00FF00");
+   
    return SUCCESS;
 }
 
@@ -305,11 +262,8 @@ INT begin_of_run(INT run_number, char *error)
 
 INT end_of_run(INT run_number, char *error)
 {
-    mudaq::DmaMudaqDevice & mu = *mup;
-
-    //myfile << "stopping run " << run_number << endl;
-    //myfile.close();
-
+   mudaq::DmaMudaqDevice & mu = *mup;
+   
    uint32_t datagen_setup = mu.read_register_rw(DATAGENERATOR_REGISTER_W);
    datagen_setup = UNSET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
    //mu.write_register_wait(DATAGENERATOR_REGISTER_W, datagen_setup,1000);
@@ -318,7 +272,7 @@ INT end_of_run(INT run_number, char *error)
    usleep(100000); // wait for remianing data to be pushed
    mu.disable(); // disable DMA
    set_equipment_status(equipment[0].name, "Ready for running", "green");
-
+   
    return SUCCESS;
 }
 
@@ -326,14 +280,14 @@ INT end_of_run(INT run_number, char *error)
 
 INT pause_run(INT run_number, char *error)
 {
-    mudaq::DmaMudaqDevice & mu = *mup;
-
-    uint32_t datagen_setup = mu.read_register_rw(DATAGENERATOR_REGISTER_W);
-    datagen_setup = UNSET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
-    mu.write_register_wait(DATAGENERATOR_REGISTER_W, datagen_setup,1000);
-
-    set_equipment_status(equipment[0].name, "Paused", "yellow");
-
+   mudaq::DmaMudaqDevice & mu = *mup;
+   
+   uint32_t datagen_setup = mu.read_register_rw(DATAGENERATOR_REGISTER_W);
+   datagen_setup = UNSET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
+   mu.write_register_wait(DATAGENERATOR_REGISTER_W, datagen_setup,1000);
+   
+   set_equipment_status(equipment[0].name, "Paused", "yellow");
+   
    return SUCCESS;
 }
 
@@ -341,14 +295,14 @@ INT pause_run(INT run_number, char *error)
 
 INT resume_run(INT run_number, char *error)
 {
-    mudaq::DmaMudaqDevice & mu = *mup;
-
-    uint32_t datagen_setup = mu.read_register_rw(DATAGENERATOR_REGISTER_W);
-    datagen_setup = SET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
-    mu.write_register_wait(DATAGENERATOR_REGISTER_W, datagen_setup,1000);
-
-    set_equipment_status(equipment[0].name, "Running", "#00FF00");
-
+   mudaq::DmaMudaqDevice & mu = *mup;
+   
+   uint32_t datagen_setup = mu.read_register_rw(DATAGENERATOR_REGISTER_W);
+   datagen_setup = SET_DATAGENERATOR_BIT_ENABLE(datagen_setup);
+   mu.write_register_wait(DATAGENERATOR_REGISTER_W, datagen_setup,1000);
+   
+   set_equipment_status(equipment[0].name, "Running", "#00FF00");
+   
    return SUCCESS;
 }
 
@@ -361,65 +315,45 @@ INT frontend_loop()
    return SUCCESS;
 }
 
-/*------------------------------------------------------------------*/
-
-/********************************************************************\
-
-  Readout routines for different events
-
-\********************************************************************/
-
 /*-- Trigger event routines ----------------------------------------*/
 
 INT poll_event(INT source, INT count, BOOL test)
 /* Polling routine for events. Returns TRUE if event
-   is available. If test equals TRUE, don't return. The test
-   flag is used to time the polling */
+ is available. If test equals TRUE, don't return. The test
+ flag is used to time the polling */
 {
-
-    if(moreevents && !test)
-        return 1;
-
-    mudaq::DmaMudaqDevice & mu = *mup;
-
-    for (int i = 0; i < count; i++) {
-        uint32_t addr = mu.last_written_addr();
-        //cm_msg(MINFO, "poll_event" , "addr 0x%x - laddr 0x%x" , addr, laddr);
-        if((addr != laddr) && !test){
-            if(firstevent){
-                newdata = addr;
-                firstevent = false;
-            } else {
-                if(addr > laddr)
-                    newdata = addr - laddr;
-                else
-                    newdata = 0x10000 - laddr + addr;
-            }
-            if(newdata > 0x10000){
-                return 0;
-            }
-            laddr = addr;
-            //cm_msg(MINFO, "poll_event" , "Data found: newdata 0x%x, laddr 0x%x" , newdata, laddr);
-            return 1;
-        }
-    }
-    return 0;
+   
+   if(moreevents && !test)
+      return 1;
+   
+   mudaq::DmaMudaqDevice & mu = *mup;
+   
+   for (int i = 0; i < count; i++) {
+      uint32_t addr = mu.last_written_addr();
+      if ((addr != laddr) && !test) {
+         if (firstevent) {
+            newdata = addr;
+            firstevent = false;
+         } else {
+            if(addr > laddr)
+               newdata = addr - laddr;
+            else
+               newdata = 0x10000 - laddr + addr;
+         }
+         if (newdata > 0x10000) {
+            return 0;
+         }
+         laddr = addr;
+         return 1;
+      }
+   }
+   return 0;
 }
 
 /*-- Interrupt configuration ---------------------------------------*/
 
 INT interrupt_configure(INT cmd, INT source, POINTER_T adr)
 {
-   switch (cmd) {
-   case CMD_INTERRUPT_ENABLE:
-      break;
-   case CMD_INTERRUPT_DISABLE:
-      break;
-   case CMD_INTERRUPT_ATTACH:
-      break;
-   case CMD_INTERRUPT_DETACH:
-      break;
-   }
    return SUCCESS;
 }
 
@@ -427,25 +361,24 @@ INT interrupt_configure(INT cmd, INT source, POINTER_T adr)
 
 INT read_stream_event(char *pevent, INT off)
 {
-
-    bk_init(pevent);
-
-    DWORD *pdata;
-    uint32_t read = 0;
-    bk_create(pevent, "HEAD", TID_DWORD, (void **)&pdata);
-    
-    for(int i =0; i < 8; i ++){
-        *pdata++ = dma_buf[(++readindex)%dma_buf_nwords];
-        read++;
-    }
-    bk_close(pevent, pdata);
-    newdata -= read;
-
-    if(read < newdata && newdata < 0x10000)
-        moreevents = true;
-    else
-        moreevents = false;
-
-    //cout <<"size: "<<bk_size(pevent)<<endl;
-    return bk_size(pevent);
+   bk_init(pevent);
+   
+   DWORD *pdata;
+   uint32_t read = 0;
+   bk_create(pevent, "HEAD", TID_DWORD, (void **)&pdata);
+   
+   for (int i =0; i < 8; i ++) {
+      *pdata++ = dma_buf[(++readindex)%dma_buf_nwords];
+      read++;
+   }
+   
+   bk_close(pevent, pdata);
+   newdata -= read;
+   
+   if (read < newdata && newdata < 0x10000)
+      moreevents = true;
+   else
+      moreevents = false;
+   
+   return bk_size(pevent);
 }
