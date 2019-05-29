00cf9c5e5c4ff9d334f70d3d9fb023a167a74355
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-05-29 14:43:11 +0200

    [fe_malibu] use sc_s4

--- fe_board/fe_malibu/tb/sram.vhd
+++ fe_board/fe_malibu/tb/sram.vhd
@@ -2,7 +2,7 @@ library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 --use ieee.std_logic_arith.all;
-use ieee.std_logic_unsigned.all;
+--use ieee.std_logic_unsigned.all;
 
 entity sram is
   port (
