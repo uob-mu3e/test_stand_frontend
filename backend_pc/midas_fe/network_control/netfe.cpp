4bae9f6000729c1f95cf4d0c5aaee18c1a3afdfd
Martin Müller <mmarti04@students.uni-mainz.de>
2019-07-01 18:47:07 +0200

    finished netfe

--- backend_pc/midas_fe/network_control/netfe.cpp
+++ backend_pc/midas_fe/network_control/netfe.cpp
@@ -26,17 +26,20 @@ BOOL frontend_call_loop = TRUE;
 INT display_period = 1000;
 
 /* maximum event size produced by this frontend */
-INT max_event_size = 10000;
+INT max_event_size = 10000*2;
 
 /* maximum event size for fragmented events (EQ_FRAGMENTED) */
-INT max_event_size_frag = 5 * 1024 * 1024;
+INT max_event_size_frag = 5 * 1024 * 10240*2;
 
 /* buffer size to hold events */
-INT event_buffer_size = 10 * 10000;
+INT event_buffer_size = 10 * 10000*2;
 
 
 vector <string> ips;
 vector <string> requestedHostnames;
+vector <string> macs;
+vector <string> expiration;
+
 vector <string> prev_ips;
 vector <string> prev_requestedHostnames;
 vector <string> active_ips;
@@ -45,6 +48,8 @@ vector <string> unknown_ips;
 vector <string> reserved_mac_addr;
 vector <string> reserved_ips;
 vector <string> reserved_hostnames;
+
+    
 
 /*-- Function declarations -----------------------------------------*/
 
@@ -57,6 +62,8 @@ void netfe_settings_changed(HNDLE, HNDLE, int, void *);
 const char *cr_settings_str[] = {
 "DHCPD Active = BOOL : 1",
 "DNS Active = BOOL : 1",
+"usercmdReserve = BOOL : 0",
+"usercmdRmReserve = BOOL : 0",
 "nUnknown = STRING[1]:",
     "[32] 0",
 "nReserved = STRING[1]:",
@@ -107,10 +114,12 @@ EQUIPMENT equipment[] = {
 
 
 /*-- read leased ip addresses from dhcp ------------------------------------------------*/
-void read_leases(string path, vector <string> *ips, vector <string> *requestedHostnames){
+void read_leases(string path, vector <string> *ips, vector <string> *requestedHostnames,vector <string> *expiration, vector <string> *macs){
     string line;
     ifstream leases(path);
     bool hostname_requested = false;
+    bool expirationfound = false;
+    bool macfound = false;
     //int lease_number = -1; 
     
     if (leases.is_open()){
@@ -118,6 +127,8 @@ void read_leases(string path, vector <string> *ips, vector <string> *requestedHo
             if(line.substr(0,5)=="lease"){
                 ips->push_back(line.substr(6,line.length()-8));
                 hostname_requested=false;
+                expirationfound = false;
+                macfound = false;
                 
                 // search for requested hostname, macaddr, etc. in this lease now:
                 while(line != "}" and getline(leases,line)){
@@ -126,15 +137,27 @@ void read_leases(string path, vector <string> *ips, vector <string> *requestedHo
                         hostname_requested=true;
                         requestedHostnames->push_back(line.substr(19,line.length()-21));
                     }
+                    if(line.substr(2,6)=="starts"){
+                        expirationfound=true;
+                        expiration->push_back(line.substr(10,line.length()-10));
+                    }
+                    if(line.substr(2,17)=="hardware ethernet"){
+                        macs->push_back(line.substr(20,line.length()-22));
+                        macfound = true;
+                        
+                    }
+                    
                 }
                 
                 if(!hostname_requested) requestedHostnames->push_back("-");
+                if(!expirationfound) expiration->push_back("not found");
+                if(!macfound) macs->push_back("-");
             }
             
         }
         leases.close();
     }
-    else cout << "Unable to open file" <<endl;
+    else cout << "Unable to open lease file" <<endl;
 }
 
 /*-- rewrite ip/hostname table for dns server ------------------------------------------------*/
@@ -156,7 +179,7 @@ void write_dns_table(string path, vector <string> ips, vector <string> requested
     db_set_value(hDB,0,"Equipment/DHCP DNS/Settings/nDNS",to_string(nDNS).c_str(), sizeof(to_string(nDNS).c_str()), 1,TID_STRING);
     dnstable.close();
   }
-  else cout << "Unable to open file"<<endl;
+  else cout << "Unable to write dns file"<<endl;
 }
 
 /*-- find active ips in subnet ------------------------------------------------*/
@@ -225,7 +248,7 @@ void read_reserved(string path, vector <string> *ips, vector <string> *hostnames
         }
         reserved.close();
     }
-    else cout << "Unable to open file" <<endl;
+    else cout << "Unable to read dhcpd.conf" <<endl;
 }
 
 
@@ -246,11 +269,15 @@ INT interrupt_configure(INT cmd, INT source, POINTER_T adr)
 INT frontend_init()
 {
    HNDLE hKey;
+   
+   system("touch /etc/dhcpd_reservations.conf");
 
    // create Settings structure in ODB
    db_create_record(hDB, 0, "Equipment/DHCP DNS/Settings", strcomb(cr_settings_str));
    db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/leasedIPs", TID_STRING);
    db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/leasedHostnames", TID_STRING);
+   db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/leasedMACs", TID_STRING);
+   db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/expiration", TID_STRING);
    db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/reservedIPs", TID_STRING);
    db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/reservedHostnames", TID_STRING);
    db_create_key(hDB, 0, "Equipment/DHCP DNS/Settings/reservedMACs", TID_STRING);
@@ -286,56 +313,46 @@ INT frontend_exit()
 
 INT frontend_loop()
 {
+    // slow down
+    sleep(10);
+    
     string subnet = "192.168.0.";  // only X.X.X. here !!!
     string dhcpd_lease_path = "/var/lib/dhcp/db/dhcpd.leases";
     string dns_zone_def_path = "/var/lib/named/master/mu3e";
-    string dhcpd_conf_path = "/etc/dhcpd.conf";
     
+    // string dhcpd_conf_path = "/etc/dhcpd.conf";
+    // i dont want to edit /etc/dhcpd.conf directly
+    // --> manually insert   include "/etc/dhcpd-reservations.conf";   into "/etc/dhcpd.conf";
+    // and use this instead:
+    string dhcpd_conf_path = "/etc/dhcpd_reservations.conf";
+
     prev_ips = ips;
     prev_requestedHostnames = requestedHostnames;
     ips.clear();
     requestedHostnames.clear();
+    expiration.clear();
+    macs.clear();
     reserved_ips.clear();
     reserved_hostnames.clear();
     reserved_mac_addr.clear();
     
-    read_leases(dhcpd_lease_path, &ips, &requestedHostnames);
+    read_leases(dhcpd_lease_path, &ips, &requestedHostnames, &expiration, &macs);
     read_reserved(dhcpd_conf_path, &reserved_ips, &reserved_hostnames, &reserved_mac_addr);
     
     // append reserved ips tp ip list:
     ips.insert(ips.end(), reserved_ips.begin(), reserved_ips.end());
     requestedHostnames.insert(requestedHostnames.end(), reserved_hostnames.begin(), reserved_hostnames.end());
+    macs.insert(macs.end(),reserved_mac_addr.begin(),reserved_mac_addr.end());
+    for(int i = 0; i< reserved_ips.size();i++)
+        expiration.push_back("inf");
     
     // if new dhcp request:   --> update dns table
-    if(prev_ips!=ips or prev_requestedHostnames != requestedHostnames){
+
+    if(prev_ips!=ips){
+        cm_msg(MINFO, "netfe_settings_changed", "new dhcp lease, odb updated");
         
         system("wall new dhcp lease, update and restart of dns server");
-        cout<<"new ip or hostname in dhcp lease list, updating and restarting DNS server"<<endl;
-        
-        cout<<"-----------------------------------------"<<endl;
-        cout<<"reserved ips:"<<endl;
-        for(int i=0; i<reserved_ips.size(); i++){
-            cout<<reserved_ips[i]<<" \t"<< reserved_hostnames[i]<<" \t"<< reserved_mac_addr[i]<<endl;
-        }
-        
-        cout<<"-----------------------------------------"<<endl;
-        cout<<"previous leased ips:"<<endl;
-        cout<<"leased ip \t"<<"requested Hostname"<< endl;
-        for(int i=0; i<prev_ips.size(); i++){
-            cout<<prev_ips[i]<<" \t"<< prev_requestedHostnames[i]<<endl;
-        }
-        
-        cout<<"-----------------------------------------"<<endl;
-        cout<<"new leased ips:"<<endl;
-        cout<<"leased ip \t"<<"requested Hostname"<< endl;
-        for(int i=0; i<ips.size(); i++){
-            cout<<ips[i]<<" \t"<< requestedHostnames[i]<<endl;
-        }
-        
-        cout<< "updating dns table"<<endl;
         write_dns_table(dns_zone_def_path,ips,requestedHostnames);
-        
-        cout<< "restarting dns server"<<endl;
         system("rcnamed restart");
         
         //update odb only if there was a change (prev_ips!=ips). Rewrite everything if something changed
@@ -344,6 +361,8 @@ INT frontend_loop()
             //TODO: find a way to do this in a single command !!! (without loop of db_set_value)
             db_set_value_index(hDB, 0, "Equipment/DHCP DNS/Settings/leasedIPs", ips[i].c_str(), sizeof(ips[i]), i, TID_STRING, FALSE);
             db_set_value_index(hDB, 0, "Equipment/DHCP DNS/Settings/leasedHostnames", requestedHostnames[i].c_str(), sizeof(requestedHostnames[i]), i, TID_STRING, FALSE);
+            db_set_value_index(hDB, 0, "Equipment/DHCP DNS/Settings/expiration", expiration[i].c_str(), sizeof(expiration[i]), i, TID_STRING, FALSE);
+            db_set_value_index(hDB, 0, "Equipment/DHCP DNS/Settings/leasedMACs", macs[i].c_str(), sizeof(macs[i]), i, TID_STRING, FALSE);
         }
         db_set_value(hDB,0,"Equipment/DHCP DNS/Settings/nLeased", to_string(ips.size()).c_str(), sizeof(to_string(ips.size()).c_str()), 1,TID_STRING);
         
@@ -355,9 +374,6 @@ INT frontend_loop()
         db_set_value(hDB,0,"Equipment/DHCP DNS/Settings/nReserved",to_string(reserved_ips.size()).c_str(), sizeof(to_string(reserved_ips.size()).c_str()), 1,TID_STRING);
         
     }
-
-    // slow down
-    sleep(10);
     
     // ping everything in subnet
     active_ips = find_active_ips("192.168.0.");
@@ -366,12 +382,11 @@ INT frontend_loop()
     int nUnknown= unknown_ips.size();
     
     if(nUnknown>0){
-        cout<<"---------------------------------------"<<endl;
-        cout<<"WARNING: found unknown fixed IPs ------"<<endl;
-        cout<<"---------------------------------------"<<endl;
-        cout<<"remove them or give them a name in dhcpd.conf:"<<endl;
+        //cout<<"---------------------------------------"<<endl;
+        //cout<<"WARNING: found unknown fixed IPs ------"<<endl;
+        //cout<<"---------------------------------------"<<endl;
+        //cout<<"remove them or give them a name in dhcpd.conf:"<<endl;
         for(int i=0; i<nUnknown; i++){
-            cout<<unknown_ips[i]<<endl;
             db_set_value_index(hDB, 0, "Equipment/DHCP DNS/Settings/unknownIPs", unknown_ips[i].c_str(), sizeof(unknown_ips[i]), i, TID_STRING, FALSE);
         }
          db_set_value(hDB,0,"Equipment/DHCP DNS/Settings/nUnknown",to_string(nUnknown).c_str(), sizeof(to_string(nUnknown).c_str()), 1,TID_STRING);
@@ -427,17 +442,85 @@ void netfe_settings_changed(HNDLE hDB, HNDLE hKey, INT, void *)
       BOOL value;
       int size = sizeof(value);
       db_get_data(hDB, hKey, &value, &size, TID_BOOL);
-      cm_msg(MINFO, "cr_settings_changed", "Set active to %d", value);
-      printf("test"); 
-      // TODO: propagate to hardware
+      if(value==true)
+        cm_msg(MINFO, "netfe_settings_changed", "DHCPD activated by user");
+      else
+        cm_msg(MINFO, "netfe_settings_changed", "DHCPD deactivated by user");
+      system("wall dhcpd changed");
    }
 
-   if (std::string(key.name) == "DNS Active") {
-      INT value;
-      int size = sizeof(value);
-      db_get_data(hDB, hKey, &value, &size, TID_INT);
-      cm_msg(MINFO, "cr_settings_changed", "Set delay to %d", value);
-      printf("test"); 
-      // TODO: propagate to hardware
+    if (std::string(key.name) == "DNS Active") {
+        BOOL value;
+        int size = sizeof(value);
+        db_get_data(hDB, hKey, &value, &size, TID_BOOL);
+        if(value==true)
+            cm_msg(MINFO, "netfe_settings_changed", "DNS activated by user");
+        else
+            cm_msg(MINFO, "netfe_settings_changed", "DNS deactivated by user");
    }
-}
+   
+   if (std::string(key.name) == "usercmdReserve") {
+        BOOL value;
+        int size = sizeof(value);
+        char ip[100];
+        char mac[100];
+        char hostname[100];
+        //char nReservedstr[100];
+        int sizeip = sizeof(ip);
+        int sizemac = sizeof(mac);
+        int sizehostname = sizeof(hostname);
+        db_get_data(hDB, hKey, &value, &size, TID_BOOL);
+        
+        if(value){
+            cm_msg(MINFO, "netfe_settings_changed", "Execute reserve IP");
+            value = FALSE; // reset flag in ODB
+            db_set_data(hDB, hKey, &value, sizeof(value), 1, TID_BOOL);
+            
+            db_get_value(hDB, 0, "/Equipment/DHCP DNS/Settings/usereditReserveIP",ip, &sizeip, TID_STRING, TRUE);
+            db_get_value(hDB, 0, "/Equipment/DHCP DNS/Settings/usereditReserveMAC",mac, &sizemac, TID_STRING, TRUE);
+            db_get_value(hDB, 0, "/Equipment/DHCP DNS/Settings/usereditReserveHost",hostname, &sizehostname, TID_STRING, TRUE);
+
+            
+            ofstream dhcpdconf ("/etc/dhcpd_reservations.conf", std::ios_base::app);
+            if (dhcpdconf.is_open())
+            {
+                dhcpdconf << "host "<<hostname<<" {\n  hardware ethernet "<<mac<<";\n  fixed-address "<<ip<<";\n  ddns-hostname \""<<hostname<<"\";\n  option host-name \""<<hostname<<"\";\n}\n\n";
+                
+                dhcpdconf.close();
+            }
+            else cout << "Unable to write dhcpdconf"<<endl;
+            
+            system("rcdhcpd restart");
+        }
+   }
+   
+    if (std::string(key.name) == "usercmdRmReserve") {
+        BOOL value;
+        int size = sizeof(value);
+        char removeip[100];
+        int sizeip = sizeof(removeip);
+        db_get_data(hDB, hKey, &value, &size, TID_BOOL);
+        if (value) {
+            cm_msg(MINFO, "netfe_settings_changed", "Execute remove reserved IP");
+            value = FALSE; // reset flag in ODB
+            db_set_data(hDB, hKey, &value, sizeof(value), 1, TID_BOOL);
+            db_get_value(hDB, 0, "/Equipment/DHCP DNS/Settings/usereditRemReserveIP",removeip, &sizeip, TID_STRING, TRUE);
+            
+            ofstream dhcpdconf ("/etc/dhcpd_reservations.conf");
+            int n = reserved_ips.size();
+            if (dhcpdconf.is_open())
+            {
+                for(int i = 0; i<n; i++){
+                    if(reserved_ips[i]!=removeip){
+                        dhcpdconf << "host "<<reserved_hostnames[i]<<" {\n  hardware ethernet "<<reserved_mac_addr[i]<<";\n  fixed-address "<<reserved_ips[i]<<";\n  ddns-hostname \""<<reserved_hostnames[i]<<"\";\n  option host-name \""<<reserved_hostnames[i]<<"\";\n}\n\n";
+                    }
+                }
+                dhcpdconf.close();
+            }
+            else cout << "Unable to write dhcpdconf"<<endl;
+            
+            system("rcdhcpd restart");
+        }
+    }
+   
+} 
