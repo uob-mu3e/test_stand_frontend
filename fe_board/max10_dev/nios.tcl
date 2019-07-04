e3474a8524845333749244b03be10242503a38d4
Marius Koeppel <makoeppe@students.uni-mainz.de>
2019-07-04 18:58:46 +0200

    [Max10] working on adc, nios.tcl is not perfect and pll does not work

--- fe_board/max10_dev/nios.tcl
+++ fe_board/max10_dev/nios.tcl
@@ -18,12 +18,26 @@ set_instance_parameter_value cpu {exceptionSlave} {ram.s1}
 
 # ram
 add_instance ram altera_avalon_onchip_memory2
-set_instance_parameter_value ram {memorySize} {0x00006500}
+set_instance_parameter_value ram {memorySize} {0x00001000}
 set_instance_parameter_value ram {initMemContent} {0}
 
 # jtag master
 add_instance jtag_master altera_jtag_avalon_master
 
+# adc
+add_instance adc altera_modular_adc
+set_instance_parameter_value adc use_tsd {1}
+set_instance_parameter_value adc seq_order_length {1}
+#set_instance_parameter_value adc {seq_order_slot_1} {"TSD"}
+
+add_connection clk.clk adc.clock
+add_connection clk.clk_reset adc.reset_sink
+add_connection cpu.instruction_master adc.sequencer_csr
+set_connection_parameter_value cpu.instruction_master/adc.sequencer_csr baseAddress {0x20000000}
+add_connection cpu.instruction_master adc.sample_store_csr
+set_connection_parameter_value cpu.instruction_master/adc.sample_store_csr baseAddress {0x30000000}
+set_interface_property adc_clock EXPORT_OF adc.adc_pll_clock
+set_interface_property adc_locked EXPORT_OF adc.adc_pll_locked
 
 add_connection clk.clk cpu.clk
 add_connection clk.clk ram.clk1
@@ -55,11 +69,11 @@ if 1 {
     add_instance sysid altera_avalon_sysid_qsys
 
     add_instance jtag_uart altera_avalon_jtag_uart
-
-    add_instance timer altera_avalon_timer
-    apply_preset timer "Simple periodic interrupt"
-    set_instance_parameter_value timer {period} {1}
-    set_instance_parameter_value timer {periodUnits} {MSEC}
+    
+    add_instance one_sec_timer altera_avalon_timer
+    apply_preset one_sec_timer "Simple periodic interrupt"
+    set_instance_parameter_value one_sec_timer {period} {1}
+    set_instance_parameter_value one_sec_timer {periodUnits} {SEC}
 
     add_instance timer_ts altera_avalon_timer
     apply_preset timer_ts "Full-featured"
@@ -70,14 +84,26 @@ if 1 {
     set_instance_parameter_value pio {direction} {Output}
     set_instance_parameter_value pio {width} {32}
     set_instance_parameter_value pio {bitModifyingOutReg} {32}
+    
+    add_instance led_io altera_avalon_pio
+    set_instance_parameter_value led_io {direction} {Output}
+    set_instance_parameter_value led_io {width} {3}
+    set_instance_parameter_value led_io {bitModifyingOutReg} {3}
+    
+    add_instance sw_io altera_avalon_pio
+    set_instance_parameter_value sw_io {direction} {Input}
+    set_instance_parameter_value sw_io {width} {3}
+    set_instance_parameter_value sw_io {bitModifyingOutReg} {3}
 
     foreach { name clk reset avalon addr } {
-        sysid     clk   reset      control_slave     0x0000
-        jtag_uart clk   reset      avalon_jtag_slave 0x0010
-        timer     clk   reset      s1                0x0100
-        timer_ts  clk   reset      s1                0x0140
-        spi       clk   reset      spi_control_port  0x0240
-        pio       clk   reset      s1                0x0280
+        sysid           clk   reset      control_slave     0x0000
+        jtag_uart       clk   reset      avalon_jtag_slave 0x0010
+        timer_ts        clk   reset      s1                0x0140
+        spi             clk   reset      spi_control_port  0x0240
+        pio             clk   reset      s1                0x0280
+        led_io          clk   reset      s1                0x0320   
+        sw_io           clk   reset      s1                0x0360
+        one_sec_timer   clk   reset      s1                0x0400
     } {
         add_connection clk.clk       $name.$clk
         add_connection clk.clk_reset $name.$reset
@@ -89,8 +115,9 @@ if 1 {
     # IRQ assignments
     foreach { name irq } {
         jtag_uart.irq 3
-        timer.irq 0
+        one_sec_timer.irq 0
         spi.irq 11
+        adc.sample_store_irq 1
     } {
         add_connection cpu.irq $name
         set_connection_parameter_value cpu.irq/$name irqNumber $irq
@@ -101,6 +128,12 @@ if 1 {
 
     add_interface pio conduit end
     set_interface_property pio EXPORT_OF pio.external_connection
+    
+    add_interface led_io conduit end
+    set_interface_property led_io EXPORT_OF led_io.external_connection
+    
+    add_interface sw_io conduit end
+    set_interface_property sw_io EXPORT_OF sw_io.external_connection
 }
 
 save_system {nios.qsys}
