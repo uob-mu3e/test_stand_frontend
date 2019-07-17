91d3854cc30c3fc5037ef8e9807ebf7960776f89
CharlesZhong <tiancheng.zhong@kip.uni-heidelberg.de>
2019-07-17 16:37:26 +0200

    [SC] fix offset buffer bug on feb

--- fe_board/fe_malibu/software/app_src/sc.h
+++ fe_board/fe_malibu/software/app_src/sc.h
@@ -12,7 +12,7 @@ void sc_callback(volatile alt_u32* data) {
 
     // offset to buffer
     alt_u32 offset = data[1] & 0xFFFF;
-    if(!(offset >= 16 && offset + n < AVM_SC_SPAN / 4)) {
+    if(!(offset >= 0 && offset + n < AVM_SC_SPAN / 4)) {
         // out of bounds
         data[0] = 0;
         return;
@@ -21,6 +21,7 @@ void sc_callback(volatile alt_u32* data) {
     switch(command) {
     case 0x0101:
         Malibu_Powerup();
+	printf("HHHH");
         break;
     case 0x0102:
         Malibu_Powerdown();
