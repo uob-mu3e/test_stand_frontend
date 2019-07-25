c9b3bacaf02a09b1751f203eb722ce04bf4ed573
Martin Mueller <mmarti04@students.uni-mainz.de>
2019-07-25 19:34:24 +0200

    Started adding history for clock and reset system

---
+++ backend_pc/midas_fe/clock_control/clock_and_reset_history_panels_odb.h
@@ -0,0 +1,203 @@
+#ifndef CLOCK_AND_RESET_HISTORY_PANELS_ODB_H
+#define CLOCK_AND_RESET_HISTORY_PANELS_ODB_H
+
+const char *clock_and_reset_history_panels[] = {\
+"[Temperatures]",\
+"Timescale = STRING : [32] 1h",\
+"Zero ylow = BOOL : y",\
+"Show run markers = BOOL : y",\
+"Show values = BOOL : y",\
+"Sort Vars = BOOL : n",\
+"Log axis = BOOL : n",\
+"Minimum = FLOAT : 0",\
+"Maximum = FLOAT : 0",\
+"Variables = STRING[3] :",\
+"[64] Clock Reset:RX Firefly Temp",\
+"[64] Clock Reset:TX Clk Firefly Temp",\
+"[64] Clock Reset:TX Rst Firefly Temp",\
+"Label = STRING[3] :",\
+"[32] ",\
+"[32] ",\
+"[32] ",\
+"Colour = STRING[3] :",\
+"[32] #0000FF",\
+"[32] #00C000",\
+"[32] #FF0000",\
+"Factor = FLOAT[3] :",\
+"[0] 1",\
+"[1] 1",\
+"[2] 1",\
+"Offset = FLOAT[3] :",\
+"[0] 0",\
+"[1] 0",\
+"[2] 0",\
+"Buttons = STRING[7] :",\
+"[32] 10m",\
+"[32] 1h",\
+"[32] 3h",\
+"[32] 12h",\
+"[32] 24h",\
+"[32] 3d",\
+"[32] 7d",\
+"Show old vars = BOOL : n",\
+"",\
+"[Daughter 0 Temperatures]",\
+"Timescale = STRING : [32] 1h",\
+"Zero ylow = BOOL : y",\
+"Show run markers = BOOL : y",\
+"Show values = BOOL : y",\
+"Sort Vars = BOOL : n",\
+"Log axis = BOOL : n",\
+"Minimum = FLOAT : 0",\
+"Maximum = FLOAT : 0",\
+"Variables = STRING[3] :",\
+"[64] Clock Reset:D 0 0 Firefly Temp",\
+"[64] Clock Reset:D 0 1 Firefly Temp",\
+"[64] Clock Reset:D 0 2 Firefly Temp",\
+"Label = STRING[3] :",\
+"[32] ",\
+"[32] ",\
+"[32] ",\
+"Colour = STRING[3] :",\
+"[32] #0000FF",\
+"[32] #00C000",\
+"[32] #FF0000",\
+"Factor = FLOAT[3] :",\
+"[0] 1",\
+"[1] 1",\
+"[2] 1",\
+"Offset = FLOAT[3] :",\
+"[0] 0",\
+"[1] 0",\
+"[2] 0",\
+"Buttons = STRING[7] :",\
+"[32] 10m",\
+"[32] 1h",\
+"[32] 3h",\
+"[32] 12h",\
+"[32] 24h",\
+"[32] 3d",\
+"[32] 7d",\
+"Show old vars = BOOL : n",\
+"",\
+"[Daughter 1 Temperatures]",\
+"Timescale = STRING : [32] 1h",\
+"Zero ylow = BOOL : y",\
+"Show run markers = BOOL : y",\
+"Show values = BOOL : n",\
+"Sort Vars = BOOL : n",\
+"Log axis = BOOL : n",\
+"Minimum = FLOAT : 0",\
+"Maximum = FLOAT : 0",\
+"Variables = STRING[3] :",\
+"[64] Clock Reset:D 1 0 Firefly Temp",\
+"[64] Clock Reset:D 1 1 Firefly Temp",\
+"[64] Clock Reset:D 1 2 Firefly Temp",\
+"Label = STRING[3] :",\
+"[32] ",\
+"[32] ",\
+"[32] ",\
+"Colour = STRING[3] :",\
+"[32] #0000FF",\
+"[32] #00C000",\
+"[32] #FF0000",\
+"Factor = FLOAT[3] :",\
+"[0] 1",\
+"[1] 1",\
+"[2] 1",\
+"Offset = FLOAT[3] :",\
+"[0] 0",\
+"[1] 0",\
+"[2] 0",\
+"Buttons = STRING[7] :",\
+"[32] 10m",\
+"[32] 1h",\
+"[32] 3h",\
+"[32] 12h",\
+"[32] 24h",\
+"[32] 3d",\
+"[32] 7d",\
+"Show old vars = BOOL : n",\
+"",\
+"[Daughter 2 Temperatures]",\
+"Timescale = STRING : [32] 1h",\
+"Zero ylow = BOOL : y",\
+"Show run markers = BOOL : y",\
+"Show values = BOOL : n",\
+"Sort Vars = BOOL : n",\
+"Log axis = BOOL : n",\
+"Minimum = FLOAT : 0",\
+"Maximum = FLOAT : 0",\
+"Variables = STRING[3] :",\
+"[64] Clock Reset:D 2 0 Firefly Temp",\
+"[64] Clock Reset:D 2 1 Firefly Temp",\
+"[64] Clock Reset:D 2 2 Firefly Temp",\
+"Label = STRING[3] :",\
+"[32] ",\
+"[32] ",\
+"[32] ",\
+"Colour = STRING[3] :",\
+"[32] #0000FF",\
+"[32] #00C000",\
+"[32] #FF0000",\
+"Factor = FLOAT[3] :",\
+"[0] 1",\
+"[1] 1",\
+"[2] 1",\
+"Offset = FLOAT[3] :",\
+"[0] 0",\
+"[1] 0",\
+"[2] 0",\
+"Buttons = STRING[7] :",\
+"[32] 10m",\
+"[32] 1h",\
+"[32] 3h",\
+"[32] 12h",\
+"[32] 24h",\
+"[32] 3d",\
+"[32] 7d",\
+"Show old vars = BOOL : n",\
+"",\
+"[Daughter 3 Temperatures]",\
+"Timescale = STRING : [32] 1h",\
+"Zero ylow = BOOL : y",\
+"Show run markers = BOOL : y",\
+"Show values = BOOL : n",\
+"Sort Vars = BOOL : n",\
+"Log axis = BOOL : n",\
+"Minimum = FLOAT : 0",\
+"Maximum = FLOAT : 0",\
+"Variables = STRING[3] :",\
+"[64] Clock Reset:D 3 0 Firefly Temp",\
+"[64] Clock Reset:D 3 1 Firefly Temp",\
+"[64] Clock Reset:D 3 2 Firefly Temp",\
+"Label = STRING[3] :",\
+"[32] ",\
+"[32] ",\
+"[32] ",\
+"Colour = STRING[3] :",\
+"[32] #0000FF",\
+"[32] #00C000",\
+"[32] #FF0000",\
+"Factor = FLOAT[3] :",\
+"[0] 1",\
+"[1] 1",\
+"[2] 1",\
+"Offset = FLOAT[3] :",\
+"[0] 0",\
+"[1] 0",\
+"[2] 0",\
+"Buttons = STRING[7] :",\
+"[32] 10m",\
+"[32] 1h",\
+"[32] 3h",\
+"[32] 12h",\
+"[32] 24h",\
+"[32] 3d",\
+"[32] 7d",\
+"Show old vars = BOOL : n",\
+"",\
+NULL };
+
+
+#endif // CLOCK_AND_RESET_HISTORY_PANELS_ODB_H
