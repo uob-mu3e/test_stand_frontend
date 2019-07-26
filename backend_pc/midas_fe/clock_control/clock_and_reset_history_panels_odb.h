8a6671252be44a7c828f147e69b9ce465c152313
Martin Mueller <mmarti04@students.uni-mainz.de>
2019-07-26 13:20:11 +0200

    More history

--- backend_pc/midas_fe/clock_control/clock_and_reset_history_panels_odb.h
+++ backend_pc/midas_fe/clock_control/clock_and_reset_history_panels_odb.h
@@ -84,7 +84,7 @@ const char *clock_and_reset_history_panels[] = {\
 "Timescale = STRING : [32] 1h",\
 "Zero ylow = BOOL : y",\
 "Show run markers = BOOL : y",\
-"Show values = BOOL : n",\
+"Show values = BOOL : y",\
 "Sort Vars = BOOL : n",\
 "Log axis = BOOL : n",\
 "Minimum = FLOAT : 0",\
@@ -123,7 +123,7 @@ const char *clock_and_reset_history_panels[] = {\
 "Timescale = STRING : [32] 1h",\
 "Zero ylow = BOOL : y",\
 "Show run markers = BOOL : y",\
-"Show values = BOOL : n",\
+"Show values = BOOL : y",\
 "Sort Vars = BOOL : n",\
 "Log axis = BOOL : n",\
 "Minimum = FLOAT : 0",\
@@ -162,7 +162,7 @@ const char *clock_and_reset_history_panels[] = {\
 "Timescale = STRING : [32] 1h",\
 "Zero ylow = BOOL : y",\
 "Show run markers = BOOL : y",\
-"Show values = BOOL : n",\
+"Show values = BOOL : y",\
 "Sort Vars = BOOL : n",\
 "Log axis = BOOL : n",\
 "Minimum = FLOAT : 0",\
@@ -197,6 +197,123 @@ const char *clock_and_reset_history_panels[] = {\
 "[32] 7d",\
 "Show old vars = BOOL : n",\
 "",\
+    "[Daughter 4 Temperatures]",\
+    "Timescale = STRING : [32] 1h",\
+    "Zero ylow = BOOL : y",\
+    "Show run markers = BOOL : y",\
+    "Show values = BOOL : y",\
+    "Sort Vars = BOOL : n",\
+    "Log axis = BOOL : n",\
+    "Minimum = FLOAT : 0",\
+    "Maximum = FLOAT : 0",\
+    "Variables = STRING[3] :",\
+    "[64] Clock Reset:D 4 0 Firefly Temp",\
+    "[64] Clock Reset:D 4 1 Firefly Temp",\
+    "[64] Clock Reset:D 4 2 Firefly Temp",\
+    "Label = STRING[3] :",\
+    "[32] ",\
+    "[32] ",\
+    "[32] ",\
+    "Colour = STRING[3] :",\
+    "[32] #0000FF",\
+    "[32] #00C000",\
+    "[32] #FF0000",\
+    "Factor = FLOAT[3] :",\
+    "[0] 1",\
+    "[1] 1",\
+    "[2] 1",\
+    "Offset = FLOAT[3] :",\
+    "[0] 0",\
+    "[1] 0",\
+    "[2] 0",\
+    "Buttons = STRING[7] :",\
+    "[32] 10m",\
+    "[32] 1h",\
+    "[32] 3h",\
+    "[32] 12h",\
+    "[32] 24h",\
+    "[32] 3d",\
+    "[32] 7d",\
+    "Show old vars = BOOL : n",\
+    "",\
+    "[Daughter 5 Temperatures]",\
+    "Timescale = STRING : [32] 1h",\
+    "Zero ylow = BOOL : y",\
+    "Show run markers = BOOL : y",\
+    "Show values = BOOL : y",\
+    "Sort Vars = BOOL : n",\
+    "Log axis = BOOL : n",\
+    "Minimum = FLOAT : 0",\
+    "Maximum = FLOAT : 0",\
+    "Variables = STRING[3] :",\
+    "[64] Clock Reset:D 5 0 Firefly Temp",\
+    "[64] Clock Reset:D 5 1 Firefly Temp",\
+    "[64] Clock Reset:D 5 2 Firefly Temp",\
+    "Label = STRING[3] :",\
+    "[32] ",\
+    "[32] ",\
+    "[32] ",\
+    "Colour = STRING[3] :",\
+    "[32] #0000FF",\
+    "[32] #00C000",\
+    "[32] #FF0000",\
+    "Factor = FLOAT[3] :",\
+    "[0] 1",\
+    "[1] 1",\
+    "[2] 1",\
+    "Offset = FLOAT[3] :",\
+    "[0] 0",\
+    "[1] 0",\
+    "[2] 0",\
+    "Buttons = STRING[7] :",\
+    "[32] 10m",\
+    "[32] 1h",\
+    "[32] 3h",\
+    "[32] 12h",\
+    "[32] 24h",\
+    "[32] 3d",\
+    "[32] 7d",\
+    "Show old vars = BOOL : n",\
+    "",\
+    "[Daughter 6 Temperatures]",\
+    "Timescale = STRING : [32] 1h",\
+    "Zero ylow = BOOL : y",\
+    "Show run markers = BOOL : y",\
+    "Show values = BOOL : y",\
+    "Sort Vars = BOOL : n",\
+    "Log axis = BOOL : n",\
+    "Minimum = FLOAT : 0",\
+    "Maximum = FLOAT : 0",\
+    "Variables = STRING[3] :",\
+    "[64] Clock Reset:D 6 0 Firefly Temp",\
+    "[64] Clock Reset:D 6 1 Firefly Temp",\
+    "[64] Clock Reset:D 6 2 Firefly Temp",\
+    "Label = STRING[3] :",\
+    "[32] ",\
+    "[32] ",\
+    "[32] ",\
+    "Colour = STRING[3] :",\
+    "[32] #0000FF",\
+    "[32] #00C000",\
+    "[32] #FF0000",\
+    "Factor = FLOAT[3] :",\
+    "[0] 1",\
+    "[1] 1",\
+    "[2] 1",\
+    "Offset = FLOAT[3] :",\
+    "[0] 0",\
+    "[1] 0",\
+    "[2] 0",\
+    "Buttons = STRING[7] :",\
+    "[32] 10m",\
+    "[32] 1h",\
+    "[32] 3h",\
+    "[32] 12h",\
+    "[32] 24h",\
+    "[32] 3d",\
+    "[32] 7d",\
+    "Show old vars = BOOL : n",\
+    "",\
 NULL };
 
 
