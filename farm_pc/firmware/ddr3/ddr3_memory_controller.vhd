5114b3b98234a556365ca7736b000a159418a0f8
Niklaus Berger <nberger@physi.uni-heidelberg.de>
2019-08-02 18:54:02 +0200

    Working on timing closure

--- farm_pc/firmware/ddr3/ddr3_memory_controller.vhd
+++ farm_pc/firmware/ddr3/ddr3_memory_controller.vhd
@@ -119,12 +119,10 @@ M_write	<= counter_write when mode = countertest else
 				'0';
 
 M_address <= counter_address when mode = countertest else
-				 ddr3addr    when mode = dataflow	 else
-				 (others => '0');		
+				 ddr3addr    when mode = dataflow;
 				
 M_writedata	<= counter_writedata when mode = countertest else
-					ddr3datain    when mode = dataflow	  else
-					(others => '0');
+					ddr3datain    when mode = dataflow;	
 					
 					
 ddr3_read_valid	<= M_readdatavalid when mode = dataflow else '0';
