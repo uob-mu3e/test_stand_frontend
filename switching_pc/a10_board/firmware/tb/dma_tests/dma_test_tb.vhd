87721d6528b1cf25aa12340c0b4f3abf8ce669b3
makoeppel <koeppel.ma@googlemail.com>
2019-08-26 16:58:29 +0200

    start to test dma halffull

--- switching_pc/a10_board/firmware/tb/dma_tests/dma_test_tb.vhd
+++ switching_pc/a10_board/firmware/tb/dma_tests/dma_test_tb.vhd
@@ -2,8 +2,6 @@ library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-use std.textio.all;
-use IEEE.std_logic_textio.all; 
 
 
 --  A testbench has no ports.
@@ -28,7 +26,18 @@ architecture behav of dma_test_tb is
 			);
 	end component dma_counter;
 
-	signal clk 			: std_logic;
+    component ip_ram is
+        port(
+            clock           : IN STD_LOGIC  := '1';
+            data            : IN STD_LOGIC_VECTOR (95 DOWNTO 0);
+            rdaddress       : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+            wraddress       : IN STD_LOGIC_VECTOR (7 DOWNTO 0);
+            wren            : IN STD_LOGIC  := '0';
+            q               : OUT STD_LOGIC_VECTOR (95 DOWNTO 0)
+        );
+    end component ip_ram;
+
+	signal clk 			: std_logic; -- 250 MHz
   	signal reset_n 		: std_logic := '1';
   	signal enable 		: std_logic := '0';
   	signal fraccount 	: std_logic_vector(7 downto 0);
@@ -38,6 +47,11 @@ architecture behav of dma_test_tb is
 	signal dma_halffull  : std_logic := '0'; 
   	signal cnt 			: std_logic_vector(95 downto 0);
   	signal dma_end_event : std_logic;
+    
+    signal radd : std_logic_vector(7 downto 0);
+    signal wadd : std_logic_vector(7 downto 0);
+    signal wait_cnt : std_logic := '0';
+    signal q          : std_logic_vector(95 downto 0);
 
   	constant ckTime		: time := 10 ns;
 
@@ -66,19 +80,6 @@ begin
 
 	   wait for 100 ns;
 	   halffull_mode <= '1';
-
-	   wait for 50 ns;
-	   dma_halffull <= '1';
-	   
-	   wait for 50 ns;
-	   dma_halffull <= '0';
-
-	   wait for 50 ns;
-	   dma_halffull <= '1';
-
-	   wait for 50 ns;
-	   dma_halffull <= '0';
-	   
 	   
 
 	   wait;
@@ -100,5 +101,43 @@ begin
 		o_dma_wen   	=> dma_wen,
 		o_cnt     		=> cnt--,
 	);
+
+    addr : process(clk, reset_n)
+        variable diff : std_logic_vector(7 downto 0);
+    begin
+        if(reset_n = '0') then
+            radd <= (others => '0');
+            wadd <= (others => '0');
+            wait_cnt <= '0';
+            dma_halffull <= '0';
+        elsif (rising_edge(clk)) then
+            wait_cnt <= not wait_cnt;
+
+            if(dma_wen = '1') then
+                wadd <= wadd + '1';
+            end if;
+
+            if(wait_cnt = '1') then
+                radd <= radd + '1';
+            end if;
+
+            if(wadd >= radd) then
+                diff := (wadd - radd);
+            else
+                diff := (radd - wadd);
+            end if;
+            dma_halffull <= diff(7);
+        end if;
+    end process addr;
+
+    e_ram : component ip_ram
+    port map (
+            clock       => clk,
+            data        => cnt, 
+            rdaddress   => radd,
+            wraddress   => wadd,
+            wren        => dma_wen,
+            q           => q--,
+        );
 
 end behav;
