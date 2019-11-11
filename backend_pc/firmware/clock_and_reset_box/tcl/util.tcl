944bc6c2e08b111e0408181347ac94a44ce9cc36
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-11-11 09:51:35 +0100

    [clk_res] impl ip_config_diff

--- backend_pc/firmware/clock_and_reset_box/tcl/util.tcl
+++ backend_pc/firmware/clock_and_reset_box/tcl/util.tcl
@@ -1,12 +1,42 @@
 #
 
-proc puts_ip_config { name } {
+proc ip_config_puts { name } {
     set ip [ get_ips $name ]
     set properties [ list_property $ip ]
 
     foreach property $properties {
-#        if { [ string match "CONFIG.*" $property ] } {
-            puts "        $property [ get_property $property $ip ] \\"
-#        }
+        puts "    $property [ get_property $property $ip ] \\"
     }
+}
+
+proc ip_config_diff {
+    name
+    { dir .cache/ }
+} {
+    set ip [ get_ips $name ]
+    set properties [ list_property $ip ]
+
+    set name_1 ${name}_1
+    if { [ string equal [ get_ips $name_1 ] "" ] } {
+        set ipdef [ get_property IPDEF $ip ]
+        foreach { ip_vendor ip_library ip_name ip_version } [ split $ipdef ":" ] {}
+
+        create_ip -vendor $ip_vendor -library $ip_library \
+                  -name $ip_name -version $ip_version \
+                  -module_name $name_1 -dir $dir
+    }
+    set ip_1 [ get_ips $name_1 ]
+
+    puts "set_property -dict \{"
+    foreach property $properties {
+        if { ! [ string match "CONFIG.*" $property ] } continue;
+        if { [ string equal $property "CONFIG.Component_Name" ] } continue;
+
+        set val [ get_property $property $ip ]
+        set val_1 [ get_property $property $ip_1 ]
+        if { [ string equal $val $val_1 ] } continue
+
+        puts "    $property $val"
+    }
+    puts "\} \$ip"
 }
