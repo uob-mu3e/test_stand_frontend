4cc1c4c05441381d5f6d8225101df1db8f0f83ec
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-11-16 13:57:54 +0100

    [a10] impl add_fifo and add_ram_2port scripts

---
+++ common/firmware/a10/ip_ram_2port.tcl
@@ -0,0 +1,79 @@
+#
+
+proc add_ram_2port { name width words args } {
+    set widthA ${width}
+    set widthB ${width}
+    set dp 0
+    set dc 0
+    set rdw 0
+    set regA 0
+    set regB 0
+    for { set i 0 } { $i < [ llength $args ] } { incr i } {
+        switch -- [ lindex $args $i ] {
+            -widthA { incr i
+                set widthA [ lindex $args $i ]
+            }
+            -widthB { incr i
+                set widthB [ lindex $args $i ]
+            }
+            -dp {
+                set dp 1
+            }
+            -dc {
+                set dc 1
+            }
+            -rdw { incr i
+                set rdw [ lindex $args $i ]
+            }
+            -regA {
+                set regA 1
+            }
+            -regB {
+                set regB 1
+            }
+            default {
+                send_message "Error" "\[add_ram_2port\] invalid argument '[ lindex $args $i ]'"
+            }
+        }
+    }
+
+    add_instance ${name} ram_2port
+
+    set_instance_parameter_value ${name} {GUI_DATAA_WIDTH} ${width}
+    set_instance_parameter_value ${name} {GUI_QA_WIDTH} ${widthA}
+    set_instance_parameter_value ${name} {GUI_QB_WIDTH} ${widthB}
+
+    set_instance_parameter_value ${name} {GUI_MEMSIZE_WORDS} ${words}
+
+    # different data widths on different ports
+    if { ${width} != ${widthA} || ${width} != ${widthB} } {
+        set_instance_parameter_value ${name} {GUI_VAR_WIDTH} {1}
+    }
+
+    # two read/write ports
+    if { ${dp} == 1 } {
+        set_instance_parameter_value ${name} {GUI_MODE} {1}
+    }
+
+    # dual clock
+    if { ${dp} == 0 && ${dc} == 1 } {
+        # separate read and write clocks
+        set_instance_parameter_value ${name} {GUI_CLOCK_TYPE} {1}
+    }
+    if { ${dp} == 1 && ${dc} == 1 } {
+        # custom clocks for A and B ports (add clock pins)
+        set_instance_parameter_value ${name} {GUI_CLOCK_TYPE} {4}
+    }
+
+    switch -- ${rdw} {
+        0 {}
+        old { set_instance_parameter_value ${name} {GUI_Q_PORT_MODE} {1} }
+        default { send_message "Error" "\[add_ram_2port\] invalid RDW '[ lindex $args $i ]'" }
+    }
+
+    # no output registers
+    set_instance_parameter_value ${name} {GUI_READ_OUTPUT_QA} ${regA}
+    set_instance_parameter_value ${name} {GUI_READ_OUTPUT_QB} ${regB}
+
+    set_instance_property ${name} AUTO_EXPORT {true}
+}
