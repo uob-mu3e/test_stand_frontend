b346f52116b9dc3f3c81eba96e4546bc3939b263
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-11-20 16:05:47 +0100

    [a10] use tcl scripts to generate ip's
    - update device to 10AX115N2F45E1SG

---
+++ switching_pc/a10_board/firmware/pcieapp/data_addrs_ram.tcl
@@ -0,0 +1,14 @@
+#
+
+package require qsys
+
+set dir0 [ file dirname [ info script ] ]
+
+source [ file join $dir0 "../device.tcl" ]
+source [ file join $dir0 "../util/altera_ip.tcl" ]
+
+set name [ file tail [ file rootname [ info script ] ] ]
+
+create_system $name
+add_ram_2port 64 4096 -2rw -rdw old -regA -regB
+save_system [ file join $dir0 "$name.qsys" ]
