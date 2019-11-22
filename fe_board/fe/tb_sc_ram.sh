a8306db167cd6bcfc7d809be20fa242688ff7c11
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-11-22 13:09:27 +0100

    [util] support altera_mf simulation

--- fe_board/fe/tb_sc_ram.sh
+++ fe_board/fe/tb_sc_ram.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+set -eu
+IFS="$(printf '\n\t')"
 unset CDPATH
 cd "$(dirname -- "$(readlink -e -- "$0")")" || exit 1
 
 export STOPTIME=4us
 
-util/sim.sh tb_sc_ram \
+./util/sim.sh tb_sc_ram \
     tb_sc_ram.vhd sc_ram.vhd \
     util/util_pkg.vhd
