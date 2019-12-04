5b5f9aa4077fdad1a6157be4fccb046ba7fd0f97
Konrad Briggl <konrad.briggl@unige.ch>
2019-12-05 00:43:57 +0100

    Make prbs decoder a two streams block, using one common two port ram

--- fe_board/firmware/FEB_mutrig/prbs_dec/source/prbs_decoder.vhd
+++ fe_board/firmware/FEB_mutrig/prbs_dec/source/prbs_decoder.vhd
@@ -27,9 +27,15 @@ port (
 --data stream input
 	i_A_data		: in std_logic_vector(33 downto 0);
 	i_A_valid		: in std_logic;
+	i_B_data		: in std_logic_vector(33 downto 0);
+	i_B_valid		: in std_logic;
+
 --data stream output
 	o_A_data		: out std_logic_vector(33 downto 0);
 	o_A_valid		: out std_logic;
+	o_B_data		: out std_logic_vector(33 downto 0);
+	o_B_valid		: out std_logic;
+
 --disable block (make transparent)
 	i_SC_disable_dec : in std_logic
 );
@@ -49,8 +55,8 @@ architecture impl of prbs_decoder is
 	signal s_init : std_logic; --state vector, '1': Block is initializing
 	signal s_init_prbs : std_logic_vector(14 downto 0); --initialization vector(original data)
 	signal s_init_dec,s_init_dec_d : std_logic_vector(14 downto 0); --initialization vector(decoded data)
-	signal s_A_addr,n_A_addr : std_logic_vector(14 downto 0);    --r/w address on port a (init and T decoding)
 
+	signal s_A_addr,n_A_addr : std_logic_vector(14 downto 0);    --r/w address on port a (init and T decoding)
 	signal s_A_valid_2 : std_logic;
 	signal s_A_valid_1 : std_logic;
 	signal s_A_data_bypass_2  : std_logic_vector(33 downto 0); -- undecoded data bypass
@@ -64,36 +70,67 @@ architecture impl of prbs_decoder is
 	signal s_A_is_header_2 : std_logic;
 	signal s_A_is_header_1 : std_logic;
 	signal s_A_is_header_0 : std_logic;
+
+	signal s_B_addr,n_B_addr : std_logic_vector(14 downto 0);    --r/w address on port a (init and T decoding)
+	signal s_B_valid_2 : std_logic;
+	signal s_B_valid_1 : std_logic;
+	signal s_B_data_bypass_2  : std_logic_vector(33 downto 0); -- undecoded data bypass
+	signal s_B_data_bypass_1  : std_logic_vector(33 downto 0); -- undecoded data bypass
+	signal s_B_data_bypass_0    : std_logic_vector(33 downto 0); -- undecoded data bypass
+	signal s_B_data_dec : std_logic_vector(14 downto 0); -- decoded data E timestamp
+	signal s_B_select_bypass_2 : std_logic;
+	signal s_B_select_bypass_1 : std_logic;
+	signal s_B_select_bypass_0 : std_logic;
+	signal s_B_is_header_3 : std_logic;
+	signal s_B_is_header_2 : std_logic;
+	signal s_B_is_header_1 : std_logic;
+	signal s_B_is_header_0 : std_logic;
 begin
 
 
 --decoder memory
 --u_mem: decoder_mem -- replaced by direct implementation 
 u_mem : altsyncram
-GENERIC MAP (
-	clock_enable_input_a => "BYPASS",
-	clock_enable_output_a => "BYPASS",
-	intended_device_family => "Stratix IV",
-	lpm_hint => "ENABLE_RUNTIME_MOD=NO",
-	lpm_type => "altsyncram",
-	numwords_a => 32768,
-	operation_mode => "SINGLE_PORT",
-	outdata_aclr_a => "NONE",
-	outdata_reg_a => "CLOCK0",
-	power_up_uninitialized => "FALSE",
-	ram_block_type => "M9K",
-	read_during_write_mode_port_a => "NEW_DATA_NO_NBE_READ",
-	widthad_a => 15,
-	width_a => 15,
-	width_byteena_a => 1
-)
-PORT MAP (
-	address_a => s_A_addr,
-	clock0 => i_coreclk,
-	data_a => s_init_dec_d,
-	wren_a => s_init,
-	q_a => s_A_data_dec
-);
+	GENERIC MAP (
+		address_reg_b => "CLOCK0",
+		clock_enable_input_a => "BYPASS",
+		clock_enable_input_b => "BYPASS",
+		clock_enable_output_a => "BYPASS",
+		clock_enable_output_b => "BYPASS",
+		indata_reg_b => "CLOCK0",
+		intended_device_family => "Stratix IV",
+		lpm_type => "altsyncram",
+		numwords_a => 32768,
+		numwords_b => 32768,
+		operation_mode => "BIDIR_DUAL_PORT",
+		outdata_aclr_a => "NONE",
+		outdata_aclr_b => "NONE",
+		outdata_reg_a => "CLOCK0",
+		outdata_reg_b => "CLOCK0",
+		power_up_uninitialized => "FALSE",
+		read_during_write_mode_mixed_ports => "DONT_CARE",
+		read_during_write_mode_port_a => "NEW_DATA_NO_NBE_READ",
+		read_during_write_mode_port_b => "NEW_DATA_NO_NBE_READ",
+		widthad_a => 15,
+		widthad_b => 15,
+		width_a => 15,
+		width_b => 15,
+		width_byteena_a => 1,
+		width_byteena_b => 1,
+		wrcontrol_wraddress_reg_b => "CLOCK0"
+	)
+	PORT MAP (
+		address_a => s_A_addr,
+		address_b => s_B_addr,
+		clock0 => i_coreclk,
+		data_a => s_init_dec_d,
+		wren_a => s_init,
+		q_a => s_A_data_dec,
+		q_b => s_B_data_dec,
+		data_b => (others => '-'),
+		wren_b => '-'
+	);
+
 
 
 --decoder init and bypass registers
@@ -114,6 +151,11 @@ begin
 			s_A_valid_2<='0';
 			s_A_valid_1<='0';
 			o_A_valid<='0';
+
+		--B_part
+			s_B_valid_2<='0';
+			s_B_valid_1<='0';
+			o_B_valid<='0';
 		else
 		--init part
 			s_init_prbs<=s_init_prbs(13 downto 0) & not (s_init_prbs(14) xor s_init_prbs(13));
@@ -144,6 +186,28 @@ begin
 			s_A_is_header_2 <=s_A_is_header_3;
 			s_A_is_header_1 <=s_A_is_header_2;
 			s_A_is_header_0 <=s_A_is_header_1;
+
+		--B_part
+			s_B_valid_2<= i_B_valid;
+			s_B_valid_1<= s_B_valid_2;
+			o_B_valid<=s_B_valid_1 and ((not s_init) or i_SC_disable_dec); --A or init
+			s_B_data_bypass_2 <= i_B_data;
+			s_B_data_bypass_1 <= s_B_data_bypass_2;
+			s_B_data_bypass_0 <= s_B_data_bypass_1;
+
+			--scan input data for header or trailer, to be bypassed
+			s_B_select_bypass_2 <=i_B_data(33);
+			if(i_B_data(33 downto 32)="10") then
+				s_B_is_header_3 <='1';
+			else
+				s_B_is_header_3 <='0';
+			end if;
+			if(i_SC_disable_dec='1') then s_B_select_bypass_2 <= '1'; end if;
+			s_B_select_bypass_1<= s_B_select_bypass_2;
+			s_B_select_bypass_0<= s_B_select_bypass_1;
+			s_B_is_header_2 <=s_B_is_header_3;
+			s_B_is_header_1 <=s_B_is_header_2;
+			s_B_is_header_0 <=s_B_is_header_1;
 		end if;
 	end if;
 end process;
@@ -151,10 +215,14 @@ end process;
 --address assignments: PRBS data for TCC and ECC or initialization vector
 --pipelined to improve timing budget
 n_A_addr <= s_init_prbs when s_init='1' else i_A_data(20 downto 6);
+n_B_addr <= i_B_data(20 downto 6);
 
 --output assignment: replace TCC and ECC by their decoded counterparts when not bypassing
 o_A_data<=s_A_data_bypass_0	when s_A_select_bypass_0='1' or s_A_is_header_0='1' else
 	s_A_data_bypass_0(33 downto 21) & s_A_data_dec(14 downto 0) & s_A_data_bypass_0(5 downto 0);
+
+o_B_data<=s_B_data_bypass_0	when s_B_select_bypass_0='1' or s_B_is_header_0='1' else
+	s_B_data_bypass_0(33 downto 21) & s_B_data_dec(14 downto 0) & s_B_data_bypass_0(5 downto 0);
 
 o_initializing <= s_init;
 end architecture;
--- fe_board/firmware/FEB_mutrig/prbs_dec/testbench/testbench.vhd
+++ fe_board/firmware/FEB_mutrig/prbs_dec/testbench/testbench.vhd
@@ -35,9 +35,13 @@ port (
 --data stream input
 	i_A_data	: in std_logic_vector(33 downto 0);
 	i_A_valid	: in std_logic;
+	i_B_data	: in std_logic_vector(33 downto 0);
+	i_B_valid	: in std_logic;
 --data stream output
 	o_A_data	: out std_logic_vector(33 downto 0);
 	o_A_valid	: out std_logic;
+	o_B_data	: out std_logic_vector(33 downto 0);
+	o_B_valid	: out std_logic;
 --disable block (make transparent)
 	i_SC_disable_dec : in std_logic
 );
@@ -330,12 +334,20 @@ signal i_A_valid	: std_logic:='0';
 signal o_A_data	  	: std_logic_vector(33 downto 0);
 signal o_A_valid	: std_logic;
 
+signal i_B_data	  	: std_logic_vector(35 downto 0):=(others =>'0');
+signal i_B_valid	: std_logic:='0';
+signal o_B_data	  	: std_logic_vector(33 downto 0);
+signal o_B_valid	: std_logic;
+
+
 --system signals
 signal i_rst		: std_logic:='0';
 signal i_coreclk  	: std_logic:='0';
 signal o_initializing   : std_logic;
-signal s_header_payload_pre : std_logic:='0';
-signal s_header_payload_post : std_logic:='0';
+signal s_A_header_payload_pre : std_logic:='0';
+signal s_A_header_payload_post : std_logic:='0';
+signal s_B_header_payload_pre : std_logic:='0';
+signal s_B_header_payload_post : std_logic:='0';
 begin
 
 -- basic stimulus for receiver
@@ -351,11 +363,15 @@ dut: prbs_decoder
     		i_A_valid		=> i_A_valid,
     		o_A_data		=> o_A_data,
     		o_A_valid		=> o_A_valid,
+    		i_B_data		=> i_B_data(33 downto 0),
+    		i_B_valid		=> i_B_valid,
+    		o_B_data		=> o_B_data,
+    		o_B_valid		=> o_B_valid,
 		i_SC_disable_dec=> '0',
 		o_initializing  => o_initializing
 	);
 
-stim: process
+stim_A: process
 begin
 	wait for 1 us;
 	i_A_valid<='0';
@@ -373,9 +389,30 @@ begin
 	wait;	
 end process;
 
+stim_B: process
+begin
+	wait for 1 us;
+	i_B_valid<='0';
+	wait until falling_edge(o_initializing);
+	wait until rising_edge(i_coreclk);
+	wait until rising_edge(i_coreclk);
+	wait until rising_edge(i_coreclk);
+	wait until rising_edge(i_coreclk);
+	for i in 1 to 100 loop
+		i_B_valid<='1';
+		i_B_data<=s_stimulus(i);
+		wait until rising_edge(i_coreclk);
+	end loop;
+	i_B_valid<='0';
+	wait for 200 ns;
+	assert false report "Simulation Finished." severity FAILURE;
+	wait;	
+end process;
+
+-------------------------------------------A side logger --------------------------------------------------------
 -- prepocess logger
-fifo_logging: process (i_coreclk)
-file log_file : TEXT open write_mode is "preprocess_data.txt";
+fifo_logging_A: process (i_coreclk)
+file log_file : TEXT open write_mode is "preprocess_A_data.txt";
 variable l : line;
 begin
 	if rising_edge(i_rst) then
@@ -387,7 +424,7 @@ begin
 			write(log_file,"Frame Header / Payload 1"&
 				HT & "RAW "& to_hstring(i_A_data) & 
 				LF);
-				s_header_payload_pre<='1';
+				s_A_header_payload_pre<='1';
 		elsif(i_A_data(33 downto 32)="11") then
 			write(log_file,"Frame Trailer "&
 				HT & "RAW "& to_hstring(i_A_data) & 
@@ -395,14 +432,14 @@ begin
 				HT & "CRC="& "0x" & to_hstring(i_A_data(0 downto 0)) &
 				LF);
 		elsif(i_A_data(33 downto 32)="00") then
-			if(s_header_payload_pre='1') then
+			if(s_A_header_payload_pre='1') then
 				write(log_file,"Frame Header / Payload 2"&
 					HT & "RAW "& to_hstring(i_A_data) & 
 					HT & "TS(LO)="& "0x" & to_hstring(i_A_data(31 downto 16)) &
 					HT & "FSYN="& "0x" & to_hstring(i_A_data(15 downto 15)) &
 					HT & "FID ="& "0x" & to_hstring(i_A_data(14 downto 0)) & 
 					LF);
-				s_header_payload_pre<='0';
+				s_A_header_payload_pre<='0';
 			else
 				write(log_file,"Hit data");
 				write(log_file,
@@ -424,8 +461,8 @@ end process;
 
 
 -- postpocess logger
-post_logging: process (i_coreclk)
-file log_file : TEXT open write_mode is "postprocess_data.txt";
+post_logging_A: process (i_coreclk)
+file log_file : TEXT open write_mode is "postprocess_data_A.txt";
 variable l : line;
 begin
 	if rising_edge(i_rst) then
@@ -437,7 +474,7 @@ begin
 			write(log_file,"Frame Header / Payload 1"&
 				HT & "RAW "& to_hstring(o_A_data) & 
 				LF);
-				s_header_payload_post<='1';
+				s_A_header_payload_post<='1';
 		elsif(o_A_data(33 downto 32)="11") then
 			write(log_file,"Frame Trailer "&
 				HT & "RAW "& to_hstring(o_A_data) & 
@@ -445,14 +482,14 @@ begin
 				HT & "CRC="& "0x" & to_hstring(o_A_data(0 downto 0)) &
 				LF);
 		elsif(o_A_data(33 downto 32)="00") then
-			if(s_header_payload_post='1') then
+			if(s_A_header_payload_post='1') then
 				write(log_file,"Frame Header / Payload 2"&
 					HT & "RAW "& to_hstring(o_A_data) & 
 					HT & "TS(LO)="& "0x" & to_hstring(o_A_data(31 downto 16)) &
 					HT & "FSYN="& "0x" & to_hstring(o_A_data(15 downto 15)) &
 					HT & "FID ="& "0x" & to_hstring(o_A_data(14 downto 0)) & 
 					LF);
-				s_header_payload_post<='0';
+				s_A_header_payload_post<='0';
 			else
 				write(log_file,"Hit data");
 				write(log_file,
@@ -464,6 +501,107 @@ begin
 					HT & " ECC "& to_hstring(o_A_data(20 downto  6)) & 
 					HT & " EFC "& to_hstring(o_A_data( 5 downto  1)) & 
 					HT & "EFLG "& to_hstring(o_A_data( 0 downto  0)) &
+					LF);
+			end if;
+		end if;
+
+		flush(log_file);
+	end if;
+end process;
+
+
+-------------------------------------------B side logger --------------------------------------------------------
+-- prepocess logger
+fifo_logging_B: process (i_coreclk)
+file log_file : TEXT open write_mode is "preprocess_B_data.txt";
+variable l : line;
+begin
+	if rising_edge(i_rst) then
+		-- write header
+	write(l, string'("--------------------------------------------------"));
+		
+	elsif (rising_edge(i_coreclk) and i_B_valid='1') then
+		if(i_B_data(33 downto 32)="10") then
+			write(log_file,"Frame Header / Payload 1"&
+				HT & "RAW "& to_hstring(i_B_data) & 
+				LF);
+				s_B_header_payload_pre<='1';
+		elsif(i_B_data(33 downto 32)="11") then
+			write(log_file,"Frame Trailer "&
+				HT & "RAW "& to_hstring(i_B_data) & 
+				HT & "L2F="& "0x" & to_hstring(i_B_data(1 downto 1)) &
+				HT & "CRC="& "0x" & to_hstring(i_B_data(0 downto 0)) &
+				LF);
+		elsif(i_B_data(33 downto 32)="00") then
+			if(s_B_header_payload_pre='1') then
+				write(log_file,"Frame Header / Payload 2"&
+					HT & "RAW "& to_hstring(i_B_data) & 
+					HT & "TS(LO)="& "0x" & to_hstring(i_B_data(31 downto 16)) &
+					HT & "FSYN="& "0x" & to_hstring(i_B_data(15 downto 15)) &
+					HT & "FID ="& "0x" & to_hstring(i_B_data(14 downto 0)) & 
+					LF);
+				s_B_header_payload_pre<='0';
+			else
+				write(log_file,"Hit data");
+				write(log_file,
+					HT & " RAW "& to_hstring(i_B_data) & 
+					HT & "ASIC "& natural'image(to_integer(unsigned(i_B_data(31 downto 28)))) & 
+					HT & "TYPE "& natural'image(to_integer(unsigned(i_B_data(27 downto 27)))) & 
+					HT & "  CH "& natural'image(to_integer(unsigned(i_B_data(26 downto 22)))) & 
+					HT & " EBH "& to_hstring(i_B_data(21 downto 21)) & 
+					HT & " ECC "& to_hstring(i_B_data(20 downto  6)) & 
+					HT & " EFC "& to_hstring(i_B_data( 5 downto  1)) & 
+					HT & "EFLG "& to_hstring(i_B_data( 0 downto  0)) &
+					LF);
+			end if;
+		end if;
+
+		flush(log_file);
+	end if;
+end process;
+
+
+-- postpocess logger
+post_logging_B: process (i_coreclk)
+file log_file : TEXT open write_mode is "postprocess_data_B.txt";
+variable l : line;
+begin
+	if rising_edge(i_rst) then
+		-- write header
+	write(l, string'("--------------------------------------------------"));
+		
+	elsif (rising_edge(i_coreclk) and o_B_valid='1') then
+		if(o_B_data(33 downto 32)="10") then
+			write(log_file,"Frame Header / Payload 1"&
+				HT & "RAW "& to_hstring(o_B_data) & 
+				LF);
+				s_B_header_payload_post<='1';
+		elsif(o_B_data(33 downto 32)="11") then
+			write(log_file,"Frame Trailer "&
+				HT & "RAW "& to_hstring(o_B_data) & 
+				HT & "L2F="& "0x" & to_hstring(o_B_data(1 downto 1)) &
+				HT & "CRC="& "0x" & to_hstring(o_B_data(0 downto 0)) &
+				LF);
+		elsif(o_B_data(33 downto 32)="00") then
+			if(s_B_header_payload_post='1') then
+				write(log_file,"Frame Header / Payload 2"&
+					HT & "RAW "& to_hstring(o_B_data) & 
+					HT & "TS(LO)="& "0x" & to_hstring(o_B_data(31 downto 16)) &
+					HT & "FSYN="& "0x" & to_hstring(o_B_data(15 downto 15)) &
+					HT & "FID ="& "0x" & to_hstring(o_B_data(14 downto 0)) & 
+					LF);
+				s_B_header_payload_post<='0';
+			else
+				write(log_file,"Hit data");
+				write(log_file,
+					HT & " RAW "& to_hstring(o_B_data) & 
+					HT & "ASIC "& natural'image(to_integer(unsigned(o_B_data(31 downto 28)))) & 
+					HT & "TYPE "& natural'image(to_integer(unsigned(o_B_data(27 downto 27)))) & 
+					HT & "  CH "& natural'image(to_integer(unsigned(o_B_data(26 downto 22)))) & 
+					HT & " EBH "& to_hstring(o_B_data(21 downto 21)) & 
+					HT & " ECC "& to_hstring(o_B_data(20 downto  6)) & 
+					HT & " EFC "& to_hstring(o_B_data( 5 downto  1)) & 
+					HT & "EFLG "& to_hstring(o_B_data( 0 downto  0)) &
 					LF);
 			end if;
 		end if;
