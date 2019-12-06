78f711ccf8026beb878b73243ddea6588a9bacc0
Alexandr Kozlinskiy <akozlins@gmail.com>
2019-12-06 14:32:44 +0100

    [mscb] mv crc8 to util.h
    - the crc8 is CRC-8/MAXIM-DOW

--- fe_board/fe/software/app_src/mscb_user.h
+++ fe_board/fe/software/app_src/mscb_user.h
@@ -13,6 +13,8 @@
 #include "mscb.h"
 #include "system.h"
 
+#include "../include/util.h"
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -183,72 +185,6 @@ int time_diff(int t1, int t2)
     //int diff_ = (t2 - t1);
     //alt_u32 diff = (alt_u32)(t2-t1);/// (alt_u32)TIME_FREQUENCY;
     return t2-t1;
-}
-
-/*------------------------------------------------------------------*/
-
-unsigned char crc8_data[] = {
-    0x00, 0x5e, 0xbc, 0xe2, 0x61, 0x3f, 0xdd, 0x83,
-    0xc2, 0x9c, 0x7e, 0x20, 0xa3, 0xfd, 0x1f, 0x41,
-    0x9d, 0xc3, 0x21, 0x7f, 0xfc, 0xa2, 0x40, 0x1e,
-    0x5f, 0x01, 0xe3, 0xbd, 0x3e, 0x60, 0x82, 0xdc,
-    0x23, 0x7d, 0x9f, 0xc1, 0x42, 0x1c, 0xfe, 0xa0,
-    0xe1, 0xbf, 0x5d, 0x03, 0x80, 0xde, 0x3c, 0x62,
-    0xbe, 0xe0, 0x02, 0x5c, 0xdf, 0x81, 0x63, 0x3d,
-    0x7c, 0x22, 0xc0, 0x9e, 0x1d, 0x43, 0xa1, 0xff,
-    0x46, 0x18, 0xfa, 0xa4, 0x27, 0x79, 0x9b, 0xc5,
-    0x84, 0xda, 0x38, 0x66, 0xe5, 0xbb, 0x59, 0x07,
-    0xdb, 0x85, 0x67, 0x39, 0xba, 0xe4, 0x06, 0x58,
-    0x19, 0x47, 0xa5, 0xfb, 0x78, 0x26, 0xc4, 0x9a,
-    0x65, 0x3b, 0xd9, 0x87, 0x04, 0x5a, 0xb8, 0xe6,
-    0xa7, 0xf9, 0x1b, 0x45, 0xc6, 0x98, 0x7a, 0x24,
-    0xf8, 0xa6, 0x44, 0x1a, 0x99, 0xc7, 0x25, 0x7b,
-    0x3a, 0x64, 0x86, 0xd8, 0x5b, 0x05, 0xe7, 0xb9,
-    0x8c, 0xd2, 0x30, 0x6e, 0xed, 0xb3, 0x51, 0x0f,
-    0x4e, 0x10, 0xf2, 0xac, 0x2f, 0x71, 0x93, 0xcd,
-    0x11, 0x4f, 0xad, 0xf3, 0x70, 0x2e, 0xcc, 0x92,
-    0xd3, 0x8d, 0x6f, 0x31, 0xb2, 0xec, 0x0e, 0x50,
-    0xaf, 0xf1, 0x13, 0x4d, 0xce, 0x90, 0x72, 0x2c,
-    0x6d, 0x33, 0xd1, 0x8f, 0x0c, 0x52, 0xb0, 0xee,
-    0x32, 0x6c, 0x8e, 0xd0, 0x53, 0x0d, 0xef, 0xb1,
-    0xf0, 0xae, 0x4c, 0x12, 0x91, 0xcf, 0x2d, 0x73,
-    0xca, 0x94, 0x76, 0x28, 0xab, 0xf5, 0x17, 0x49,
-    0x08, 0x56, 0xb4, 0xea, 0x69, 0x37, 0xd5, 0x8b,
-    0x57, 0x09, 0xeb, 0xb5, 0x36, 0x68, 0x8a, 0xd4,
-    0x95, 0xcb, 0x29, 0x77, 0xf4, 0xaa, 0x48, 0x16,
-    0xe9, 0xb7, 0x55, 0x0b, 0x88, 0xd6, 0x34, 0x6a,
-    0x2b, 0x75, 0x97, 0xc9, 0x4a, 0x14, 0xf6, 0xa8,
-    0x74, 0x2a, 0xc8, 0x96, 0x15, 0x4b, 0xa9, 0xf7,
-    0xb6, 0xe8, 0x0a, 0x54, 0xd7, 0x89, 0x6b, 0x35,
-};
-
-unsigned char crc8(unsigned char *buffer, int len)
-/********************************************************************\
-
-  Routine: crc8
-
-  Purpose: Calculate 8-bit cyclic redundancy checksum for a full
-           buffer
-
-  Input:
-    unsigned char *data     data buffer
-    int len                 data length in bytes
-
-
-  Function value:
-    unsighend char          CRC-8 code
-
-\********************************************************************/
-{
-    int i;
-    unsigned char crc8_code, index;
-
-    crc8_code = 0;
-    for (i = 0; i < len; i++) {
-        index = buffer[i] ^ crc8_code;
-        crc8_code = crc8_data[index];
-    }
-    return crc8_code;
 }
 
 void mscb_init()
@@ -533,8 +469,8 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
 //  }
 
     // check CRC
-    if (crc8(buf, buflen-1) != buf[buflen-1]) {
-        //printf("MSCB interprete: Invalid CRC %02X vs %02X\r\n", crc8(buf, buflen-1), buf[buflen-1]);
+    if (util::crc8_dow(buf, buflen-1) != buf[buflen-1]) {
+        //printf("MSCB interprete: Invalid CRC %02X vs %02X\r\n", util::crc8_dow(buf, buflen-1), buf[buflen-1]);
         return 0;
     }
     //else printf("CRC ok\n");
@@ -599,7 +535,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
             rb[n++] = 1024 >> 8;                 // max. buffer size 1024 bytes
             rb[n++] = 1024 & 0xFF;
 
-            rb[n] = crc8(rb, n);
+            rb[n] = util::crc8_dow(rb, n);
             n++;
             break;
 
@@ -622,7 +558,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
                 for (i = 0; i < 8; i++)            // send variable name
                 rb[n++] = pvar->name[i];
 
-                rb[n] = crc8(rb, n);
+                rb[n] = util::crc8_dow(rb, n);
                 n++;
             } else {
                 /* just send dummy ack */
@@ -668,7 +604,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
             printf("ECHO\n");
             rb[0] = MCMD_ACK + 1;
             rb[1] = buf[1];
-            rb[2] = crc8(rb, 2);
+            rb[2] = util::crc8_dow(rb, 2);
             n = 3;
             break;
 
@@ -698,7 +634,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
             }
 
             rb[0] = MCMD_ACK;
-            rb[1] = crc8(buf, size);
+            rb[1] = util::crc8_dow(buf, size);
             n = 2;
             buf[0] = 0; // do not re-interprete command below
 
@@ -731,7 +667,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
             rb[1] = 0x80 | ((size >> 8) & 0x7F);
             rb[2] = size & 0xFF;
             memcpy(rb+3, buf, size);
-            rb[3+size] = crc8(rb, size+3);
+            rb[3+size] = util::crc8_dow(rb, size+3);
             n = size+4;
             buf[0] = 0; // do not re-interprete command below
             break;
@@ -747,7 +683,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
                 rb[0] = MCMD_ACK + 7;          // and places data directly in out_buf
                 rb[1] = n;
                 n += 2;
-                rb[n] = crc8(rb, n);           // generate CRC code
+                rb[n] = util::crc8_dow(rb, n);           // generate CRC code
                 n += 1;
             } else {
                 user_read(buf[1]);
@@ -765,7 +701,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
                     n += 1;
                 }
 
-                rb[n] = crc8(rb, n);           // generate CRC code
+                rb[n] = util::crc8_dow(rb, n);           // generate CRC code
                 n++;
             }
         } else {
@@ -796,7 +732,7 @@ unsigned int mscb_interprete(int submaster, unsigned char *buf, unsigned char *r
                         rb[n++] = ((char *) variables[i].ud)[j+g_var_size * g_cur_sub_addr];
                 }
 
-                rb[n] = crc8(rb, n);
+                rb[n] = util::crc8_dow(rb, n);
                 n++;
             } else {
                 /* just send dummy ack to indicate error */
