708cba19670925830eb987e4c7db447d1a0a4055
Martin Mueller <mmarti04@students.uni-mainz.de>
2020-02-12 17:45:48 +0100

    add software to control lab power supplies from midas

---
+++ backend_pc/midas_fe/lab_power_control/lv.h
@@ -0,0 +1,14 @@
+/********************************************************************\
+
+  Name:         lv.h
+  Created by:   Frederik Wauters
+
+  Contents:     Low Voltage Class Driver Header File
+
+  $Id:$
+
+\********************************************************************/
+
+/* class driver routines */
+INT cd_lv(INT cmd, PEQUIPMENT pequipment);
+INT cd_lv_read(char *pevent, int);
