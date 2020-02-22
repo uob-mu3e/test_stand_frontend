cfa14254959c07da78b96b06a70654e352a0b9a6
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-02-22 12:19:03 +0100

    fifo_merger - fix bugs, improve next_index

--- switching_pc/a10_board/firmware/fifo_merger.vhd
+++ switching_pc/a10_board/firmware/fifo_merger.vhd
@@ -10,14 +10,14 @@ generic (
 );
 port (
     i_rdata     : in    std_logic_vector(N*W-1 downto 0);
-    i_rsop      : in    std_logic_vector(N-1 downto 0); -- start of packet
-    i_reop      : in    std_logic_vector(N-1 downto 0); -- end of packet
+    i_rsop      : in    std_logic_vector(N-1 downto 0); -- start of packet (SOP)
+    i_reop      : in    std_logic_vector(N-1 downto 0); -- end of packet (EOP)
     i_rempty    : in    std_logic_vector(N-1 downto 0);
     o_rack      : out   std_logic_vector(N-1 downto 0);
 
     o_wdata     : out   std_logic_vector(W-1 downto 0);
-    o_wsop      : out   std_logic; -- start of packet
-    o_weop      : out   std_logic; -- end of packet
+    o_wsop      : out   std_logic; -- SOP
+    o_weop      : out   std_logic; -- EOP
     o_we        : out   std_logic;
 
     i_reset_n   : in    std_logic;
@@ -27,45 +27,46 @@ end entity;
 
 architecture arch of fifo_merger is
 
+    -- current index
     signal index : integer range 0 to N-1 := 0;
 
+    -- get next index such that empty(index) = '0'
     function next_index (
         index : integer;
         empty : std_logic_vector--;
     ) return integer is
-        variable i : integer;
     begin
-        for i in index+1 to empty'length loop
-            if ( empty(i) = '0' ) then
-                return i;
-            end if;
+        for i in empty'range loop
+            index := (index + 1) mod empty'length;
+            exit when ( empty(index) = '0' );
         end loop;
-        for i in 0 to index-1 loop
-            if ( empty(i) = '0' ) then
-                return i;
-            end if;
-        end loop;
-        if ( index = empty'length-1 ) then
-            return 0;
-        else
-            return index + 1;
-        end if;
+        return index;
     end function;
 
-    signal rsop : std_logic_vector(N-1 downto 0);
+    -- SOP marker
+    signal sop : std_logic_vector(N-1 downto 0);
 
 begin
 
-    o_rack <= (
-        index => not i_rempty(index),
-        others => '0'
-    );
+    -- drive rack (read ack) for current not empty input
+    process(index, i_rempty, i_reset_n)
+    begin
+        o_rack <= (others => '0');
+        if ( i_rempty(index) = '0' and i_reset_n = '1' ) then
+            o_rack(index) <= '1';
+        end if;
+    end process;
 
     process(i_clk, i_reset_n)
     begin
-    if ( i_reset_n = '0' ) then
-        o_rack <= (others => '0');
+    if ( i_reset_n /= '1' ) then
+        o_wdata <= (others => '0');
+        o_wsop <= (others => '0');
+        o_weop <= (others => '0');
         o_we <= '0';
+
+        index <= 0;
+        sop <= (others => '0');
         --
     elsif rising_edge(i_clk) then
         o_wdata <= i_rdata(W-1 + W*index downto 0 + W*index);
@@ -79,16 +80,16 @@ begin
 
             if ( i_rsop(index) = '1' ) then
                 -- mark SOP
-                rsop <= i_rsop(index);
+                sop(index) <= i_rsop(index);
             end if;
 
             if ( i_reop(index) = '1' ) then
                 -- reset SOP
-                rsop <= '0';
+                sop(index) <= '0';
                 -- go to next index
                 index <= next_index(index, i_rempty);
             end if;
-        elsif ( rsop(index) = '0' ) then
+        elsif ( sop(index) = '0' ) then
             -- not inside packet -> go to next index
             index <= next_index(index, i_rempty);
         end if;
