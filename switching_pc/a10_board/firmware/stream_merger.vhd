c0ebe0d769777507396132dbfa1081cab8619767
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-02-22 13:28:19 +0100

    stream_merger - replace sop with busy

--- switching_pc/a10_board/firmware/stream_merger.vhd
+++ switching_pc/a10_board/firmware/stream_merger.vhd
@@ -1,8 +1,8 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
---use ieee.std_logic_unsigned.all;
 
+-- merge packets delimited by SOP and EOP from N input streams
 entity stream_merger is
 generic (
     W : positive := 32;
@@ -13,13 +13,13 @@ port (
     i_rsop      : in    std_logic_vector(N-1 downto 0); -- start of packet (SOP)
     i_reop      : in    std_logic_vector(N-1 downto 0); -- end of packet (EOP)
     i_rempty    : in    std_logic_vector(N-1 downto 0);
-    o_rack      : out   std_logic_vector(N-1 downto 0);
+    o_rack      : out   std_logic_vector(N-1 downto 0); -- read ACK
 
     o_wdata     : out   std_logic_vector(W-1 downto 0);
     o_wsop      : out   std_logic; -- SOP
     o_weop      : out   std_logic; -- EOP
     i_wfull     : in    std_logic;
-    o_we        : out   std_logic;
+    o_we        : out   std_logic; -- write enable
 
     i_reset_n   : in    std_logic;
     i_clk       : in    std_logic--;
@@ -31,7 +31,7 @@ architecture arch of stream_merger is
     -- current index
     signal index : integer range 0 to N-1 := 0;
 
-    -- get next index such that empty(index) = '0'
+    -- get next (Round-Robin) index such that empty(index) = '0'
     function next_index (
         index : integer;
         empty : std_logic_vector--;
@@ -39,18 +39,18 @@ architecture arch of stream_merger is
         variable i : integer;
     begin
         for j in empty'range loop
-            i := (index + j + 1) mod empty'length;
+            i := (index + 1 + j) mod empty'length;
             exit when ( empty(i) = '0' );
         end loop;
         return i;
     end function;
 
-    -- SOP marker
-    signal sop : std_logic_vector(N-1 downto 0);
+    -- SOP mark
+    signal busy : std_logic;
 
 begin
 
-    -- drive rack (read ack) for current not empty input
+    -- drive rack for current not empty input (and not full and not reset)
     process(index, i_rempty, i_reset_n)
     begin
         o_rack <= (others => '0');
@@ -68,7 +68,7 @@ begin
         o_we <= '0';
 
         index <= 0;
-        sop <= (others => '0');
+        busy <= '0';
         --
     elsif rising_edge(i_clk) then
         o_wdata <= i_rdata(W-1 + W*index downto 0 + W*index);
@@ -81,18 +81,18 @@ begin
             o_we <= '1';
 
             if ( i_rsop(index) = '1' ) then
-                -- mark SOP
-                sop(index) <= i_rsop(index);
+                -- set SOP mark
+                busy <= '1';
             end if;
 
             if ( i_reop(index) = '1' ) then
-                -- reset SOP
-                sop(index) <= '0';
+                -- reset SOP mark
+                busy <= '0';
                 -- go to next index
                 index <= next_index(index, i_rempty);
             end if;
-        elsif ( sop(index) = '0' ) then
-            -- not inside packet -> go to next index
+        elsif ( busy = '0' ) then
+            -- go to next index
             index <= next_index(index, i_rempty);
         end if;
 
