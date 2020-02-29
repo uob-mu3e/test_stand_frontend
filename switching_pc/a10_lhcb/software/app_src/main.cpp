129461b3f716fffd8c5b0348cf9edb66384f0954
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-02-29 12:31:18 +0100

    [a10_lhcb] add clocks_menu

--- switching_pc/a10_lhcb/software/app_src/main.cpp
+++ switching_pc/a10_lhcb/software/app_src/main.cpp
@@ -2,14 +2,48 @@
 #include "../include/base.h"
 
 #include "../include/i2c.h"
-
 #include "../include/si534x.h"
+
+i2c_t i2c;
+si534x_t si5345 { i2c.dev, 0x68 };
+
+void clocks_menu() {
+    while (1) {
+        printf("\n");
+        printf("[PCIe40] -------- menu --------\n");
+
+        printf("\n");
+        printf("  [1] => si5345_1\n");
+        printf("  [2] => si5345_2\n");
+        printf("  [m] => ...\n");
+        printf("  [e] => ...\n");
+        printf("  [q] => exit\n");
+
+        printf("Select entry ...\n");
+        char cmd = wait_key();
+        switch(cmd) {
+        case '1':
+            IOWR_ALTERA_AVALON_PIO_DATA(I2C_CS_BASE, 1 << 1); // i2c_cs(1) <= '1'
+            break;
+        case '2':
+            IOWR_ALTERA_AVALON_PIO_DATA(I2C_CS_BASE, 1 << 2); // i2c_cs(2) <= '1'
+            break;
+        case 'm':
+            si5345.menu();
+            break;
+        case 'e':
+            // TODO: enable outputs
+            break;
+        case 'q':
+            return;
+        default:
+            printf("invalid command: '%c'\n", cmd);
+        }
+    }
+}
 
 int main() {
     base_init();
-
-    i2c_t i2c;
-    si534x_t si5345_1 { i2c.dev, 0x68 };
 
     while (1) {
         printf("\n");
@@ -25,8 +59,7 @@ int main() {
         case '1':
             break;
         case 'c':
-            IOWR_ALTERA_AVALON_PIO_DATA(I2C_CS_BASE, 1 << 1);
-            si5345_1.menu();
+            clocks_menu();
             break;
         default:
             printf("invalid command: '%c'\n", cmd);
