bf77d296a09d0abb5a965618fad3ab035280ebd1
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-03-02 07:38:56 +0100

    [a10_lhcb] add si5345_1 registers
    SI5345_1:
    in0 = 40 MHz
    in1 = 40 MHz
    in2 = 125 MHZ
    out0 = out1 = out2 = out3 = 125 MHz
    out4 = 250 MHz

--- switching_pc/a10_lhcb/software/app_src/main.cpp
+++ switching_pc/a10_lhcb/software/app_src/main.cpp
@@ -1,10 +1,14 @@
 
 #include "../include/base.h"
 
+#include "../include/xcvr.h"
+
 #include "../include/i2c.h"
 #include "../include/si534x.h"
 
 i2c_t i2c;
+
+#include "Si5345-RevD-SI5345_1-Registers.h"
 si534x_t si5345 { i2c.dev, 0x68 };
 
 void clocks_menu() {
@@ -15,8 +19,8 @@ void clocks_menu() {
         printf("\n");
         printf("  [1] => si5345_1\n");
         printf("  [2] => si5345_2\n");
-        printf("  [m] => ...\n");
-        printf("  [e] => ...\n");
+        printf("  [m] => menu\n");
+        printf("  [I] => init\n");
         printf("  [q] => exit\n");
 
         printf("Select entry ...\n");
@@ -31,8 +35,8 @@ void clocks_menu() {
         case 'm':
             si5345.menu();
             break;
-        case 'e':
-            // TODO: enable outputs
+        case 'I':
+            si5345.init(si5345_revd_registers, sizeof(si5345_revd_registers) / sizeof(si5345_revd_registers[0]));
             break;
         case 'q':
             return;
@@ -51,12 +55,16 @@ int main() {
 
         printf("\n");
         printf("  [1] => ...\n");
+        printf("  [p] => PODs\n");
         printf("  [c] => clocks\n");
 
         printf("Select entry ...\n");
         char cmd = wait_key();
         switch(cmd) {
         case '1':
+            break;
+        case 'p':
+            menu_xcvr((alt_u32*)(AVM_POD_BASE | ALT_CPU_DCACHE_BYPASS_MASK), 'A');
             break;
         case 'c':
             clocks_menu();
