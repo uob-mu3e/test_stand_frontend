d60589518fcd98cadc0c65dbd0af4fb4782de4ba
Alexandr Kozlinskiy <akozlins@gmail.com>
2020-03-25 11:16:43 +0100

    [util] add mode option to add_altera_xcvr_native_a10
    - support basic_std and basic_enh

--- common/firmware/util/altera_ip.tcl
+++ common/firmware/util/altera_ip.tcl
@@ -329,13 +329,17 @@ proc ::add_altera_xcvr_fpll_a10 { refclk_frequency output_clock_frequency args }
 # ::add_altera_xcvr_native_a10 --
 proc ::add_altera_xcvr_native_a10 { channels channel_width cdr_refclk_freq data_rate args } {
     set name xcvr_native_a10_0
+    set mode basic_std
     for { set i 0 } { $i < [ llength $args ] } { incr i } {
         switch -- [ lindex $args $i ] {
             -name { incr i
                 set name [ lindex $args $i ]
             }
+            -mode { incr i
+                set mode [ lindex $args $i ]
+            }
             default {
-                send_message "Error" "\[add_altera_xcvr_native_a10\] invalid argument '[ lindex $args $i ]'"
+                send_message "Error" "\[[ lindex [ info level 0 ] 0 ]\] invalid argument '[ lindex $args $i ]'"
             }
         }
     }
@@ -346,36 +350,51 @@ proc ::add_altera_xcvr_native_a10 { channels channel_width cdr_refclk_freq data_
     add_instance ${name} altera_xcvr_native_a10
     set_instance_parameter_value ${name} {design_environment} {NATIVE}
 
-    if { ${channel_width} == 8 } {
-        set_instance_parameter_value ${name} {std_pcs_pma_width} {10}
-    } \
-    else {
-        set_instance_parameter_value ${name} {std_pcs_pma_width} {20}
-    }
-
-    if { ${channel_width} == 32 } {
-        set_instance_parameter_value ${name} {std_tx_byte_ser_mode} {Serialize x2}
-        set_instance_parameter_value ${name} {std_rx_byte_deser_mode} {Deserialize x2}
-    }
-
     set_instance_parameter_value ${name} {support_mode} {user_mode}
-    set_instance_parameter_value ${name} {protocol_mode} {basic_std}
+
+    if { [ string equal $mode basic_std ] } {
+        if { ${channel_width} == 8 } {
+            set_instance_parameter_value ${name} {std_pcs_pma_width} {10}
+        } \
+        else {
+            set_instance_parameter_value ${name} {std_pcs_pma_width} {20}
+        }
+        if { ${channel_width} == 32 } {
+            set_instance_parameter_value ${name} {std_tx_byte_ser_mode} {Serialize x2}
+            set_instance_parameter_value ${name} {std_rx_byte_deser_mode} {Deserialize x2}
+        }
+    }
+    if { [ string equal $mode basic_enh ] } {
+        set_instance_parameter_value ${name} {enh_pcs_pma_width} ${channel_width}
+        set_instance_parameter_value ${name} {enh_pld_pcs_width} ${channel_width}
+
+        # gearbox
+        set_instance_parameter_value ${name} {enable_port_rx_enh_bitslip} {1}
+        set_instance_parameter_value ${name} {enh_rx_bitslip_enable} {1}
+    }
+
+    # datapath options
+    set_instance_parameter_value ${name} {protocol_mode} ${mode}
     set_instance_parameter_value ${name} {pma_mode} {basic}
     set_instance_parameter_value ${name} {duplex_mode} {duplex}
     set_instance_parameter_value ${name} {channels} ${channels}
+
     set_instance_parameter_value ${name} {set_data_rate} ${data_rate}
+
     set_instance_parameter_value ${name} {enable_simple_interface} {1}
     set_instance_parameter_value ${name} {enable_split_interface} {0}
 
+    # CDR options
     set_instance_parameter_value ${name} {set_cdr_refclk_freq} ${cdr_refclk_freq}
     set_instance_parameter_value ${name} {rx_ppm_detect_threshold} {1000}
 
+    # PMA ports
     set_instance_parameter_value ${name} {enable_port_rx_is_lockedtodata} {1}
     set_instance_parameter_value ${name} {enable_port_rx_is_lockedtoref} {1}
-
     set_instance_parameter_value ${name} {enable_port_rx_seriallpbken_tx} {1}
     set_instance_parameter_value ${name} {enable_port_rx_seriallpbken} {1}
 
+    # standard PCS
     set_instance_parameter_value ${name} {std_tx_8b10b_enable} {1}
     set_instance_parameter_value ${name} {std_rx_8b10b_enable} {1}
 
@@ -384,6 +403,7 @@ proc ::add_altera_xcvr_native_a10 { channels channel_width cdr_refclk_freq data_
     # word aligner pattern K28.5
     set_instance_parameter_value ${name} {std_rx_word_aligner_pattern} {0x283}
 
+    # dynamic reconfiguration
     set_instance_parameter_value ${name} {rcfg_enable} {1}
     set_instance_parameter_value ${name} {rcfg_shared} {1}
     set_instance_parameter_value ${name} {rcfg_separate_avmm_busy} {1}
