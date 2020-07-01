0abf92bd199a86a4cff18b39d3fd4a762f34e570
marius_koeppel <koeppel.ma@googlemail.com>
2020-07-01 15:58:01 +0200

    update tmux start script

--- online/scifi/setup_odb.sh
+++ online/scifi/setup_odb.sh
@@ -13,18 +13,20 @@ odbedit -c 'mkdir rootana'
 odbedit -d rootana -c 'create STRING cors'
 odbedit -c 'set rootana/cors *'
 
-#create custom pages for DAQ slow control and Clock/Reset system
+# create custom pages for DAQ slow control
 odbedit -d Custom -c 'create STRING SC'
 odbedit -c 'set Custom/SC sc.html'
-odbedit -d Custom -c 'create STRING CR'
-odbedit -c 'set Custom/CR cr.html'
-
-
-#custom pages for SciFi
 odbedit -d Custom -c 'create STRING SciFi-ASICs'
 odbedit -c 'set Custom/SciFi-ASICs mutrigTdc.html'
+#odbedit -d Custom -c 'create STRING MALIBU'
+#odbedit -c 'set Custom/MALIBU TileGUI/MALIBUMonitor/Monitor.html'
+odbedit -d Custom -c 'create STRING MuPix'
+odbedit -c 'set Custom/MuPix MuPixTB.html'
 
+# mutrigana
 odbedit -d Custom -c 'create STRING MutrigAna'
 odbedit -c 'set Custom/MutrigAna ana_mutrig/mutrig/mutrigana.html'
 
-
+# set mhttpd to run http
+odbedit -d Experiment -c 'create BOOL "http redirect to https"'
+odbedit -c 'set "Experiment/http redirect to https" "n"'
