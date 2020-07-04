a7b79124847666512c7cc9d1d579f2874a7a61e0
Niklaus Berger <niberger@uni-mainz.de>
2020-07-04 23:48:02 +0200

    Fix to IP tcl

--- backend_pc/firmware/clock_and_reset_box/ip/ila_0.tcl
+++ backend_pc/firmware/clock_and_reset_box/ip/ila_0.tcl
@@ -7,7 +7,7 @@ create_ip -vlnv xilinx.com:ip:ila:6.2 \
           -module_name $module_name -dir $dir
 
 set_property -dict [ list \
-    {CONFIG.C_DATA_DEPTH} {4096} \		 
+    {CONFIG.C_DATA_DEPTH} {4096} \
     {CONFIG.C_NUM_OF_PROBES} {16} \
     {CONFIG.C_PROBE2_WIDTH} {32} \
     {CONFIG.C_PROBE5_WIDTH} {32} \
