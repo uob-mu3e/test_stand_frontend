c2e7a88804a4b60130251ab56725fed11157e6a8
Martin Mueller <mmarti04@students.uni-mainz.de>
2020-09-08 18:44:37 +0200

    feb v2 compiling in online Q18.0, TODO: test Q18.1, transceiver avalon interface

--- fe_board/firmware/FEB_common/firefly/firefly.vhd
+++ fe_board/firmware/FEB_common/firefly/firefly.vhd
@@ -144,7 +144,7 @@ begin
 --------------------------------------------------
 
 -- 4-channel RX/TX
-    xcvr: fastlink
+    xcvr: entity work.ip_altera_xcvr_native_av
     port map(
         --clocks
         tx_pll_refclk(0)        => i_clk,
@@ -196,7 +196,7 @@ begin
     );
     
 -- 4-channel TX
-    xcvr2: fastlink_small
+    xcvr2: entity work.fastlink_small
     port map(
         --clocks
         tx_pll_refclk(0)        => i_clk,
@@ -256,7 +256,7 @@ begin
 -- reset controller (2)
 --------------------------------------------------
 
-    reset_controller: native_reset
+    reset_controller: entity work.ip_altera_xcvr_reset_control
     port map(
         clock                   => i_sysclk,
         reset                   => not i_reset_n,
@@ -274,7 +274,7 @@ begin
         rx_cal_busy             => rx_cal_busy--,
     );
     
-    reset_controller2: native_reset_tx
+    reset_controller2: entity work.native_reset_tx
     port map(
         clock                   => i_sysclk,
         reset                   => not i_reset_n,
@@ -291,7 +291,7 @@ begin
 -- reconfig controller (2)
 --------------------------------------------------
 
-    reconfig_controller: native_reconfig
+    reconfig_controller: entity work.ip_alt_xcvr_reconfig
     port map(
         reconfig_busy             => open,
         mgmt_clk_clk              => i_sysclk,
