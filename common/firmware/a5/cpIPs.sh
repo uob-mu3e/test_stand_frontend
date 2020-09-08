6cc9c4b5cfa2832a681b1f11643e711922d4954b
Martin Mueller <mmarti04@students.uni-mainz.de>
2020-09-08 17:38:28 +0200

    tclify Arria5 transceiver generation

---
+++ common/firmware/a5/cpIPs.sh
@@ -0,0 +1,4 @@
+# Quartus modifies these files, different for each quartus version, i dont want to track that in git  -- M. Mueller
+cat ip_alt_xcvr_reconfig_template.vhd > ip_alt_xcvr_reconfig.vhd
+cat ip_altera_xcvr_reset_control_template.vhd > ip_altera_xcvr_reset_control.vhd
+cat ip_altera_xcvr_native_av_template.vhd > ip_altera_xcvr_native_av.vhd
---
+++ common/firmware/a5/genIPs.tcl
@@ -0,0 +1,3 @@
+qexec "qmegawiz -silent ip_alt_xcvr_reconfig.vhd"
+qexec "qmegawiz -silent ip_altera_xcvr_native_av.vhd"
+qexec "qmegawiz -silent ip_altera_xcvr_reset_control.vhd" 
