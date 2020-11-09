ccfe731770a66969ed61edbbd6572d8e73653ac9
mu3e_labor <labor@labor.com>
2020-11-09 17:22:05 +0100

    [Tests] add script for jenkins

--- common/tests/vhdl_tests/example_test/run_example_test.sh
+++ common/tests/vhdl_tests/example_test/run_example_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Run this in the install folder"
 cd ../common/tests/vhdl_tests/example_test
-python run.py
-python run.py --export-json example_test.json
+python3 run.py
+python3 run.py --export-json example_test.json
 cd -
 
---
+++ common/tests/vhdl_tests/run_tests.sh
@@ -0,0 +1,3 @@
+cd ../common/tests/vhdl_tests/example_test
+./run_example_test.sh
+cd -
