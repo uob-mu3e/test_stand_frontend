b6bbb8f036dc51a0a0a92b6abd080ead26f43c19
Martin Mueller <mmarti04@students.uni-mainz.de>
2020-11-11 11:57:37 +0100

    bugfix: read-req sc packets need to have a constant length in pcie mem

--- common/libmudaq/mudaq_device.cpp
+++ common/libmudaq/mudaq_device.cpp
@@ -450,7 +450,7 @@ int MudaqDevice::FEB_read(uint32_t FPGA_ID, uint16_t length, uint32_t startaddr)
     write_memory_rw(3, 0x0000009c);
     
     // SC_MAIN_LENGTH_REGISTER_W starts from 1
-    write_register_wait(SC_MAIN_LENGTH_REGISTER_W, 4 + length, 100);
+    write_register_wait(SC_MAIN_LENGTH_REGISTER_W, 2, 100);
     write_register_wait(SC_MAIN_ENABLE_REGISTER_W, 0x1, 100);
     // firmware regs SC_MAIN_ENABLE_REGISTER_W so that it only starts on a 0->1 transition
     write_register_wait(SC_MAIN_ENABLE_REGISTER_W, 0x0, 100);
