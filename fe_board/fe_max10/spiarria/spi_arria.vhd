2bf5d9b00f61751c379032e0a0b1ee2571db1b07
Niklaus Berger <niberger@uni-mainz.de>
2021-02-12 14:15:18 +0100

    Registered inputs to remove instabilities

--- fe_board/fe_max10/spiarria/spi_arria.vhd
+++ fe_board/fe_max10/spiarria/spi_arria.vhd
@@ -39,6 +39,15 @@ architecture RTL of spi_arria is
     signal nibblecount : integer;
     signal addroffset_int : integer;
     signal haveread : std_logic;
+	 
+	 signal csn_reg   : std_logic;
+	 signal clk_reg 	: std_logic;
+	 signal mosi_reg	: std_logic;
+	 signal miso_reg	: std_logic;
+	 signal d1_reg		: std_logic;
+	 signal d2_reg		: std_logic;
+	 signal d3_reg		: std_logic;
+	 
     begin
     
     addroffset <= std_logic_vector(to_unsigned(addroffset_int, addroffset'length));
@@ -56,11 +65,23 @@ architecture RTL of spi_arria is
     byte_en         <= '0';
     next_data       <= '0';
     clklast         <= '0';
+	 clk_reg			  <= '0';
+	 csn_reg			  <= '1';
     elsif(clk100'event and clk100 = '1')then    
         word_en         <= '0';
         byte_en         <= '0';
         next_data       <= '0';
-        clklast         <= i_SPI_clk;
+		  
+		  csn_reg			<= i_SPI_csn;
+		  clk_reg			<= i_SPI_clk;
+		  miso_reg			<= io_SPI_miso;
+		  mosi_reg			<= io_SPI_mosi;
+		  d1_reg				<= io_SPI_D1;
+		  d2_reg				<= io_SPI_D2;
+		  d3_reg				<= io_SPI_D3;		  
+		  
+        clklast         <= clk_reg;
+		  
         case spistate is
         when idle =>
             io_SPI_mosi     <= 'Z';
@@ -69,17 +90,17 @@ architecture RTL of spi_arria is
             io_SPI_D2       <= 'Z';
             io_SPI_D3       <= 'Z';
 				nibblecount 	<=  0;
-            if(i_SPI_csn = '0') then
+            if(csn_reg = '0') then
                 spistate    <= address;
                 nibblecount <= 0;
                 addroffset_int  <= 0;
             end if;
         when address =>
-            if(clklast = '0' and i_SPI_clk = '1') then
-                addrshiftregister(4) <=  io_SPI_mosi;
-                addrshiftregister(5) <=  io_SPI_D1;
-                addrshiftregister(6) <=  io_SPI_D2;
-                addrshiftregister(7) <=  io_SPI_D3;
+            if(clklast = '0' and clk_reg = '1') then
+                addrshiftregister(4) <=  mosi_reg;
+                addrshiftregister(5) <=  d1_reg;
+                addrshiftregister(6) <=  d2_reg;
+                addrshiftregister(7) <=  d3_reg;
                 addrshiftregister(3 downto 0) <= addrshiftregister(7 downto 4);
                 nibblecount     <= nibblecount +1;
             end if;  
@@ -95,17 +116,17 @@ architecture RTL of spi_arria is
                end if;
             end if;
 				
-				if(i_SPI_csn = '1') then
+				if(csn_reg = '1') then
                 spistate <= idle;
             end if;
 				
         when writing =>
             haveread <= '0';
-            if(clklast = '0' and i_SPI_clk = '1') then
-                datashiftregister(28) <=  io_SPI_mosi;
-                datashiftregister(29) <=  io_SPI_D1;
-                datashiftregister(30) <=  io_SPI_D2;
-                datashiftregister(31) <=  io_SPI_D3;
+            if(clklast = '0' and clk_reg = '1') then
+                datashiftregister(28) <=  mosi_reg;
+                datashiftregister(29) <=  d1_reg;
+                datashiftregister(30) <=  d2_reg;
+                datashiftregister(31) <=  d3_reg;
                 datashiftregister(27 downto 0) <= datashiftregister(31 downto 4);
                 nibblecount     <= nibblecount +1;
                 haveread        <= '1';
@@ -122,11 +143,11 @@ architecture RTL of spi_arria is
                 word_en         <= '1';
             end if;
 
-            if(i_SPI_csn = '1') then
+            if(csn_reg = '1') then
                 spistate <= idle;
             end if;
         when waiting =>
-            if(clklast = '0' and i_SPI_clk = '1') then
+            if(clklast = '0' and clk_reg = '1') then
                 nibblecount     <= nibblecount +1;
             end if;   
             if(nibblecount = 1) then
@@ -136,12 +157,12 @@ architecture RTL of spi_arria is
                 next_data         <= '1';
             end if;
             
-            if(i_SPI_csn = '1') then
+            if(csn_reg = '1') then
                 spistate <= idle;
             end if;
         when reading =>
             haveread <= '0';
-            if(clklast = '0' and i_SPI_clk = '1') then
+            if(clklast = '0' and clk_reg = '1') then
                 io_SPI_mosi <= datareadshiftregister(0);
                 io_SPI_D1   <= datareadshiftregister(1);
                 io_SPI_D2   <= datareadshiftregister(2);
@@ -156,7 +177,7 @@ architecture RTL of spi_arria is
                 next_data         <= '1';
             end if;
 
-            if(i_SPI_csn = '1') then
+            if(csn_reg = '1') then
                 spistate <= idle;
             end if;
 
