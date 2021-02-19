1dd07ef898f3f5443d0812ea272ce97a2a2c43df
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-02-19 14:28:45 +0100

    [util] use nios_clk_mhz (nios_freq is deprecated)

--- switching_pc/a10_lhcb/device.tcl
+++ switching_pc/a10_lhcb/device.tcl
@@ -3,7 +3,7 @@
 set_project_property DEVICE_FAMILY {Arria 10}
 set_project_property DEVICE {10AX115S4F45E3SG}
 
-set nios_freq 100000000
+set nios_clk_mhz 100.0
 
 set txrx_data_rate 5000
 set refclk_freq_mhz 125.0
