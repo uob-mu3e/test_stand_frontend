8a1aa72c0857f6d2bd9f57ec3e22eeb06b15d1f7
Niklaus Berger <niberger@uni-mainz.de>
2021-03-04 18:02:55 +0100

    Programming now terminating

--- switching_pc/slowcontrol/MuFEB.cpp
+++ switching_pc/slowcontrol/MuFEB.cpp
@@ -101,6 +101,9 @@ void MuFEB::LoadFirmware(std::string filename, uint16_t FPGA_ID)
     feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,2);
     feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,0);
 
+    feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,1);
+
+
     long pos =0;
     while(pos < fsize){
         uint32_t buffer[256];
@@ -108,31 +111,37 @@ void MuFEB::LoadFirmware(std::string filename, uint16_t FPGA_ID)
         fread(buffer,sizeof(uint32_t),256, f);
         vector<uint32_t> data(buffer, buffer+256);
         feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_DATA_W,data,true);
-        feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,1);
 
         for(int i=0; i < 4; i++){
             feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_ADDR_W,addr);
 
             uint32_t readback = 2;
             uint32_t count = 0;
-            while(readback & 0x2 && count < 200){
-                feb_sc.FEB_register_read(FEB.SB_Port(),PROGRAMMING_STATUS_R,readback);
-                //printf(".");
+            while(readback & 0x2 && count < 1e7){
+                int ec = feb_sc.FEB_register_read(FEB.SB_Port(),PROGRAMMING_STATUS_R,readback);
+                if(ec != FEBSlowcontrolInterface::ERRCODES::OK){
+                    printf("Error reading back!\n");
+                    feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,0);
+                    return;
+                }
                 count++;
                 usleep(100);
             }
             //printf("\n");
-            if(count == 200)
+            if(count == 1e7){
                 printf("Timeout\n");
+                feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,0);
+                return;
+            }
             addr += 256;
         }
         pos  += 256;
         if(pos%4096==0)
             printf("Loaded %f of file\n", (double)pos/fsize);
-        feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,0);
+
     }
 
-
+    feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,0);
 
     feb_sc.FEB_register_write(FEB.SB_Port(),PROGRAMMING_CTRL_W,0);
 
