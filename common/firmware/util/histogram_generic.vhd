e327dc2bbbfaa0c769644cf64e82ffe517d679a0
Sebastian Dittmeier <seb.dittmeier@gmail.com>
2021-03-09 15:42:01 +0100

    adding generic histogram with full rate capabilities, testbench and scripts

--- common/firmware/util/histogram_generic.vhd
+++ common/firmware/util/histogram_generic.vhd
@@ -1,9 +1,11 @@
 -----------------------------------------------------------------------------
--- Generic histogram with a simple dual port ram dual clock
+-- Generic histogram with a true dual port ram dual clock
 -- Date: 08.03.2021
 -- Sebastian Dittmeier, Heidelberg University
 -- dittmeier@physi.uni-heidelberg.de
 --
+-- instantiates 2 histogram_generic_half_rate in order to provide
+-- full rate histogramming at 100% wclk speed
 -----------------------------------------------------------------------------
 
 
@@ -20,120 +22,79 @@ generic(
    port (
       rclk:         in std_logic;   -- clock for read part
       wclk:         in std_logic;   -- clock for write part
-      rst:          in std_logic;   -- all RAM cells are reset to zero, takes a moment
+      rst_n:        in std_logic;   -- reset statemachine
+      zeromem:      in std_logic;   -- clear memories, split this from the reset
       ena:          in std_logic;   -- if set to '1', and histogram is busy_n = '1', updates histogram
       can_overflow: in std_logic;   -- if set to '1', bins can overflow
-      data_in:      in std_logic_vector(DATA_WIDTH-1 downto 0); -- data to be histogrammed
+      data_in:      in std_logic_vector(ADDR_WIDTH-1 downto 0); -- data to be histogrammed, actually refers to a bin, hence addr_width
       valid_in:     in std_logic;
       busy_n:       out std_logic;  -- shows that it is ready to accept data
       
       raddr_in:     in std_logic_vector(ADDR_WIDTH-1 downto 0); -- address for readout
-      q_out:        out std_logic_vector(DATA_WIDTH-1 downto 0) -- data to be readout, appears 1 cycle after readaddr_in is changed
+      q_out:        out std_logic_vector(DATA_WIDTH-1 downto 0) -- data to be readout, appears 1 cycle after raddr_in is set
    );
 end histogram_generic;
 
 
 architecture RTL of histogram_generic is
 
-signal waddr    : std_logic_vector(ADDR_WIDTH-1 downto 0);
-signal wdata    : std_logic_vector(DATA_WIDTH-1 downto 0);
-signal we       : std_logic;
-signal raddr    : std_logic_vector(ADDR_WIDTH-1 downto 0);
-signal q        : std_logic_vector(DATA_WIDTH-1 downto 0);
+type histo_q_array is array (0 to 1) of std_logic_vector(DATA_WIDTH-2 downto 0);
+signal q            : histo_q_array;
+signal valid        : std_logic_vector(1 downto 0);
+signal busy_n_int   : std_logic_vector(1 downto 0);
+signal data         : std_logic_vector(ADDR_WIDTH-1 downto 0); -- one pipeline stage
 
-
-type   state_type is (zeroing, waiting, enabled, readwaiting, writing);
-signal state : state_type;
-
-constant addr_all_one : std_logic_vector(waddr'range) := (others => '1');
-constant data_all_one : std_logic_vector(q'range) := (others => '1');
-
-signal n_raddr      : natural range 0 to 2**ADDR_WIDTH - 1;
-signal n_waddr      : natural range 0 to 2**ADDR_WIDTH - 1;
 
 begin
 
-n_raddr <= to_integer(unsigned(raddr_in));
-n_waddr <= to_integer(unsigned(waddr));
+gen_half_rate_histos:
+for I in 0 to 1 generate
+    i_histo: entity work.histogram_generic_half_rate
+       generic map
+       (
+          DATA_WIDTH => DATA_WIDTH-1,   -- only needs half the size, memory is split into two blocks
+          ADDR_WIDTH => ADDR_WIDTH
+       )
+       port map
+       (
+          rclk          => rclk,
+          wclk          => wclk,
+          rst_n         => rst_n,
+          zeromem       => zeromem,
+          ena           => ena,
+          can_overflow  => can_overflow,
+          data_in       => data,    -- data can be presented to both
+          valid_in      => valid(I),
+          busy_n        => busy_n_int(I),
+          
+          raddr_in      => raddr_in,
+          q_out         => q(I)
+       );
+end generate gen_half_rate_histos;
 
-i_ram: entity work.true_dual_port_ram_dual_clock
-   generic map
-   (
-      DATA_WIDTH => DATA_WIDTH,
-      ADDR_WIDTH => ADDR_WIDTH
-   )
-   port map
-   (
-        clk_a   => rclk,    -- towards the outside, here we only read, never write
-        clk_b   => wclk,    -- within, we actually need to read and write!
-        addr_a  => n_raddr,
-        addr_b  => n_waddr,
-        data_a  => (others => '0'),
-        data_b  => wdata,
-        we_a    => '0',
-        we_b    => we,
-        q_a     => q_out,
-        q_b     => q
-   );
-
+-- q_out is simply the sum of both outputs, we only do it combinatorial, so no more delay is added
+q_out   <= ('0'& q(1)) + ('0'& q(0));
+busy_n  <= busy_n_int(1) or busy_n_int(0); -- if either one of them is not busy we are fine
     -- write state machine
     write_sm: process(wclk)
     begin
-    if(rising_edge(wclk)) then
-        if(rst = '1')then
-            busy_n  <= '0';     -- we are busy after a reset
-            state   <= zeroing; -- and we first clear the RAM
-            we      <= '0';     -- nothing gets written to the RAM
-        else
-            case state is
-                when zeroing => 
-                    we      <= '1';
-                    wdata   <= (others => '0');
-                    -- coming from reset, we is '0', so we use this to write also address 0!
-                    if(we = '0')then
-                        waddr   <= (others => '0'); -- reset waddr to 0
-                    else
-                        waddr   <= waddr + '1';   -- increment
-                    end if;
-                    if(waddr = addr_all_one) then  -- we will increment address to zero again
-                        state   <= waiting;           -- we are done here
-                        we      <= '0';               -- don't need to write zeros again
-                    end if;
-            
-                when waiting =>
-                    busy_n  <= '1';     -- now we can accept data
-                    if (ena = '1')then  -- we are getting the signal to accept data
-                        state   <= enabled;
-                    end if;
-                    
-                when enabled =>
-                    we     <= '0';
-                    if(valid_in = '1')then  -- we get some valid data
-                        waddr    <= data_in; -- we set the address, we want to read data!
-                        state    <= readwaiting;
-                    end if;
-                    if(ena = '0')then       -- go back to waiting mode, no more updates to the histogram
-                        state   <= waiting;   
-                    end if;
-                
-                when readwaiting =>
-                    state   <= writing;
-                
-                when writing => 
-                    we     <= '1';
-                    if(can_overflow = '0' and q = data_all_one)then
-                        wdata   <= q;
-                    else
-                        wdata   <= q + '1'; 
-                    end if;
-                    state   <= enabled;
-                    
-                when others => 
-                    we      <= '0';
-                    state   <= waiting;
-            end case;
+        if(rst_n = '0')then
+            valid   <= "00";    -- disable all writes
+        elsif(rising_edge(wclk)) then
+            data <= data_in;
+            valid   <= "00";    -- default
+            if(valid_in = '1')then  -- in principle we would do a round robin
+                                    -- but half rate histograms can accept to consecutive requests, 
+                                    -- and are less efficient with 1 cycle pause
+                if(busy_n_int(0) = '1')then -- so the default is the first histogram
+                    valid(0) <= '1';
+                elsif(busy_n_int(1) = '1')then
+                    valid(1) <= '1';
+                else
+                    -- error!
+                end if;
+            end if;
         end if;
-    end if;
     end process write_sm;
 
 end RTL;
