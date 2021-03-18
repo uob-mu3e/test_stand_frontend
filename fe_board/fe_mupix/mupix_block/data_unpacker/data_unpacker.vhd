034afbe93d8b646155599eb20d5a72dcaaa32a27
Martin Mueller <mmarti04@students.uni-mainz.de>
2021-03-18 15:18:51 +0100

    remove coarsecounters from mp datastream

--- fe_board/fe_mupix/mupix_block/data_unpacker/data_unpacker.vhd
+++ fe_board/fe_mupix/mupix_block/data_unpacker/data_unpacker.vhd
@@ -55,6 +55,10 @@ architecture RTL of data_unpacker is
 
     signal data_i               : std_logic_vector(31 downto 0) := (others => '0');
 
+    signal input_buffer         : std_logic_vector(31 downto 0);
+    signal input_hit_ena_buffer : std_logic;
+    signal dataWithoutSC        : std_logic_vector(31 downto 0);
+
     signal errorcounter_reg     : std_logic_vector(31 downto 0);
 
     signal link_i               : std_logic_vector(7 downto 0);
@@ -179,7 +183,7 @@ begin
         if reset_n = '0' then
             NS <= IDLE;
 
-            hit_ena             <= '0';
+            input_hit_ena_buffer<= '0';
             coarsecounter       <= (others => '0');
             coarsecounter_ena   <= '0';
             link_flag           <= '0';
@@ -203,24 +207,14 @@ begin
             coarse_reg          <= '0';
             hit_reg             <= '0';
 
-            hit_ena             <= hit_reg;
+            input_hit_ena_buffer<= hit_reg;
             coarsecounter_ena   <= coarse_reg;
             link_flag           <= link_flag_reg;
 
             coarsecounter       <= data_i(7 downto 0) & data_i(31 downto 8); -- gray counter & binary counter
 
             if(hit_reg = '1')then
-                -- The data arrives from MuPix10: TS2[4:0] TS1[10:0] Col[6:0] Row[8:0]
-                    -- TS2 = data_i(31 downto 27)
-                    -- TS1 = data_i(26 downto 16)
-                    -- Col = data_i(15 downto 9)
-                    -- Row = data_i(8 downto 0)
-                -- We present to the outside:
-                ts              <= data_i(26 downto 16);
-                o_chip_ID       <= convert_lvds_to_chip_id(LVDS_ID,chip_ID_mode);
-                row             <= convert_row(data_i(8 downto 0));
-                col             <= convert_col(data_i(15 downto 9),data_i(8 downto 0));
-                ts2             <= data_i(31 downto 27);
+                input_buffer    <= data_i;
             end if;
 
             if(readyin = '0')then
@@ -317,19 +311,29 @@ begin
 
     end process;
 
-    e_mp_sc_rm: mp_sc_removal
+    e_mp_sc_rm: work.mp_sc_removal
     port map(
         i_reset_n               => reset_n,
         i_clk                   => clk, 
         i_sc_active             => '1',
         i_new_block             => link_flag,
-        i_hit                   => hit_out_i,
-        i_hit_ena               => hit_ena_i,
+        i_hit                   => input_buffer,
+        i_hit_ena               => input_hit_ena_buffer,
         i_coarsecounters_ena    => coarsecounter_ena,
-        o_hit                   => hit_out_o,
-        o_hit_ena               => hit_ena_o--,
+        o_hit                   => dataWithoutSC,
+        o_hit_ena               => hit_ena--,
     );
-
+    -- The data arrives from MuPix10: TS2[4:0] TS1[10:0] Col[6:0] Row[8:0]
+                -- TS2 = data_i(31 downto 27)
+                -- TS1 = data_i(26 downto 16)
+                -- Col = data_i(15 downto 9)
+                -- Row = data_i(8 downto 0)
+            -- We remove sc and coarsecounters and present to the hitsorter:
+    ts              <= dataWithoutSC(26 downto 16);
+    o_chip_ID       <= convert_lvds_to_chip_id(LVDS_ID,chip_ID_mode);
+    row             <= convert_row(dataWithoutSC(8 downto 0));
+    col             <= convert_col(dataWithoutSC(15 downto 9),dataWithoutSC(8 downto 0));
+    ts2             <= dataWithoutSC(31 downto 27);
 
     degray_single : work.hit_ts_conversion
     port map(
--- fe_board/fe_mupix/mupix_block/data_unpacker/mp_sc_removal.vhd
+++ fe_board/fe_mupix/mupix_block/data_unpacker/mp_sc_removal.vhd
@@ -5,27 +5,24 @@
 -- 
 -- dittmeier@physi.uni-heidelberg.de
 --
+-- last edit: M.Mueller, March 2021
 ----------------------------------
-
 
 library ieee;
 use ieee.std_logic_1164.all;
-use work.mupix_constants.all;
-	
 
 entity mp_sc_removal is 
-	port (
-		reset_n				: in std_logic;
-		clk					: in std_logic;
-		is_MP10				: in std_logic;
-		sc_active			: in std_logic;
-		new_block			: in std_logic;
-		hit_in				: in std_logic_vector(UNPACKER_HITSIZE-1 DOWNTO 0);
-		hit_ena_in			: in std_logic;
-		coarsecounters_ena: in std_logic;	-- has to be '0' for hits!
-		hit_out				: out std_logic_vector(UNPACKER_HITSIZE-1 DOWNTO 0);
-		hit_ena_out			: out std_logic
-		);
+    port (
+        i_reset_n           : in std_logic;
+        i_clk               : in std_logic;
+        i_sc_active         : in std_logic;
+        i_new_block         : in std_logic;
+        i_hit               : in std_logic_vector(31 DOWNTO 0);
+        i_hit_ena           : in std_logic;
+        i_coarsecounters_ena: in std_logic; -- has to be '0' for hits!
+        o_hit               : out std_logic_vector(31 DOWNTO 0);
+        o_hit_ena           : out std_logic--;
+        );
 end mp_sc_removal;
 
 
@@ -34,55 +31,54 @@ architecture rtl of mp_sc_removal is
 -- it may be only the last two hits of a readout block, that could be SC
 -- so we simply store the last two hits of a readout cycle
 -- we check the following signals:
--- is_MP10: only if = '1' then we take action, otherwise bypass
 -- sc_active: only if = '1' then we take action, otherwise bypass
 -- new_block: indicates, that a readout cylce has passed, then we delete last two hits
 -- we do that by erasing the hin_ena flag
 -- we can use the link_id_flag for that
-	signal hit_reg_1 			: std_logic_vector(UNPACKER_HITSIZE-1 DOWNTO 0);
-	signal hit_reg_2 			: std_logic_vector(UNPACKER_HITSIZE-1 DOWNTO 0);
-	signal hit_reg_out		: std_logic_vector(UNPACKER_HITSIZE-1 DOWNTO 0);
-	signal hit_ena_reg_1		: std_logic;
-	signal hit_ena_reg_2		: std_logic;
-	signal hit_ena_reg_out 	: std_logic;
+    signal hit_reg_1        : std_logic_vector(31 DOWNTO 0);
+    signal hit_reg_2        : std_logic_vector(31 DOWNTO 0);
+    signal hit_reg_out      : std_logic_vector(31 DOWNTO 0);
+    signal hit_ena_reg_1    : std_logic;
+    signal hit_ena_reg_2    : std_logic;
+    signal hit_ena_reg_out  : std_logic;
 
 
 begin
 
-	-- we have to blindly forward coarsecounter data on hit lines
-	hit_out 		<= 	hit_in				when coarsecounters_ena = '1' else
-							hit_reg_out 		when (is_MP10 = '1' and sc_active = '1') else
-							hit_in;
-	
-	-- we have to blindly forward coarsecounters_ena signal
-	hit_ena_out <= 	hit_ena_in			when coarsecounters_ena = '1' else	
-							hit_ena_reg_out 	when (is_MP10 = '1' and sc_active = '1') else
-							hit_ena_in;
+    -- we do not forward coarsecounter data on hit lines
+    o_hit               <= (others => '0')  when i_coarsecounters_ena = '1' else
+                            hit_reg_out when (i_sc_active = '1') else
+                            i_hit;
 
-	process(reset_n, clk)
-	begin
-	if(reset_n = '0')then
-		hit_reg_1 		<= (others => '0');
-		hit_reg_2 		<= (others => '0');
-		hit_reg_out		<= (others => '0');
-		hit_ena_reg_1 	<= '0';
-		hit_ena_reg_2 	<= '0';
-		hit_ena_reg_out	<= '0';
-	elsif(rising_edge(clk))then
-		hit_ena_reg_out	<= '0';	-- defaults to low
-		if(new_block = '1')then
-			hit_ena_reg_1 <= '0';	-- we delete the stored flags
-			hit_ena_reg_2 <= '0';	-- we delete the stored flags
-		elsif(hit_ena_in = '1' and coarsecounters_ena = '0')then	-- here we deal with hits
-			hit_reg_1		<= hit_in;
-			hit_reg_2		<= hit_reg_1;
-			hit_reg_out		<= hit_reg_2;		-- SC data would actually pop out as hits, but the enable signals are deleted
-				
-			hit_ena_reg_1	<= hit_ena_in;
-			hit_ena_reg_2	<= hit_ena_reg_1;
-			hit_ena_reg_out<= hit_ena_reg_2;	
-		end if;
-	end if;
-	end process;
+    -- we do not forward coarsecounters_ena signal
+    o_hit_ena           <= '0' when i_coarsecounters_ena = '1' else
+                            hit_ena_reg_out when (i_sc_active = '1') else
+                            i_hit_ena;
+
+    process(i_reset_n, i_clk)
+    begin
+    if(i_reset_n = '0')then
+        hit_reg_1       <= (others => '0');
+        hit_reg_2       <= (others => '0');
+        hit_reg_out     <= (others => '0');
+        hit_ena_reg_1   <= '0';
+        hit_ena_reg_2   <= '0';
+        hit_ena_reg_out <= '0';
+    elsif(rising_edge(i_clk))then
+        hit_ena_reg_out <= '0';	-- defaults to low
+        if(i_new_block = '1')then
+            hit_ena_reg_1 <= '0'; -- we delete the stored flags
+            hit_ena_reg_2 <= '0'; -- we delete the stored flags
+        elsif(i_hit_ena = '1' and i_coarsecounters_ena = '0')then -- here we deal with hits
+            hit_reg_1       <= i_hit;
+            hit_reg_2       <= hit_reg_1;
+            hit_reg_out     <= hit_reg_2; -- SC data would actually pop out as hits, but the enable signals are deleted
+
+            hit_ena_reg_1   <= i_hit_ena;
+            hit_ena_reg_2   <= hit_ena_reg_1;
+            hit_ena_reg_out <= hit_ena_reg_2;
+        end if;
+    end if;
+    end process;
 
 end rtl;
