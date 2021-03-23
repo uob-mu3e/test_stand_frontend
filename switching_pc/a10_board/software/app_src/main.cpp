0cce3949500c0bc93a01c8b4fbaf74c4111b2053
Martin Mueller <mmarti04@students.uni-mainz.de>
2021-03-23 17:25:17 +0100

    fix xcvr

--- switching_pc/a10_board/software/app_src/main.cpp
+++ switching_pc/a10_board/software/app_src/main.cpp
@@ -68,7 +68,7 @@ int main() {
             flash.menu();
             break;
         case '2':
-            menu_xcvr((alt_u32*)((AVM_XCVR1_BASE + 0x00000) | ALT_CPU_DCACHE_BYPASS_MASK), 'A');
+            menu_xcvr((alt_u32*)((AVM_XCVR0_BASE + 0x00000) | ALT_CPU_DCACHE_BYPASS_MASK), 'A');
             break;
         case '3':
             menu_xcvr((alt_u32*)((AVM_XCVR1_BASE + 0x10000) | ALT_CPU_DCACHE_BYPASS_MASK), 'B');
