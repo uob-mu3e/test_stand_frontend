54b363146b655170d73f843b9bd98a48be75df1c
Teferi <teferi@ethz.ch>
2021-04-29 09:34:51 +0200

    Set MUTRIG_CONFIG_LEN_BITS to 2719 and added test patterns for configuration

--- fe_board/fe_v2_scifi/software/builtin_config/mutrig2_config.h
+++ fe_board/fe_v2_scifi/software/builtin_config/mutrig2_config.h
@@ -2,7 +2,7 @@
 //Standard configuration patterns and length definition
 //for *mutrig2* ASIC
 const uint32_t MUTRIG_CONFIG_LEN_BYTES=340;
-const uint32_t MUTRIG_CONFIG_LEN_BITS =2720;
+const uint32_t MUTRIG_CONFIG_LEN_BITS =2719;
 
 //configuration "mutrig_config_ALL_OFF"
 #include "mutrig2/ALL_OFF.h"
