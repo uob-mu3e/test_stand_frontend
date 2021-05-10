634082b74c62b329e4c009853792eddab171a73f
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-05-10 13:44:59 +0200

    [max10] fix nios ram init

--- fe_board/fe_max10/nios_ufm.tcl
+++ fe_board/fe_max10/nios_ufm.tcl
@@ -3,6 +3,8 @@
 add_instance flash altera_onchip_flash
 set_instance_parameter_value flash {CLOCK_FREQUENCY} [ expr $nios_freq / 1000000 ]
 set_instance_parameter_value flash {DATA_INTERFACE} {Parallel}
+set_instance_parameter_value flash {SECTOR_ACCESS_MODE} {Read\ and\ write Read\ and\ write Read\ only Read\ only Read\ only}
+set_instance_parameter_value flash {CONFIGURATION_MODE} {Single Uncompressed Image with Memory Initialization}
 
 nios_base.connect flash clk nreset data 0x00000000
 nios_base.connect flash ""    ""   csr 0x700F00F0
