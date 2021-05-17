3cdd088e4ece0c217a353168edd4aea8170007b3
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-05-17 21:41:45 +0200

    [a10_farm] fix device.tcl and Makefile

--- farm_pc/firmware/device.tcl
+++ farm_pc/firmware/device.tcl
@@ -3,6 +3,14 @@
 set_project_property DEVICE_FAMILY {Arria 10}
 set_project_property DEVICE {10AX115N2F45E1SG}
 
-set nios_clk_mhz 125.0
-set txrx_data_rate 6250
-set refclk_freq_mhz 125.0
+set nios_clk_mhz 50.0
+
+set xcvr_clk_mhz 125.0
+set xcvr_refclk_mhz 125.0
+set xcvr_rate_mbps 6250
+set xcvr_channels 4
+
+set xcvr_enh_clk_mhz 125.0
+set xcvr_enh_refclk_mhz 125.0
+set xcvr_enh_rate_mbps 10000
+set xcvr_enh_channels 4
