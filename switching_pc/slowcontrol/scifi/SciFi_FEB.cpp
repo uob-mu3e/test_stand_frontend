e994a7b41f99c57a2abb62922a2f0de90456e311
Lukas Gerritzen <gerritzen@phys.ethz.ch>
2021-05-17 14:35:56 +0200

    Small edits on SW slow control

--- switching_pc/slowcontrol/scifi/SciFi_FEB.cpp
+++ switching_pc/slowcontrol/scifi/SciFi_FEB.cpp
@@ -15,5 +15,5 @@ Contents:       Class to alter settings on a SciFi-FE-FPGA. Derives from MutrigF
 using midas::odb;
 
 //Mapping to physical ports of switching board.
-uint16_t SciFiFEB::FPGAid_from_ID(int asic) const {return asic/8;}
-uint16_t SciFiFEB::ASICid_from_ID(int asic) const {return asic%8;}
+uint16_t SciFiFEB::FPGAid_from_ID(int asic) const {return asic/GetASICSPerModule() + 0;}
+uint16_t SciFiFEB::ASICid_from_ID(int asic) const {return asic%GetASICSPerModule() + 0;}
