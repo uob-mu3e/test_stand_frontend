8707683be6059250ec36ce4c21aeb75976302851
Alexandr Kozlinskiy <akozlins@gmail.com>
2021-06-11 12:01:20 +0200

    [a10_board] revert/restore flash1616

--- switching_pc/a10_board/nios.tcl
+++ switching_pc/a10_board/nios.tcl
@@ -6,10 +6,7 @@ source {util/nios_base.tcl}
 set_instance_parameter_value ram {memorySize} {0x00080000}
 set_instance_parameter_value spi numberOfSlaves 32
 
-# add dummy flash_tcm_* ports
-add_instance flash a10_flash1616
-add_interface flash conduit end
-set_interface_property flash EXPORT_OF flash.out
+source {util/a10_flash1616.tcl}
 
 nios_base.export_avm avm_xcvr0 18 0x70100000
 nios_base.export_avm avm_xcvr1 18 0x70200000
