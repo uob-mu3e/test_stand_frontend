0a304e17478db346612b4bbf527e4fdcb7d3891e
Niklaus Berger <niberger@uni-mainz.de>
2021-06-14 11:58:54 +0200

    Sorter counters now displayed

--- mhttpd/custom/daqcounters.js
+++ mhttpd/custom/daqcounters.js
@@ -1,3 +1,4 @@
+
 function generateTableHead(table, febname){
     let thead = table.createTHead();
     let row1 = thead.insertRow();
@@ -16,7 +17,7 @@ function generateTableHead(table, febname){
     }
 }
 
-function generateTable(table){
+function generateTable(table, counters, fractions, inside){
 
     let row1 = table.insertRow();
     let cell1 = row1.insertCell();
@@ -30,6 +31,7 @@ function generateTable(table){
         let cell = row2.insertCell();
         let text = document.createTextNode(345678+17*i);
         cell.appendChild(text);
+        counters.push(text);
     }
 
     let row3 = table.insertRow();
@@ -44,6 +46,7 @@ function generateTable(table){
         let cell = row4.insertCell();
         let text = document.createTextNode(1234+665*i);
         cell.appendChild(text);
+        counters.push(text);
     }
 
     let row5 = table.insertRow();
@@ -58,6 +61,7 @@ function generateTable(table){
         let cell = row6.insertCell();
         let text = document.createTextNode(0.999);
         cell.appendChild(text);
+        fractions.push(text);
     }
 
     let row7 = table.insertRow();
@@ -72,6 +76,7 @@ function generateTable(table){
         let cell = row8.insertCell();
         let text = document.createTextNode(456);
         cell.appendChild(text);
+        counters.push(text);
     }
 
     let row9 = table.insertRow();
@@ -93,6 +98,7 @@ function generateTable(table){
     cell94.setAttribute("colspan","2");
     let text94 = document.createTextNode("123456");
     cell94.appendChild(text94);
+    counters.push(text94);
 
     let cell95 = row9.insertCell();
     cell95.setAttribute("colspan","2");
@@ -103,6 +109,7 @@ function generateTable(table){
     cell96.setAttribute("colspan","2");
     let text96 = document.createTextNode("723456");
     cell96.appendChild(text96);
+    inside.push(text96);
 
     let cell97 = row9.insertCell();
     //cell97.setAttribute("colspan","2");
@@ -112,22 +119,72 @@ function generateTable(table){
     let cell98 = row9.insertCell();
     let text98 = document.createTextNode("123");
     cell98.appendChild(text98);
-
+    counters.push(text98);
 
 }
 
-let table = document.getElementById("febtable");
-generateTable(table);
-generateTableHead(table, "FEB 1");
+var counters =[];
+var fractions =[];
+var inside=[];
 
+function init(){
+   let main =  document.getElementById("mmain");
+    // Loop over pixel FEBs
+    for(var i=0; i < 10; i++){
+        let div =  document.createElement("div");
+        div.setAttribute("style","float:left");
+        main.appendChild(div);
 
+        counters.push([]);
+        fractions.push([]);
+        inside.push([]);
 
+        let tab = document.createElement("table");
+        tab.setAttribute("class","mtable");
+        tab.setAttribute("style","margin-left: 0.5em");
+        generateTable(tab, counters[i], fractions[i], inside[i]);
+        generateTableHead(tab, "FEB "+i);
 
-function update_slots(valuex){
+        div.appendChild(tab);
+    }
+
+    mjsonrpc_db_get_values(["/Equipment/Switching/Variables/SCSO"]).then(function(rpc) {
+        update_scso(rpc.result.data[0]);
+     }).catch(function(error) {
+        mjsonrpc_error_alert(error);
+     });
+}
+
+init();
+
+function update_scso(valuex){
     var value = valuex;
     if(typeof valuex === 'string')
         value = JSON.parse(valuex);
 
 
+
+    for(var feb=0; feb < 10; feb++){
+        var sum = 0;
+        for(var input=0; input <12; input++){
+            counters[feb][input].nodeValue=value[feb*39 + input + 1];
+            sum += value[feb*39 + input + 1];
+            counters[feb][12+input].nodeValue = value[feb*39 +12 + input + 1];
+            counters[feb][24+input].nodeValue = value[feb*39 +24 + input + 1];
+            sum -= value[feb*39 +24 + input + 1];
+            counters[feb][36].nodeValue = value[feb*39 + 37];
+            sum -= value[feb*39 + 37];
+            counters[feb][37].nodeValue = value[feb*39 + 38];
+
+            var frac = value[feb*39 + input + 1] / (value[feb*39 + input + 1] + value[feb*39 +12 + input + 1]);
+            fractions[feb][input].nodeValue = frac.toFixed(4);
+            if(frac > 0.99)
+               fractions[feb][input].parentNode.style.color = "green";
+            else
+               fractions[feb][input].parentNode.style.color = "red";
+        }
+        inside[feb][0].nodeValue = sum;
+
+    }
 }
 
