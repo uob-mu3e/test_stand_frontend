34c0e6335475e57e2288786ce1f5fa3af1fc78bb
lvigani <vigani@physi.uni-heidelberg.de>
2021-06-22 18:10:20 +0200

    Now submaster is read from ODB. Only if it does not exists, it will be set to the hard coded value. If you want to change it, just do it in the odb and restart the executable.

--- backend_pc/midas_fe/environment_control/environment_control.cpp
+++ backend_pc/midas_fe/environment_control/environment_control.cpp
@@ -127,10 +127,25 @@ void mscb_define(const char *submaster,const char *equipment,const char *devname
     cm_get_experiment_database(&hDB, NULL);
     
     if (submaster && submaster[0]) {
+        char dev_name[500];
+        int size_dev_name = sizeof(dev_name);
+
         sprintf(str, "/Equipment/%s/Settings/Devices/%s/Device", equipment, devname);
-        db_set_value(hDB, 0, str, submaster, 32, 1, TID_STRING);
+        int has_subname = db_get_value(hDB, 0, str, dev_name, &size_dev_name, TID_STRING, TRUE);
+
+        if (has_subname != DB_SUCCESS) { //Variable not set, first time...
+            sprintf(str, "/Equipment/%s/Settings/Devices/%s/Device", equipment, devname);
+            db_set_value(hDB, 0, str, submaster, 32, 1, TID_STRING);
+
+        }
+
         sprintf(str, "/Equipment/%s/Settings/Devices/%s/Pwd", equipment, devname);
-        db_set_value(hDB, 0, str, "meg", 32, 1, TID_STRING);
+        int has_subpwd = db_get_value(hDB, 0, str, dev_name, &size_dev_name, TID_STRING, TRUE);
+
+        if (has_subpwd != DB_SUCCESS) { //Variable not set, first time...
+            sprintf(str, "/Equipment/%s/Settings/Devices/%s/Pwd", equipment, devname);
+            db_set_value(hDB, 0, str, "mu3e", 32, 1, TID_STRING);
+        }
     }
     
     /* find device in device driver */
