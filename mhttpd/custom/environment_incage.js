30956be250386c2a0806a8b73a4b5014aece7e40
lvigani <vigani@physi.uni-heidelberg.de>
2021-06-27 21:56:42 +0200

    Alarms and history set

--- mhttpd/custom/environment_incage.js
+++ mhttpd/custom/environment_incage.js
@@ -1,35 +1,35 @@
 var scheme = {
-    0: { "Env_US1 - Water": "undefined" },
-    1: { "Env_US1 - PCMini52_RH": "undefined" },
-    2: { "Env_US1 - PCMini52_T": "undefined" },
-    3: { "Env_US2 - O2-7-top": "undefined" },
-    4: { "Env_US2 - HIH4040_top": "undefined" },
-    5: { "Env_US2 - LM35_Fibre": "undefined" },
-    6: { "Env_US2 - LM35_FEC1": "undefined" },
-    7: { "Env_US2 - LM35_top": "undefined" },
+    0: {"Env_US1 - Water": "Water"} ,
+    1: {"Env_US1 - PCMini52_RH": "PCMini52RH"} ,
+    2: {"Env_US1 - PCMini52_T": "PCMini52T" } ,
+    3: {"Env_US2 - O2-7-top": "O2" } ,
+    4: {"Env_US2 - HIH4040_top": "Honeywell" } ,
+    5: {"Env_US2 - LM35_Fibre": "LM35" } ,
+    6: {"Env_US2 - LM35_FEC1": "LM35" } ,
+    7: {"Env_US2 - LM35_top": "LM35" } ,
 
-    9: { "Env_DS1 - Water": "undefined" },
-    11: { "Env_DS2 - O2-4-top": "undefined" },
-    12: { "Env_DS2 - HIH4040_top": "undefined" },
-    13: { "Env_DS2 - LM35_Fibre": "undefined" },
-    14: { "Env_DS2 - LM35_FEC1": "undefined" },
-    15: { "Env_DS2 - LM35_top": "undefined" },
+    9: { "Env_DS1 - Water": "Water" } ,
+    11: { "Env_DS2 - O2-4-top": "O2" } ,
+    12: {"Env_DS2 - HIH4040_top": "Honeywell" } ,
+    13: {"Env_DS2 - LM35_Fibre": "LM35" } ,
+    14: {"Env_DS2 - LM35_FEC1": "LM35" } ,
+    15: {"Env_DS2 - LM35_top": "LM35" } ,
 
-    18: { "Env_DS3 - O2-11-bottom": "undefined" },
-    19: { "Env_US3 - O2-6-central": "undefined" },
-    20: { "Env_US3 - HIH4040_bottom": "undefined" },
-    21: { "Env_US3 - HIH4040_central": "undefined" },
-    23: { "Env_US6 - O2_3": "undefined" },
-    24: { "Env_US6 - LM35_3": "undefined" },
-    25: { "Env_US6 - HIH_3": "undefined" },
+    18: { "Env_DS3 - O2-11-bottom": "O2" } ,
+    19: { "Env_US3 - O2-6-central": "O2" } ,
+    20: {"Env_US3 - HIH4040_bottom": "Honeywell" } ,
+    21: {"Env_US3 - HIH4040_central": "Honeywell" } ,
+    //{23: {"Env_US6 - O2_3": "undefined" },
+    //{24: {"Env_US6 - LM35_3": "undefined" },
+    //{25: {"Env_US6 - HIH_3": "undefined" },
 
-    27: { "Env_DS3 - O2-11-bottom": "undefined" },
-    28: { "Env_DS3 - O2-6-central": "undefined" },
-    29: { "Env_DS3 - HIH4040_bottom": "undefined" },
-    30: { "Env_DS3 - HIH4040_central": "undefined" },
-    32: { "Env_DS6 - LM35_6": "undefined" },
-    33: { "Env_DS6 - HIH_6": "undefined" },
-    34: { "Env_DS6 - O2_6": "undefined" }
+    27: { "Env_DS3 - O2-11-bottom": "O2" } ,
+    28: { "Env_DS3 - O2-6-central": "O2" } ,
+    29: {"Env_DS3 - HIH4040_bottom": "Honeywell" } ,
+    30: {"Env_DS3 - HIH4040_central": "Honeywell" }
+    //{32: {"Env_DS6 - LM35_6": "undefined" },
+    //{33: {"Env_DS6 - HIH_6": "undefined" },
+    //{34: {"Env_DS6 - O2_6": "undefined" }}
 }
 
 var table = document.getElementById("env_tab")
@@ -41,6 +41,7 @@ for (var key in scheme) {
     var cell2 = row1.insertCell(1);
     var cell3 = row1.insertCell(2);
     var cell4 = row1.insertCell(3);
+    var cell5 = row1.insertCell(4);
     for (var keya in scheme[key]) {
         cell2.textContent = keya
 
@@ -51,5 +52,27 @@ for (var key in scheme) {
         cell4.classList.add("modbvalue")
         var link2 = "/Equipment/Environment/Converted/" + keya
         cell4.setAttribute("data-odb-path", link2)
+        if (scheme[key][keya] == "Honeywell" || scheme[key][keya] == "PCMini52RH")
+            cell5.textContent = "%RH"
+        else if (scheme[key][keya] == "LM35" || scheme[key][keya] == "PCMini52T")
+            cell5.textContent = "C"
+        else if (scheme[key][keya] == "O2")
+            cell5.textContent = "%"
+        else if (scheme[key][keya] == "Water")
+            cell5.textContent = "(bool)"
     }
 }
+
+var row2 = table.insertRow(-1);
+var cell21 = row2.insertCell(0);
+cell21.textContent = "-"
+var cell22 = row2.insertCell(1);
+var cell23 = row2.insertCell(2);
+var cell24 = row2.insertCell(3);
+var cell25 = row2.insertCell(4);
+cell22.textContent = "Dew Point"
+cell23.textContent = "-"
+cell24.classList.add("modbvalue")
+var link22 = "/Equipment/Environment/Converted/Computed/DewPoint"
+cell24.setAttribute("data-odb-path", link22)
+cell25.textContent = "C"
