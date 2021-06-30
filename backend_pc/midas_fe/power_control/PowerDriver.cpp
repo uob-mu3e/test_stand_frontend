06c8be9224955ffdfd19f1575db66c6e9e8f4f6f
mu3eSwitch0 <mu3e@mu3e.com>
2021-06-30 21:39:20 +0200

    add sleep in readloop

--- backend_pc/midas_fe/power_control/PowerDriver.cpp
+++ backend_pc/midas_fe/power_control/PowerDriver.cpp
@@ -74,8 +74,9 @@ void PowerDriver::ReadLoop()
 {
     while(!stop){
         if(!read){
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         } else {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
             readstatus = ReadAll();
             read = 0;
         }
