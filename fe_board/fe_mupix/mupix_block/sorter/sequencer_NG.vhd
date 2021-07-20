02110f7b241e729c7c07a70549b40fc809583881
Niklaus Berger <nberger@physi.uni-heidelberg.de>
2021-07-20 17:40:37 +0200

    New reading scheme

--- fe_board/fe_mupix/mupix_block/sorter/sequencer_NG.vhd
+++ fe_board/fe_mupix/mupix_block/sorter/sequencer_NG.vhd
@@ -40,125 +40,108 @@ type output_type is(none, header1, header2, subheader, hits, footer);
 signal output: output_type;
 signal current_block:	block_t;
 constant block_max:		block_t := (others => '1');
+constant block_zero:	block_t := (others => '0');
 signal current_ts:		ts_t;
 signal counters_reg: std_logic_vector(MEMCOUNTERRANGE);
 signal subaddr:			counter_t;
-signal dohits: std_logic;
-signal from_fifo_reg	: sorterfifodata_t;
-signal read_fifo_int: std_logic;
-signal fifo_reg_valid: std_logic;
-signal fifo_new: std_logic;
-signal fifo_empty_last : std_logic;
-signal read_token :std_logic;
+signal hasmem:			std_logic;
+signal fifo_new: 		std_logic;
+signal read_fifo_int: 	std_logic;
+signal make_header:		std_logic_vector(1 downto 0);
+
 
 begin
 
 read_fifo <= read_fifo_int;
 
 process(reset_n, clk)
-	variable stop_fifo_reading : std_logic;
+	variable copy_fifo : std_logic;
 begin
 if (reset_n = '0') then	
 	running 		<= '0';
 	running_last 	<= '0';
-	output 			<= none;
-	dohits			<= '0';
 	read_fifo_int	<= '0';
-	fifo_reg_valid	<= '0';
+	output 			<= none;
 	fifo_new		<= '0';
-	fifo_empty_last	<= '1';
-	read_token		<= '1';
 elsif (clk'event and clk = '1') then
-	stop_fifo_reading := '0';
+
+	
 	running_last 	<= running;
 	if (running = '0')then
 		if (fifo_empty = '0') then
 			running <= '1';
 		end if;
+	end if;
+
+	copy_fifo	<= '1';
+
+	if(make_header = "11")then
+		output 			<= footer;
+		make_header 	<= "10";
+	elsif(make_header = "10")then
+		output <= header1;
+		make_header 	<= "01";
+	elsif(make_header = "01")then
+		output <= header2;
+		make_header 	<= "00";
 	else
-		if (running_last = '0' and running = '1') then
-			output <= header1;
-		elsif (output = header1) then
-			output <= header2;
-			if (from_fifo(HASMEMBIT) = '1') then
-				stop_fifo_reading := '1';
-			end if;
-		elsif (output = header2) then
-			output <= subheader;
-			current_block <= from_fifo(TSBLOCKINFIFORANGE);
-			if (from_fifo(HASMEMBIT) = '1') then
-				stop_fifo_reading := '1';
-				counters_reg <= from_fifo(MEMCOUNTERRANGE);
-				current_ts	 <= from_fifo(TSINFIFORANGE);
-				fifo_new	 <= '0';
-			end if;
-		elsif (output = footer) then
-			output <= header1;
-			stop_fifo_reading	:= '1';
-		elsif (dohits = '1') then
-			stop_fifo_reading	:= '1';
+		if(blockchange = '1')then
+			output 			<= subheader;
+			blockchange 	<= '0';
+		elsif(hasmem)then
+			output 			<= hits;
+			copy_fifo		<= '0';
 			if(counters_reg(3 downto 0) = "0001") then -- switch chip
 				counters_reg(counters_reg'left-8 downto 0)	 <= counters_reg(counters_reg'left downto 8);
 				counters_reg(counters_reg'left downto counters_reg'left-7)	 <= (others => '0');
 				subaddr					<= "0000";
 			else -- more hits from same chip
-				counters_reg <= counters_reg;
+				counters_reg (from_fifo(3 downto 0) = "0001" and from_fifo(11 downto 8) = "0000"<= counters_reg;
 				counters_reg(3 downto 0) <= counters_reg(3 downto 0) -'1';
 				subaddr					 <= subaddr + "1";
 			end if;
-			if((counters_reg(3 downto 0) = "0010" and counters_reg(11 downto 8) = "0000") or
-				(counters_reg(3 downto 0) = "0001" and counters_reg(11 downto 8) = "0001" and counters_reg(19 downto 16) = "0000")) then
-				dohits <= '0';
-				stop_fifo_reading	:= '0';
-			end if;		
-		elsif (from_fifo(TSBLOCKINFIFORANGE) /= current_block and fifo_new = '1') then
-			output 			<= subheader;
-			current_block 	<= from_fifo(TSBLOCKINFIFORANGE);
-			if(from_fifo(HASMEMBIT) = '1')then -- could get faster here by not stopping the read if there is a single
-				stop_fifo_reading := '1';
-				fifo_new	 	<= '1';
-				counters_reg 	<= from_fifo(MEMCOUNTERRANGE);
-				current_ts	 	<= from_fifo(TSINFIFORANGE);
-			else
-				fifo_new	 	<= '0';
+			if(counters_reg(3 downto 0) = "0001" and counters_reg(11 downto 8) = "0000")then
+				hasmem					<= '0';
+				copy_fifo				<= '1';
 			end if;
-			if(current_block = block_max) then
-				output 			<= footer;
-				stop_fifo_reading := '1';
-				fifo_new	 	<= '1';
-			end if;
-		elsif (from_fifo(HASMEMBIT) = '1' and fifo_new = '1') then
-			output 			<= hits;
-			counters_reg	<= from_fifo(MEMCOUNTERRANGE);
-			current_ts	 	<= from_fifo(TSINFIFORANGE);
-			fifo_new	 	<= '0';
-			subaddr			<= "0000";
-			stop_fifo_reading := '0';
-			dohits			<= '1';
-			if(from_fifo(3 downto 0) = "0001" and from_fifo(11 downto 8) = "0000") then
-				dohits <= '0';
-			end if;
-		elsif (from_fifo(HASMEMBIT) = '0' and fifo_new = '1') then
-			output 			<= none;
-			fifo_new	 	<= '0';
-		else -- fifo empty
-			output 			<= none;
+		else
+			output			<= none;
 		end if;
+	end if;		
+
+	-- When to continue reading the FIFO
+	if(from_fifo(HASMEMBIT) = '0' or fifo_empty = '1' or (from_fifo(3 downto 0) = "0001" and from_fifo(11 downto 8) = "0000"))then
+		read_fifo_int <= '1';
+	else
+		read_fifo_int <= '0';
 	end if;
 
-	read_fifo_int	<= '0';
-	if(running = '1' and (stop_fifo_reading = '0' or read_token = '1'))  then
-		read_fifo_int		<= '1';
-		if(fifo_empty = '0') then
-			read_token 	<= '0';
-		else 
-			read_token 	<= '1';
-		end if;
-	end if;
 
-	if(read_fifo_int = '1' and fifo_empty = '0') then
-		fifo_new	<= '1';
-		read_fifo_int	<= '0';
+	
+	if(read_fifo_int = '1' and fifo_empty = '0')then
+		if(copy_fifo = '1')then
+			fifo_new		<= '0';
+		else
+			fifo_new		<= '1';
+		end if;	
+	elsif(copy_fifo = '1' and fifo_new = '1')then
+		fifo_new		<= '0';
+	end if;	
+
+	if(copy_fifo = '1' and ((read_fifo_int = '1' and fifo_empty = '0') or fifo_new = '1'))then
+		current_block 	<= from_fifo(TSBLOCKINFIFORANGE);
+		current_ts	 	<= from_fifo(TSINFIFORANGE);
+		counters_reg	<= from_fifo(MEMCOUNTERRANGE);
+		hasmem 			<= from_fifo(HASMEMBIT);
+		subaddr			<= "0000";
+		if(from_fifo(TSBLOCKINFIFORANGE) /= current_block)then
+			blockchange <= '1';
+			if(from_fifo(TSBLOCKINFIFORANGE) = block_zero)then
+				make_header <= "1" & running;
+			end if;
+		else
+			blockchange <= '0';
+		end if;
 	end if;
 end if;
 end process;
