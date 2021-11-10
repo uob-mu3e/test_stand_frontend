8f59996389d955e5d02e8e0f7d617f1112c233ce
Martin Mueller <mmarti04@students.uni-mainz.de>
2021-11-10 11:15:02 +0100

    doc for FEB registers

--- common/firmware/registers/feb_sc_registers.vhd
+++ common/firmware/registers/feb_sc_registers.vhd
@@ -18,49 +18,49 @@ package feb_sc_registers is
     --subtype FEB_SC_DATA_SIZE_RANGE  is integer range 512 downto 1;
     --constant FEB_SC_RAM_SIZE : std_logic_vector(3 downto 0) := "1110";
 
-    constant STATUS_REGISTER_R                  :   integer := 16#FC00#;
-    constant GIT_HASH_REGISTER_R                :   integer := 16#FC01#;
-    constant FPGA_TYPE_REGISTER_R               :   integer := 16#FC02#;
-    constant FPGA_ID_REGISTER_RW                :   integer := 16#FC03#;
-    constant CMD_LEN_REGISTER_RW                :   integer := 16#FC04#;
-    constant CMD_OFFSET_REGISTER_RW             :   integer := 16#FC05#;
-    constant RUN_STATE_RESET_BYPASS_REGISTER_RW :   integer := 16#FC06#;
+    constant STATUS_REGISTER_R                  :   integer := 16#FC00#;        -- DOC: not in use | FEB_ALL
+    constant GIT_HASH_REGISTER_R                :   integer := 16#FC01#;        -- DOC: contains the git hash of used firmware | FEB_ALL
+    constant FPGA_TYPE_REGISTER_R               :   integer := 16#FC02#;        -- DOC: contains fpga type 111010: mupix, 111000 : mutrig | FEB_ALL
+    constant FPGA_ID_REGISTER_RW                :   integer := 16#FC03#;        -- DOC: fgpa ID | FEB_ALL
+    constant CMD_LEN_REGISTER_RW                :   integer := 16#FC04#;        -- DOC: length of data to read in a nios rpc call | FEB_ALL
+    constant CMD_OFFSET_REGISTER_RW             :   integer := 16#FC05#;        -- DOC: positon of the data to read in a nios rpc call | FEB_ALL
+    constant RUN_STATE_RESET_BYPASS_REGISTER_RW :   integer := 16#FC06#;        -- DOC: used to bypass the reset system and run without a clock box | FEB_ALL
 
-    subtype  RUN_STATE_RANGE                    is  integer range 31 downto 16;
-    subtype  RESET_BYPASS_RANGE                 is  integer range 7 downto 0;
-    constant RESET_BYPASS_BIT_REQUEST           :   integer := 8;
-    constant RESET_BYPASS_BIT_ENABLE            :   integer := 9;
+    subtype  RUN_STATE_RANGE                    is  integer range 31 downto 16; -- DOC: not in use
+    subtype  RESET_BYPASS_RANGE                 is  integer range 7 downto 0;   -- DOC: not in use
+    constant RESET_BYPASS_BIT_REQUEST           :   integer := 8;               -- DOC: sends a bypass reset command | FEB_ALL
+    constant RESET_BYPASS_BIT_ENABLE            :   integer := 9;               -- DOC: enables the use of the reset bypass | FEB_ALL
 
-    constant RESET_PAYLOAD_REGISTER_RW          :   integer := 16#FC07#;
-    constant RESET_OPTICAL_LINKS_REGISTER_RW    :   integer := 16#FC08#;
-    constant RESET_PHASE_REGISTER_R             :   integer := 16#FC09#;
-    constant MERGER_RATE_REGISTER_R             :   integer := 16#FC0A#;
+    constant RESET_PAYLOAD_REGISTER_RW          :   integer := 16#FC07#;        -- DOC: payload for the reset bypass commands | FEB_ALL
+    constant RESET_OPTICAL_LINKS_REGISTER_RW    :   integer := 16#FC08#;        -- DOC: reset firefly | FEB_ALL
+    constant RESET_PHASE_REGISTER_R             :   integer := 16#FC09#;        -- DOC: phase between reset rx clock and global clk | FEB_ALL
+    constant MERGER_RATE_REGISTER_R             :   integer := 16#FC0A#;        -- DOC: output rate of the data merger | FEB_ALL
 
-    constant ARRIA_TEMP_REGISTER_RW             :   integer := 16#FC10#;
-    constant MAX10_ADC_0_1_REGISTER_R           :   integer := 16#FC11#;
+    constant ARRIA_TEMP_REGISTER_RW             :   integer := 16#FC10#;        -- DOC: ARRIAV internal temp sense | FEB_ALL
+    constant MAX10_ADC_0_1_REGISTER_R           :   integer := 16#FC11#;        -- DOC: MAX10 adc data | FEB_ALL
     constant MAX10_ADC_2_3_REGISTER_R           :   integer := 16#FC12#;
     constant MAX10_ADC_4_5_REGISTER_R           :   integer := 16#FC13#;
     constant MAX10_ADC_6_7_REGISTER_R           :   integer := 16#FC14#;
     constant MAX10_ADC_8_9_REGISTER_R           :   integer := 16#FC15#;
     
-    constant FIREFLY1_TEMP_REGISTER_R           :   integer := 16#FC16#;
-    constant FIREFLY1_VOLT_REGISTER_R           :   integer := 16#FC17#;
-    constant FIREFLY1_RX1_POW_REGISTER_R        :   integer := 16#FC18#;
-    constant FIREFLY1_RX2_POW_REGISTER_R        :   integer := 16#FC19#;
-    constant FIREFLY1_RX3_POW_REGISTER_R        :   integer := 16#FC1A#;
-    constant FIREFLY1_RX4_POW_REGISTER_R        :   integer := 16#FC1B#;
-    constant FIREFLY1_ALARM_REGISTER_R          :   integer := 16#FC1C#;
+    constant FIREFLY1_TEMP_REGISTER_R           :   integer := 16#FC16#;        -- DOC: contains the temp of firefly1 | FEB_ALL
+    constant FIREFLY1_VOLT_REGISTER_R           :   integer := 16#FC17#;        -- DOC: contains the voltage of firefly1 | FEB_ALL
+    constant FIREFLY1_RX1_POW_REGISTER_R        :   integer := 16#FC18#;        -- DOC: received optical power on link 1 | FEB_ALL
+    constant FIREFLY1_RX2_POW_REGISTER_R        :   integer := 16#FC19#;        -- DOC: received optical power on link 2 | FEB_ALL
+    constant FIREFLY1_RX3_POW_REGISTER_R        :   integer := 16#FC1A#;        -- DOC: received optical power on link 3 | FEB_ALL
+    constant FIREFLY1_RX4_POW_REGISTER_R        :   integer := 16#FC1B#;        -- DOC: received optical power on link 4 | FEB_ALL
+    constant FIREFLY1_ALARM_REGISTER_R          :   integer := 16#FC1C#;        -- DOC: firefly1 alarm | FEB_ALL
 
-    constant FIREFLY2_TEMP_REGISTER_R           :   integer := 16#FC1D#;
-    constant FIREFLY2_VOLT_REGISTER_R           :   integer := 16#FC1E#;
-    constant FIREFLY2_RX1_POW_REGISTER_R        :   integer := 16#FC1F#;
-    constant FIREFLY2_RX2_POW_REGISTER_R        :   integer := 16#FC20#;
-    constant FIREFLY2_RX3_POW_REGISTER_R        :   integer := 16#FC21#;
-    constant FIREFLY2_RX4_POW_REGISTER_R        :   integer := 16#FC22#;
-    constant FIREFLY2_ALARM_REGISTER_R          :   integer := 16#FC23#;
+    constant FIREFLY2_TEMP_REGISTER_R           :   integer := 16#FC1D#;        -- DOC: contains the temp of firefly2 | FEB_ALL
+    constant FIREFLY2_VOLT_REGISTER_R           :   integer := 16#FC1E#;        -- DOC: contains the voltage of firefly2 | FEB_ALL
+    constant FIREFLY2_RX1_POW_REGISTER_R        :   integer := 16#FC1F#;        -- DOC: received optical power on link 5 | FEB_ALL
+    constant FIREFLY2_RX2_POW_REGISTER_R        :   integer := 16#FC20#;        -- DOC: received optical power on link 6 | FEB_ALL
+    constant FIREFLY2_RX3_POW_REGISTER_R        :   integer := 16#FC21#;        -- DOC: received optical power on link 7 | FEB_ALL
+    constant FIREFLY2_RX4_POW_REGISTER_R        :   integer := 16#FC22#;        -- DOC: received optical power on link 8 | FEB_ALL
+    constant FIREFLY2_ALARM_REGISTER_R          :   integer := 16#FC23#;        -- DOC: firefly1 alarm | FEB_ALL
 
-    constant NONINCREMENTING_TEST_REGISTER_RW   :   integer := 16#FC24#;
-    constant MAX10_VERSION_REGISTER_R           :   integer := 16#FC25#;
+    constant NONINCREMENTING_TEST_REGISTER_RW   :   integer := 16#FC24#;        -- DOC: testing nonincrementing reads/writes to fifo (not in use) | FEB_ALL
+    constant MAX10_VERSION_REGISTER_R           :   integer := 16#FC25#;        -- DOC: git hash of the max10 firmware | FEB_ALL
     constant MAX10_STATUS_REGISTER_R            :   integer := 16#FC26#;
     constant MAX10_STATUS_BIT_PLL_LOCKED        :   integer := 0;
     constant MAX10_STATUS_BIT_SPI_ARRIA_CLK     :   integer := 1;
--- common/firmware/registers/mupix_registers.vhd
+++ common/firmware/registers/mupix_registers.vhd
@@ -25,98 +25,81 @@ constant MUPIX_LVDS_STATUS_BLOCK_LENGTH     : integer := 36;
 ---- mupix ctrl -------------------------------------------------
 -----------------------------------------------------------------
 
-    constant MP_CTRL_ENABLE_REGISTER_W          :  integer := 16#0400#;
-        constant WR_BIAS_BIT                    :  integer := 0;
-        constant WR_CONF_BIT                    :  integer := 1;
-        constant WR_VDAC_BIT                    :  integer := 2;
-        constant WR_COL_BIT                     :  integer := 3;
-        constant WR_TEST_BIT                    :  integer := 4;
-        constant WR_TDAC_BIT                    :  integer := 5;
-        constant CLEAR_BIAS_FIFO_BIT            :  integer := 6;
-        constant CLEAR_CONF_FIFO_BIT            :  integer := 7;
-        constant CLEAR_VDAC_FIFO_BIT            :  integer := 8;
-        constant CLEAR_COL_FIFO_BIT             :  integer := 9;
-        constant CLEAR_TEST_FIFO_BIT            :  integer := 10;
-        constant CLEAR_TDAC_FIFO_BIT            :  integer := 11;
+    constant MP_CTRL_ENABLE_REGISTER_W          :  integer := 16#0400#; -- DOC: Used to start the mupix configuration | MP_FEB
+        constant WR_BIAS_BIT                    :  integer := 0;        -- DOC: start spi writing of the mupix bias reg (one can start all in parallel)| MP_FEB
+        constant WR_CONF_BIT                    :  integer := 1;        -- DOC: start spi writing of the mupix conf reg | MP_FEB
+        constant WR_VDAC_BIT                    :  integer := 2;        -- DOC: start spi writing of the mupix VDAC reg | MP_FEB
+        constant WR_COL_BIT                     :  integer := 3;        -- DOC: start spi writing of the mupix COL reg | MP_FEB
+        constant WR_TEST_BIT                    :  integer := 4;        -- DOC: start spi writing of the mupix TEST reg | MP_FEB
+        constant WR_TDAC_BIT                    :  integer := 5;        -- DOC: start spi writing of the mupix TDAC reg | MP_FEB
+        constant CLEAR_BIAS_FIFO_BIT            :  integer := 6;        -- DOC: clear Bias fifo in mp_ctrl | MP_FEB
+        constant CLEAR_CONF_FIFO_BIT            :  integer := 7;        -- DOC: clear Bias fifo in mp_ctrl | MP_FEB
+        constant CLEAR_VDAC_FIFO_BIT            :  integer := 8;        -- DOC: clear Bias fifo in mp_ctrl | MP_FEB
+        constant CLEAR_COL_FIFO_BIT             :  integer := 9;        -- DOC: clear Bias fifo in mp_ctrl | MP_FEB
+        constant CLEAR_TEST_FIFO_BIT            :  integer := 10;       -- DOC: clear Bias fifo in mp_ctrl | MP_FEB
+        constant CLEAR_TDAC_FIFO_BIT            :  integer := 11;       -- DOC: clear Bias fifo in mp_ctrl | MP_FEB
 
-    constant MP_CTRL_BIAS_REGISTER_W            :  integer := 16#0401#;
-    constant MP_CTRL_CONF_REGISTER_W            :  integer := 16#0402#;
-    constant MP_CTRL_VDAC_REGISTER_W            :  integer := 16#0403#;
-    constant MP_CTRL_COL_REGISTER_W             :  integer := 16#0404#;
-    constant MP_CTRL_TEST_REGISTER_W            :  integer := 16#0405#;
-    constant MP_CTRL_TDAC_REGISTER_W            :  integer := 16#0406#;
+    constant MP_CTRL_BIAS_REGISTER_W            :  integer := 16#0401#; -- DOC: If you want to write the mupix BIAS reg only, send data here | MP_FEB
+    constant MP_CTRL_CONF_REGISTER_W            :  integer := 16#0402#; -- DOC: If you want to write the mupix CONF reg only, send data here | MP_FEB
+    constant MP_CTRL_VDAC_REGISTER_W            :  integer := 16#0403#; -- DOC: If you want to write the mupix VDAC reg only, send data here | MP_FEB
+    constant MP_CTRL_COL_REGISTER_W             :  integer := 16#0404#; -- DOC: If you want to write the mupix COL reg only, send data here | MP_FEB
+    constant MP_CTRL_TEST_REGISTER_W            :  integer := 16#0405#; -- DOC: If you want to write the mupix TEST reg only, send data here | MP_FEB
+    constant MP_CTRL_TDAC_REGISTER_W            :  integer := 16#0406#; -- DOC: If you want to write the mupix TDAC reg only, send data here | MP_FEB
 
-    constant MP_CTRL_SLOW_DOWN_REGISTER_W       :  integer := 16#0407#;
-    constant MP_CTRL_CHIP_MASK_REGISTER_W       :  integer := 16#0408#;
-    constant MP_CTRL_INVERT_REGISTER_W          :  integer := 16#0409#;
-        constant MP_CTRL_INVERT_29_BIT          :  integer := 0;
-        constant MP_CTRL_INVERT_CSN_BIT         :  integer := 1;
+    constant MP_CTRL_SLOW_DOWN_REGISTER_W       :  integer := 16#0407#; -- DOC: Division factor for the mupix spi clk | MP_FEB
+    constant MP_CTRL_CHIP_MASK_REGISTER_W       :  integer := 16#0408#; -- DOC: MASK for the SPI writing to mupix (one can write in parallel) | MP_FEB
+    constant MP_CTRL_INVERT_REGISTER_W          :  integer := 16#0409#; -- DOC: To be removed, inversions in mupix SPI | MP_FEB
+        constant MP_CTRL_INVERT_29_BIT          :  integer := 0;        -- DOC: inverts oder of the mupix 29 bit spi shift reg | MP_FEB
+        constant MP_CTRL_INVERT_CSN_BIT         :  integer := 1;        -- DOC: inverts mupix spi chip select bit | MP_FEB
 
-    constant MP_CTRL_ALL_REGISTER_W             :  integer := 16#040A#;
-    constant MP_CTRL_SPI_BUSY_REGISTER_R        :  integer := 16#040F#;
+    constant MP_CTRL_ALL_REGISTER_W             :  integer := 16#040A#; -- DOC: Write complete mupix configuration to this address | MP_FEB
+    constant MP_CTRL_SPI_BUSY_REGISTER_R        :  integer := 16#040F#; -- DOC: Indicates if the mupix spi is busy, do not send new data | MP_FEB
 
 -----------------------------------------------------------------
 ---- mupix datapath ---------------------------------------------
 -----------------------------------------------------------------
 
-    constant MP_READOUT_MODE_REGISTER_W         :  integer := 16#0900#;
-        -- if set: TS is inverted
-        constant INVERT_TS_BIT                  :  integer := 0;
-        -- if set: TS2 is inverted
-        constant INVERT_TS2_BIT                 :  integer := 1;
-        -- if set: TS is grey-decoded
-        constant GRAY_TS_BIT                    :  integer := 2;
-        -- if set: TS2 is grey-decoded
-        constant GRAY_TS2_BIT                   :  integer := 3;
-        -- bits to select different chip id numbering modes (layers etc.)
-        -- not in use at the moment
-        subtype  CHIP_ID_MODE_RANGE             is integer range 5 downto 4;
-        -- bits to select different TOT calculation modes
-        -- Default is to send TS2 as TOT
-        subtype  TOT_MODE_RANGE                 is integer range 8 downto 6;
-    constant MP_LVDS_LINK_MASK_REGISTER_W       :  integer := 16#0901#;
-    constant MP_LVDS_LINK_MASK2_REGISTER_W      :  integer := 16#0902#;
-    constant MP_LVDS_DATA_VALID_REGISTER_R      :  integer := 16#0903#;
-    constant MP_LVDS_DATA_VALID2_REGISTER_R     :  integer := 16#0904#;
-    constant MP_DATA_GEN_CONTROL_REGISTER_W     :  integer := 16#0905#;
-        -- hit output probability is 1/(2^(MP_DATA_GEN_HIT_P_RANGE+1)) for each cycle where a hit could be send
-        -- (in 125 MHz minus protocol overhead sorter -> merger)
-        subtype  MP_DATA_GEN_HIT_P_RANGE        is integer range 3 downto 0;
-        -- if set: hit output probability is 1
-        constant MP_DATA_GEN_FULL_STEAM_BIT     :  integer := 4;
-        -- if set: generator seed is the same on all boards else: generator seed depends on ref_addr from backplane
-        constant MP_DATA_GEN_SYNC_BIT           :  integer := 5;
-        -- if set: use hits from generator, datapath is not connected to link
-        constant MP_DATA_GEN_ENGAGE_BIT         :  integer := 16;
-        -- if set: generated hits are inserted after the data_unpacker
-        constant MP_DATA_GEN_SORT_IN_BIT        :  integer := 17;
-        -- if set: enable hit generation (set MP_DATA_GEN_ENGAGE_BIT to actually replace sorter output with these hits)
-        constant MP_DATA_GEN_ENABLE_BIT         :  integer := 31;
-    -- start of lvds status register block, 1 Word for each chip
-    constant MP_LVDS_STATUS_START_REGISTER_W    :  integer := 16#0906#;
-        subtype  MP_LVDS_STATUS_DISP_ERR_RANGE  is integer range 27 downto 0;
-        constant MP_LVDS_STATUS_PLL_LOCKED_BIT  :  integer := 28;
-        subtype  MP_LVDS_STATUS_STATE_RANGE     is integer range 30 downto 29;
-        constant MP_LVDS_STATUS_READY_BIT       :  integer := 31;
-    constant MP_LVDS_INVERT_REGISTER_W          :  integer := 16#0930#;
-    constant MP_SORTER_DELAY_REGISTER_W         :  integer := 16#0931#;
-    -- 40 counters
-    constant MP_SORTER_COUNTER_REGISTER_R       :  integer := 16#0932#;
-    constant MP_DATA_BYPASS_SELECT_REGISTER_W   :  integer := 16#094B#;
-    constant MP_TS_HISTO_SELECT_REGISTER_W      :  integer := 16#094C#;
-        subtype  MP_TS_HISTO_LINK_SELECT_RANGE  is integer range 15 downto 0;
-        subtype  MP_TS_HISTO_N_SAMPLE_RANGE     is integer range 31 downto 16;
-    constant MP_LAST_SORTER_HIT_REGISTER_R      :  integer := 16#094D#;
-    constant MP_SORTER_INJECT_REGISTER_W        :  integer := 16#094E#;
+    constant MP_READOUT_MODE_REGISTER_W         :  integer := 16#0900#; -- DOC: to be removed | MP_FEB
+        constant INVERT_TS_BIT                  :  integer := 0;        -- DOC: if set: TS is inverted | MP_FEB
+        constant INVERT_TS2_BIT                 :  integer := 1;        -- DOC: if set: TS2 is inverted | MP_FEB
+        constant GRAY_TS_BIT                    :  integer := 2;        -- DOC: if set: TS is grey-decoded | MP_FEB
+        constant GRAY_TS2_BIT                   :  integer := 3;        -- DOC: if set: TS2 is grey-decoded | MP_FEB
+        subtype  CHIP_ID_MODE_RANGE             is integer range 5 downto 4; -- DOC: bits to select different chip id numbering modes (not in use) | MP_FEB
+        subtype  TOT_MODE_RANGE                 is integer range 8 downto 6; -- DOC: bits to select different TOT calculation modes (Default is to send TS2 as TOT, not in use) | MP_FEB
+    constant MP_LVDS_LINK_MASK_REGISTER_W       :  integer := 16#0901#; -- DOC: masking of mupix lvds connections | MP_FEB
+    constant MP_LVDS_LINK_MASK2_REGISTER_W      :  integer := 16#0902#; -- DOC: masking of mupix lvds connections | MP_FEB
+    constant MP_LVDS_DATA_VALID_REGISTER_R      :  integer := 16#0903#; -- DOC: indicates if lvds rec from mupix is ready | MP_FEB
+    constant MP_LVDS_DATA_VALID2_REGISTER_R     :  integer := 16#0904#; -- DOC: indicates if lvds rec from mupix is ready | MP_FEB
+    constant MP_DATA_GEN_CONTROL_REGISTER_W     :  integer := 16#0905#; -- DOC: controls the mupix data generator | MP_FEB
+        subtype  MP_DATA_GEN_HIT_P_RANGE        is integer range 3 downto 0; -- DOC: generator hit output probability, 1/(2^(MP_DATA_GEN_HIT_P_RANGE+1)) for each cycle where a hit could be send | MP_FEB
+        constant MP_DATA_GEN_FULL_STEAM_BIT     :  integer := 4;        -- DOC: if set: generator hit output probability is 1 | MP_FEB
+        constant MP_DATA_GEN_SYNC_BIT           :  integer := 5;        -- DOC: if set: generator seed is the same on all boards else: generator seed depends on FPGA_ID | MP_FEB
+        constant MP_DATA_GEN_ENGAGE_BIT         :  integer := 16;       -- DOC: if set: use hits from generator, datapath is not connected to link | MP_FEB
+        constant MP_DATA_GEN_SORT_IN_BIT        :  integer := 17;       -- DOC: if set: generated hits are inserted after the data_unpacker (bevore sorter) | MP_FEB
+        constant MP_DATA_GEN_ENABLE_BIT         :  integer := 31;       -- DOC: if set: data generator generates hits | MP_FEB
+    constant MP_LVDS_STATUS_START_REGISTER_W    :  integer := 16#0906#; -- DOC: start of lvds status register block, 1 Word for each chip from here on| MP_FEB
+        subtype  MP_LVDS_STATUS_DISP_ERR_RANGE  is integer range 27 downto 0; -- DOC: Disparity error counter in each lvds status register | MP_FEB 
+        constant MP_LVDS_STATUS_PLL_LOCKED_BIT  :  integer := 28;             -- DOC: PLL locked bit in each lvds status register | MP_FEB 
+        subtype  MP_LVDS_STATUS_STATE_RANGE     is integer range 30 downto 29;-- DOC: status Bit in each lvds status register | MP_FEB 
+        constant MP_LVDS_STATUS_READY_BIT       :  integer := 31;             -- DOC: if set: this mupix lvds link is locked and ready | MP_FEB 
+    constant MP_LVDS_INVERT_REGISTER_W          :  integer := 16#0930#;       -- DOC: inverting mupix lvds lines | MP_FEB 
+    constant MP_SORTER_DELAY_REGISTER_W         :  integer := 16#0931#;       -- DOC: Minimum round-trip delay from sync reset going off to hit with TS > 0 appearing at sorter input in 8 ns steps | MP_FEB
+    constant MP_SORTER_COUNTER_REGISTER_R       :  integer := 16#0932#;       -- DOC: Hit counters in the sorter, 40 32 bit counters in total. For the inner pixel FEBs: 12 counters with in-time hits per chip, 12 counters with out-of-time hits per chip, 12 counters with overflows per chip, a counter with the number of output hits and the current credit value. The last two counters are currently reserved for future use | MP_FEB
+    constant MP_DATA_BYPASS_SELECT_REGISTER_W   :  integer := 16#095B#;       -- DOC: bypass the mupix soter and put input to_integer(THISREG) directly on optical link (implemented but not connected in top) | MP_FEB
+    constant MP_TS_HISTO_SELECT_REGISTER_W      :  integer := 16#095C#;       -- DOC: not in use
+        subtype  MP_TS_HISTO_LINK_SELECT_RANGE  is integer range 15 downto 0; -- DOC: not in use
+        subtype  MP_TS_HISTO_N_SAMPLE_RANGE     is integer range 31 downto 16;-- DOC: not in use
+    constant MP_LAST_SORTER_HIT_REGISTER_R      :  integer := 16#095D#;       -- DOC: register that contains the last mupix hit of the sorter output | MP_FEB
+    constant MP_SORTER_INJECT_REGISTER_W        :  integer := 16#095E#;       -- DOC: used to inject single hits at the sorter inputs | MP_FEB
         -- select the input of the sorter to inject to
-        subtype MP_SORTER_INJECT_SELECT_RANGE   is integer range 7 downto 4;
+        subtype MP_SORTER_INJECT_SELECT_RANGE   is integer range 7 downto 4;  -- DOC: input of the sorter to inject to | MP_FEB
         -- rising edge on this bit will trigger a single inject of the word MP_SORTER_INJECT_REGISTER_W at sorter input MP_SORTER_INJECT_REGISTER_W(MP_SORTER_INJECT_SELECT_RANGE)
-        constant MP_SORTER_INJECT_ENABLE_BIT    :  integer := 8;
-    constant MP_HIT_ENA_CNT_REGISTER_R          :  integer := 16#094F#;
-    constant MP_HIT_ENA_CNT_SELECT_REGISTER_W   :  integer := 16#0950#;
-    constant MP_HIT_ENA_CNT_SORTER_IN_REGISTER_R :  integer := 16#0951#;
-    constant MP_HIT_ENA_CNT_SORTER_SELECT_REGISTER_W :  integer := 16#0952#;
-    constant MP_HIT_ENA_CNT_SORTER_OUT_REGISTER_R : integer := 16#0953#;
-    constant MP_RESET_LVDS_N_REGISTER_W         :  integer := 16#0954#;
+        constant MP_SORTER_INJECT_ENABLE_BIT    :  integer := 8;              -- DOC: rising_edge: single hit is injected | MP_FEB
+    constant MP_HIT_ENA_CNT_REGISTER_R          :  integer := 16#095F#;       -- DOC: hit enable counter | MP_FEB
+    constant MP_HIT_ENA_CNT_SELECT_REGISTER_W   :  integer := 16#0960#;       -- DOC: register to select the link for hit ena counter | MP_FEB
+    constant MP_HIT_ENA_CNT_SORTER_IN_REGISTER_R :  integer := 16#0961#;      -- DOC: hit enable counter at the sorter input | MP_FEB
+    constant MP_HIT_ENA_CNT_SORTER_SELECT_REGISTER_W :  integer := 16#0962#;  -- DOC: register to select the link for the sorter input hin ena counter | MP_FEB
+    constant MP_HIT_ENA_CNT_SORTER_OUT_REGISTER_R : integer := 16#0963#;      -- DOC: hit counter at sorter output | MP_FEB
+    constant MP_RESET_LVDS_N_REGISTER_W         :  integer := 16#0964#;       -- DOC: reset register for mupix lvds rx | MP_FEB
 
 end package mupix_registers;
