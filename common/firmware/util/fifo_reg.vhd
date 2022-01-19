9275673c198bb0c4502bb67d3274b52f4f230661
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-01-19 09:41:34 +0100

    [util] refactor fifo_reg
    - rename data -> cell
    - add o_rempty/wfull_n ports
    - add full register (full is '1' during reset)

--- common/firmware/util/fifo_reg.vhd
+++ common/firmware/util/fifo_reg.vhd
@@ -20,10 +20,12 @@ port (
     o_rdata     : out   std_logic_vector(g_DATA_WIDTH-1 downto 0);
     i_rack      : in    std_logic;
     o_rempty    : out   std_logic;
+    o_rempty_n  : out   std_logic;
 
     i_wdata     : in    std_logic_vector(g_DATA_WIDTH-1 downto 0);
     i_we        : in    std_logic;
     o_wfull     : out   std_logic;
+    o_wfull_n   : out   std_logic;
 
     i_reset_n   : in    std_logic;
     i_clk       : in    std_logic--;
@@ -32,48 +34,56 @@ end entity;
 
 architecture arch of fifo_reg is
 
-    type data_array_t is array (natural range <>) of std_logic_vector(g_DATA_WIDTH-1 downto 0);
-    signal data : data_array_t(0 to g_N+1) := (others => (others => '-'));
+    type cell_array_t is array (natural range <>) of std_logic_vector(g_DATA_WIDTH-1 downto 0);
+    signal cell : cell_array_t(0 to g_N+1) := (others => (others => '-'));
     signal empty : std_logic_vector(0 to g_N+1) := (0 => '0', others => '1');
+    signal full : std_logic;
 
 begin
 
     -- empty(0) is used when i_rack = '0' and index is 1
     empty(0) <= '0';
-    data(0) <= (others => '-');
-    -- data(g_N+1) is used when i_rack = '1' and index is g_N
+    cell(0) <= (others => '-');
+    -- cell(g_N+1) is used when i_rack = '1' and index is g_N
     empty(g_N+1) <= '1';
-    data(g_N+1) <= (others => '-');
+    cell(g_N+1) <= (others => '-');
 
-    -- data(1) contains readable data
-    o_rdata <= data(1);
+    -- cell(1) contains readable data
+    o_rdata <= cell(1);
     o_rempty <= empty(1);
-    o_wfull <= not empty(g_N);
+    o_rempty_n <= not empty(1);
+    o_wfull <= full;
+    o_wfull_n <= not full;
 
     process(i_clk, i_reset_n)
     begin
     if ( i_reset_n = '0' ) then
         for i in 1 to g_N loop
-            data(i) <= (others => '-');
+            cell(i) <= (others => '-');
             empty(i) <= '1';
         end loop;
+        full <= '1';
     elsif rising_edge(i_clk) then
+        full <= not empty(g_N);
+
         for i in 1 to g_N loop
             if ( i_rack = '1' and empty(1) = '0' ) then
-                -- read from data(1) and shift all data left
-                data(i) <= data(i+1);
+                -- read from cell(1) and shift left
+                cell(i) <= cell(i+1);
                 empty(i) <= empty(i+1);
-                -- read from fifo to first empty data
-                -- NOTE: do not touch last data (o_wfull is '1')
+                -- copy wdata into first empty cell
+                -- NOTE: do not touch last cell (o_wfull is '1')
                 if ( empty(i to i+1) = "01" and i < g_N ) then
-                    data(i) <= i_wdata;
+                    cell(i) <= i_wdata;
                     empty(i) <= not i_we;
                 end if;
+                full <= '0';
             else
-                -- read from fifo to first empty data
+                -- copy wdata into first empty cell
                 if ( empty(i-1 to i) = "01" ) then
-                    data(i) <= i_wdata;
+                    cell(i) <= i_wdata;
                     empty(i) <= not i_we;
+                    full <= work.util.to_std_logic(i = g_N) and i_we;
                 end if;
             end if;
         end loop;
