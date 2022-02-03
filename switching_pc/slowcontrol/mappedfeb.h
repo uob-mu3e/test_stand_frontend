b44d0cd7608fc0d248928e55ca0d274fbb29e0e4
Niklaus Berger <niberger@uni-mainz.de>
2022-02-03 14:33:31 +0100

    Added default constructor needed for dummy use (ease of interface for broadcasts)

--- switching_pc/slowcontrol/mappedfeb.h
+++ switching_pc/slowcontrol/mappedfeb.h
@@ -12,6 +12,8 @@ class mappedFEB{
 public:
     mappedFEB(LinkStatus & _linkstatus, uint16_t ID, uint32_t linkmask, std::string physName, uint16_t _crate, uint16_t _slot, uint32_t _version =20):
         LinkID(ID),mask(linkmask), fullname_link(physName),crate(_crate),slot(_slot),version(_version), linkstatus(_linkstatus){};
+    mappedFEB():
+        LinkID(-1),mask(0), fullname_link("dummy"),crate(-1),slot(-1),version(20), linkstatus(dls){};
     bool IsScEnabled() const {return mask&FEBLINKMASK::SCOn;}
     bool IsDataEnabled() const {return mask&FEBLINKMASK::DataOn;}
     uint16_t GetLinkID() const {return LinkID;}
@@ -32,7 +34,7 @@ protected:
     uint32_t version;
     LinkStatus & linkstatus;
 
-
+    static LinkStatus dls; // ugly and only needed for constructor without arguments, which in turn is needed (as dummy argument) for broadcasts in the FEBSlowcontrol interface
 };
 
 
