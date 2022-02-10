22a620269f7477bd58416ab582454d70ba9c72ce
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-02-10 09:51:06 +0100

    [a10] update W/RREG_N values

--- common/firmware/a10/time_merger_tree_fifo_32_v2.vhd
+++ common/firmware/a10/time_merger_tree_fifo_32_v2.vhd
@@ -194,6 +194,7 @@ begin
             generic map (
                 g_ADDR_WIDTH => 11,
                 g_DATA_WIDTH => 38,
+                g_WREG_N => 1, -- TNS=...
                 g_RREG_N => 1--, -- TNS=-2300
             )
             port map (
@@ -216,7 +217,8 @@ begin
             generic map (
                 g_ADDR_WIDTH => TREE_w,
                 g_DATA_WIDTH => w_width,
-                g_WREG_N => 1--, -- TNS=-8800
+                g_WREG_N => 1, -- TNS=-8800
+                g_RREG_N => 1--, -- TNS=-...
             )
             port map (
                 i_wdata         => data(i),
