9f63e91cd2cd29c7db55ced1c4048757198b97a7
Alexandr Kozlinskiy <akozlins@gmail.com>
2022-02-16 09:47:56 +0100

    [util] sim - get TB name from first .vhd param

--- common/firmware/util/tb/tb_fifo_sc.sh
+++ common/firmware/util/tb/tb_fifo_sc.sh
@@ -1,13 +1,7 @@
-#!/bin/sh
-set -eu
-IFS="$(printf '\n\t')"
-unset CDPATH
-cd "$(dirname -- "$(readlink -e -- "$0")")" || exit 1
-
-entity=$(basename "$0" .sh)
+#!/bin/bash
 
 STOP_TIME_US=1 \
-../sim.sh "$entity" \
+../sim.sh \
+    tb_fifo_sc.vhd \
     ../*.vhd \
-    ../quartus/*.vhd \
-    "$entity.vhd"
+    ../quartus/*.vhd
