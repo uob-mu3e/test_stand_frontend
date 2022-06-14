dbff483364c6b0f3c289019973037a83a1a503f9
Marius Koeppel <koeppel.ma@googlemail.com>
2022-06-14 10:33:25 +0200

    add local stuff

--- switching_pc/slowcontrol/mupix/mupix_FEB.cpp
+++ switching_pc/slowcontrol/mupix/mupix_FEB.cpp
@@ -345,7 +345,7 @@ int MupixFEB::ConfigureTDACs(){
                                 std::vector<uint32_t> tdac_page(PAGESIZE);
                                 // how to do this wihout copy ?
                                 tdac_page = std::vector<uint32_t>(tdac_pages.at(internal_febID).at(chip).begin() + current_page*PAGESIZE, tdac_pages.at(internal_febID).at(chip).begin() + (current_page+1)*PAGESIZE);
-                                feb_sc.FEB_write(feb, MP_CTRL_TDAC_START_REGISTER_W + chip, tdac_page, true, false);
+                                feb_sc.FEB_write(feb, MP_CTRL_TDAC_START_REGISTER_W + pos, tdac_page, true, false);
                                 //printf(" write page, remaining: %i\n", pages_remaining_this_feb);
                                 printf("Writing chip %x page %x pos %x, remaining %i\n", chip, current_page, pos, pages_remaining.at(internal_febID)[chip]);
                                 usleep(100000);
