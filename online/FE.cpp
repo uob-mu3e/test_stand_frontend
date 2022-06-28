e80e68a696500daaccde7fc86b2a94db458e38fa
Nhat Pham <lckdscl+github@protonmail.com>
2022-06-28 15:43:20 +0100

    Removed: unused lines Formatted: with clang-format

--- online/FE.cpp
+++ online/FE.cpp
@@ -2,49 +2,45 @@
 
   Name:         frontend.c
   Created by:   Midas template adapted by Bristol students and A. Loreti
-
   Contents:     Slow control Bristol
-                
+
 
 \********************************************************************/
 
-#include "AS.h"
+#include <dlfcn.h>
+#include <math.h>
+#include <python2.7/Python.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "midas.h"
-#include <dlfcn.h>
 
-#include "mfe.h"
-
-#include "/software/mu3e/midas_installations/midas_v1/mu3eSummer22/packages/midas/include/odbxx.h"
-
+#include <fstream>
 #include <iostream>
-#include <fstream>
-#include <python2.7/Python.h>
-#include <fstream>
-
-#include <sstream>
 #include <limits>
+#include <sstream>
 #include <string>
 #include <typeinfo>
 
+#include "/software/mu3e/midas_installations/midas_v1/mu3eSummer22/packages/midas/include/odbxx.h"
+#include "AS.h"
+#include "mfe.h"
+#include "midas.h"
+
 using namespace std;
 
-void Com(float setpoint){
-string SP = to_string(setpoint);
-string command = "s"+SP;
-ofstream ard("/dev/ttyACM0"); //opens arduino
-if (ard){
-	ard << command << '\n';
-}
+void Com(float setpoint) {
+    string SP = to_string(setpoint);
+    string command = "s" + SP;
+    ofstream ard("/dev/ttyACM0");  // opens arduino
+    if (ard) {
+        ard << command << '\n';
+    }
 }
 
 /*-- Globals -------------------------------------------------------*/
 INT serial_port = setting_arduino();
 
 /* The frontend name (client name) as seen by other MIDAS clients   */
-const char *frontend_name = "Temperature and humidity";                   //The frontend name
+const char *frontend_name = "Temperature and humidity";  // The frontend name
 
 /* The frontend file name, don't change it */
 const char *frontend_file_name = __FILE__;
@@ -78,28 +74,40 @@ INT interrupt_configure(INT cmd, INT source, POINTER_T adr);
 INT poll_event(INT source, INT count, BOOL test);
 INT read_periodic_event(char *pevent, INT off);
 
-
 /*-- Equipment list ------------------------------------------------*/
 BOOL equipment_common_overwrite = TRUE;
 EQUIPMENT equipment[] = {
 
-   {"ArduinoTestStation",              /* equipment name */
-      {9, 0,                 /* event ID, trigger mask */		//The event ID that needs to be unique to each of the 'Equipments' used
-         "SYSTEM",           /* event buffer */
-         EQ_PERIODIC,        /* equipment type */
-         0,                  /* event source */
-         "MIDAS",            /* format */
-         TRUE,               /* enabled */
-         RO_RUNNING | RO_TRANSITIONS |   /* read when running and on transitions */
-         RO_ODB,             /* and update ODB */
-         1000,               /* read every sec */
-         0,                  /* stop run after this event limit */
-         0,                  /* number of sub events */
-         1,               /* log history */				//Boolean to determine whether the history log data will be taken for a particular variable or not. MUST be a 1 or 0.
-         "", "", "",},
-      read_periodic_event,   /* readout routine */
-   },
-   {""}
+    {
+        "ArduinoTestStation", /* equipment name */
+        {
+            9,
+            0,
+            /* event ID, trigger mask */  // The event ID that needs to be
+                                          // unique to each of the 'Equipments'
+                                          // used
+            "SYSTEM",    /* event buffer */
+            EQ_PERIODIC, /* equipment type */
+            0,           /* event source */
+            "MIDAS",     /* format */
+            TRUE,        /* enabled */
+            RO_RUNNING |
+                RO_TRANSITIONS | /* read when running and on transitions */
+                RO_ODB,          /* and update ODB */
+            1000,                /* read every sec */
+            0,                   /* stop run after this event limit */
+            0,                   /* number of sub events */
+            1,
+            /* log history */  // Boolean to determine whether the history log
+                               // data will be taken for a particular variable
+                               // or not. MUST be a 1 or 0.
+            "",
+            "",
+            "",
+        },
+        read_periodic_event, /* readout routine */
+    },
+    {""}
 
 };
 
@@ -130,108 +138,95 @@ EQUIPMENT equipment[] = {
 
 /*-- Frontend Init -------------------------------------------------*/
 
-INT frontend_init()
-{
-   /* put any hardware initialization here */
-   /* print message and return FE_ERR_HW if frontend should not be started */
-   unsigned char data[] = "m";    // sets human readable output
-   write_data(serial_port, data, sizeof(data));
-   
-   return SUCCESS;
+INT frontend_init() {
+    /* put any hardware initialization here */
+    /* print message and return FE_ERR_HW if frontend should not be started */
+    unsigned char data[] = "m";  // sets human readable output
+    write_data(serial_port, data, sizeof(data));
+
+    return SUCCESS;
 }
 
 /*-- Frontend Exit -------------------------------------------------*/
 
-INT frontend_exit()
-{
-   close(serial_port);
-   return SUCCESS;
+INT frontend_exit() {
+    close(serial_port);
+    return SUCCESS;
 }
 
 /*-- Begin of Run --------------------------------------------------*/
 
-INT begin_of_run(INT run_number, char *error)
-{
+INT begin_of_run(INT run_number, char *error) {
     ifstream myReadFile;
     string setpoint;
 
-    myReadFile.open("setpoint.txt");
-    //myReadFile >> setpoint;
-    getline(myReadFile, setpoint);
-    //cout << setpoint;
-    
-    unsigned char data[] = "s15";
-    
-    //strcpy(data, setpoint.c_str());
+    // myReadFile.open("setpoint.txt");
+    // myReadFile >> setpoint;
+    // getline(myReadFile, setpoint);
+    // cout << setpoint;
 
-    //unsigned char* data_uchar = reinterpret_cast<unsigned char*>(data);
+    unsigned char data[] = "s15";
+
+    // strcpy(data, setpoint.c_str());
+
+    // unsigned char* data_uchar = reinterpret_cast<unsigned char*>(data);
     write_data(serial_port, data, sizeof(data));
-    
+
     /* put here clear scalers etc. */
-   return SUCCESS;
+    return SUCCESS;
 }
 
 /*-- End of Run ----------------------------------------------------*/
 
-INT end_of_run(INT run_number, char *error)
-{
-   return SUCCESS;
-}
+INT end_of_run(INT run_number, char *error) { return SUCCESS; }
 
 /*-- Pause Run -----------------------------------------------------*/
 
-INT pause_run(INT run_number, char *error)
-{
-    
-    //midas::odb exp("/");
-    //exp["set_point"] = 10;
-    
-    //float setpoint=exp["set_point"];
-    //cout << "The setpoint is: " << setpoint << endl;
-    
-    //char hostname[20];
-    //char str[128];
-    //int size,status;
-    //size = sizeof(hostname); 
-    //sprintf(str,"/setpoint",equipment[FIFO].name); 
-    //status = db_get_value(hDB, 0, str, hostname, &size, TID_STRING, FALSE);
+INT pause_run(INT run_number, char *error) {
+    // midas::odb exp("/");
+    // exp["set_point"] = 10;
 
+    // float setpoint=exp["set_point"];
+    // cout << "The setpoint is: " << setpoint << endl;
 
-  //  ifstream myReadFile;
-   // string setpoint;
+    // char hostname[20];
+    // char str[128];
+    // int size,status;
+    // size = sizeof(hostname);
+    // sprintf(str,"/setpoint",equipment[FIFO].name);
+    // status = db_get_value(hDB, 0, str, hostname, &size, TID_STRING, FALSE);
 
-    //myReadFile.open("setpoint.txt");
-    //myReadFile >> setpoint;
-   //getline(myReadFile, setpoint);
-    //cout << setpoint;
+    //  ifstream myReadFile;
+    // string setpoint;
 
-    //char* data = new char[setpoint.size() + 1];
-    //strcpy(data, setpoint.c_str());
+    // myReadFile.open("setpoint.txt");
+    // myReadFile >> setpoint;
+    // getline(myReadFile, setpoint);
+    // cout << setpoint;
 
-   // unsigned char data_1[10];
-  //  strcpy((char*)data_1, setpoint.c_str());
+    // char* data = new char[setpoint.size() + 1];
+    // strcpy(data, setpoint.c_str());
 
-    //unsigned char* data_uchar = reinterpret_cast<unsigned char*>(data);
-  //  write_data(serial_port, data_1, sizeof(data_1));
-    //unsigned char data2[] = "s15";
-    //write_data(serial_port, data2, sizeof(data2));
-   return SUCCESS;
+    // unsigned char data_1[10];
+    //  strcpy((char*)data_1, setpoint.c_str());
+
+    // unsigned char* data_uchar = reinterpret_cast<unsigned char*>(data);
+    //  write_data(serial_port, data_1, sizeof(data_1));
+    // unsigned char data2[] = "s15";
+    // write_data(serial_port, data2, sizeof(data2));
+    return SUCCESS;
 }
 
 /*-- Resume Run ----------------------------------------------------*/
 
-INT resume_run(INT run_number, char *error)
-{
-   return SUCCESS;
-}
+INT resume_run(INT run_number, char *error) { return SUCCESS; }
 
 /*-- Frontend Loop -------------------------------------------------*/
 
-INT frontend_loop()
-{
-   /* if frontend_call_loop is true, this routine gets called when
-      the frontend is idle or once between every event */
-   return SUCCESS;
+INT frontend_loop() {
+    /* if frontend_call_loop is true, this routine gets called when
+       the frontend is idle or once between every event */
+    return SUCCESS;
 }
 
 /*------------------------------------------------------------------*/
@@ -243,7 +238,7 @@ INT frontend_loop()
 \********************************************************************/
 
 /*-- Event readout -------------------------------------------------*/
-//THE FRONTEND CODE WHICH CONTROLS AND TAKES DATA FROM THE SENSORS
+// THE FRONTEND CODE WHICH CONTROLS AND TAKES DATA FROM THE SENSORS
 
 /*-- Trigger event routines ----------------------------------------*/
 
@@ -252,103 +247,96 @@ INT poll_event(INT source, INT count, BOOL test)
    is available. If test equals TRUE, don't return. The test
    flag is used to time the polling */
 {
-   int i;
-   DWORD flag;
+    int i;
+    DWORD flag;
 
-   for (i = 0; i < count; i++) {
-      /* poll hardware and set flag to TRUE if new event is available */
-      flag = TRUE;
+    for (i = 0; i < count; i++) {
+        /* poll hardware and set flag to TRUE if new event is available */
+        flag = TRUE;
 
-      if (flag)
-         if (!test)
-            return TRUE;
-   }
+        if (flag)
+            if (!test) return TRUE;
+    }
 
-   return 0;
+    return 0;
 }
 
 /*-- Interrupt configuration ---------------------------------------*/
 
-INT interrupt_configure(INT cmd, INT source, POINTER_T adr)
-{
-   switch (cmd) {
-   case CMD_INTERRUPT_ENABLE:
-      break;
-   case CMD_INTERRUPT_DISABLE:
-      break;
-   case CMD_INTERRUPT_ATTACH:
-      break;
-   case CMD_INTERRUPT_DETACH:
-      break;
-   }
-   return SUCCESS;
+INT interrupt_configure(INT cmd, INT source, POINTER_T adr) {
+    switch (cmd) {
+        case CMD_INTERRUPT_ENABLE:
+            break;
+        case CMD_INTERRUPT_DISABLE:
+            break;
+        case CMD_INTERRUPT_ATTACH:
+            break;
+        case CMD_INTERRUPT_DETACH:
+            break;
+    }
+    return SUCCESS;
 }
 
-
 /*-- Periodic event ------------------------------------------------*/
-INT read_periodic_event(char *pevent, INT off)
-{
-   float *pdata;  //Pointer to the data
+INT read_periodic_event(char *pevent, INT off) {
+    float *pdata;  // Pointer to the data
 
-   /* init bank structure */
-   bk_init(pevent);
-   //bk_init32a(pevent);
- 
-	//Reads Arduino
-   vector<double> v;
-   
-    while(v.size()!=7){
-      
-      read_data1(serial_port, v);
-      
-      //Fills midas data banks
-      if(v.size()==7){
-               
-         //cout << v[0]<< '\t' <<v[1]<< '\t' <<v[2]<< '\t' << v[4] << '\t' << v[5] << '\t' << v[6] << '\t' << endl;
-         
-         midas::odb exp("/Equipment/ArduinoTestStation/Variables");
-         Com(exp["_S_"]);
+    /* init bank structure */
+    bk_init(pevent);
+    // bk_init32a(pevent);
 
-         /* create SCLR bank */
-         bk_create(pevent, "_T_", TID_FLOAT, (void **)&pdata);
-         *pdata++=(float)v[0];
-         bk_close(pevent, pdata);
+    // Reads Arduino
+    vector<double> v;
 
-         /* create SCLR bank */
-         bk_create(pevent, "_F_", TID_FLOAT, (void **)&pdata);
-         *pdata++=(float)v[1];
-         bk_close(pevent, pdata);
+    while (v.size() != 7) {
+        read_data1(serial_port, v);
 
-         /* create SCLR bank */
-         bk_create(pevent, "_P_", TID_FLOAT, (void **)&pdata);
-         *pdata++=(float)v[2];
-         bk_close(pevent, pdata);
-         
-         /* create SCLR bank */
-         bk_create(pevent, "_A_", TID_FLOAT, (void **)&pdata);
-         *pdata++=(float)v[3];
-         bk_close(pevent, pdata);  
+        // Fills midas data banks
+        if (v.size() == 7) {
+            // cout << v[0]<< '\t' <<v[1]<< '\t' <<v[2]<< '\t' << v[4] << '\t'
+            // << v[5] << '\t' << v[6] << '\t' << endl;
 
-         /* create SCLR bank */
-         //bk_create(pevent, "_S_", TID_FLOAT, (void**)&pdata);
-         //*pdata++ = (float)v[4];
-         //bk_close(pevent, pdata);
+            midas::odb exp("/Equipment/ArduinoTestStation/Variables");
+            Com(exp["_S_"]);
 
-         /* create SCLR bank */
-         bk_create(pevent, "_RH_", TID_FLOAT, (void**)&pdata);
-         *pdata++ = (float)v[5];
-         bk_close(pevent, pdata);
+            /* create SCLR bank */
+            bk_create(pevent, "_T_", TID_FLOAT, (void **)&pdata);
+            *pdata++ = (float)v[0];
+            bk_close(pevent, pdata);
 
-	       /* create SCLR bank */
-         bk_create(pevent, "_AT_", TID_FLOAT, (void**)&pdata);
-         *pdata++ = (float)v[6];
-         bk_close(pevent, pdata);
+            /* create SCLR bank */
+            bk_create(pevent, "_F_", TID_FLOAT, (void **)&pdata);
+            *pdata++ = (float)v[1];
+            bk_close(pevent, pdata);
+
+            /* create SCLR bank */
+            bk_create(pevent, "_P_", TID_FLOAT, (void **)&pdata);
+            *pdata++ = (float)v[2];
+            bk_close(pevent, pdata);
+
+            /* create SCLR bank */
+            bk_create(pevent, "_A_", TID_FLOAT, (void **)&pdata);
+            *pdata++ = (float)v[3];
+            bk_close(pevent, pdata);
+
+            /* create SCLR bank */
+            // bk_create(pevent, "_S_", TID_FLOAT, (void**)&pdata);
+            //*pdata++ = (float)v[4];
+            // bk_close(pevent, pdata);
+
+            /* create SCLR bank */
+            bk_create(pevent, "_RH_", TID_FLOAT, (void **)&pdata);
+            *pdata++ = (float)v[5];
+            bk_close(pevent, pdata);
+
+            /* create SCLR bank */
+            bk_create(pevent, "_AT_", TID_FLOAT, (void **)&pdata);
+            *pdata++ = (float)v[6];
+            bk_close(pevent, pdata);
+        }
     }
-  }
-      
-   v.clear();
-   
 
-   return bk_size(pevent);
-    
+    v.clear();
+
+    return bk_size(pevent);
 }
