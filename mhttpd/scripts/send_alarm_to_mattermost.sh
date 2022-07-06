f12e9fdba3af0d1f9f8f310223f132aa989e87bb
Frederik Wauters <fwauters@uni-mainz.de>
2022-07-06 19:15:42 +0200

    script to post alarm message to mattermost

---
+++ mhttpd/scripts/send_alarm_to_mattermost.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+alarm_message=$1
+generate_post_data()
+{
+  cat <<EOF
+  {
+  "text": "Midas alarm triggered with message = $alarm_message", 
+  "icon_emoji":":rotating_light:", 
+  "username":"Midas"}
+EOF
+}
+curl -i -X POST -H 'Content-Type: application/jsoni' --data "$(generate_post_data)" https://mattermost.gitlab.rlp.net/hooks/8fm5nqcuzjf9xnjoaao7m6im9c
+#echo $alarm_message > /home/mu3e/online/mhttpd/scripts/out.txt
